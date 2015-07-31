9124721bd4964a9435e37da6938118b8a09ab94f
nshehzad <nshehzad@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-07-31 08:42:07 +0000

    folders name changed and unneccessary folders deleted.
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/MtcaMappedDevice/branches/new_interface@1245 5f601a5c-88b9-47f3-b851-818f6741d00d

--- libplugins/src/pluginElem.cpp
+++
@@ -1,7 +0,0 @@
-/* 
- * File:   pluginElem.cpp
- * Author: apiotro
- * 
- * Created on 2 sierpień 2012, 08:47
- */
-
--- libplugins/src/pluginManager.cpp
+++
@@ -1,9 +0,0 @@
-/* 
- * File:   pluginManager.cpp
- * Author: apiotro
- * 
- * Created on 1 sierpień 2012, 18:43
- */
-
-
-
