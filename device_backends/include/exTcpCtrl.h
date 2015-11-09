cfd92651dc5736732c8070ee3423e50f279b8b20
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-11-09 10:45:43 +0000

    renamed .cpp to .cc
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/branches/rebotIntegration@1740 5f601a5c-88b9-47f3-b851-818f6741d00d

---
+++ device_backends/include/exTcpCtrl.h
@@ -0,0 +1,25 @@
+/*
+ * exTcpCtrl.h
+ *
+ *  Created on: May 29, 2015
+ *      Author: adagio
+ */
+
+#ifndef EXTCPCTRL_H_
+#define EXTCPCTRL_H_
+
+#include <mtca4u/Exception.h>
+#include <string>
+
+namespace mtca4u {
+
+///Provides class for exceptions related to RebotDevice
+class exTcpCtrl : public Exception {
+public:
+  enum { EX_OPENSOCK, EX_CONNECT, EX_CLOSESOCK,  EX_WRSOCK, EX_RDSOCK, EX_DEVICE_CLOSED, EX_SETIP, EX_SETPORT, EX_SIZEMULT};
+  exTcpCtrl(const std::string &_exMessage, unsigned int _exID);
+};
+
+} //namespace mtca4u
+
+#endif /* EXTCPCTRL_H_ */
---
+++ device_backends/src/exTcpCtrl.cpp
@@ -0,0 +1,14 @@
+/*
+ * exTcpCtrl.cpp
+ *
+ *  Created on: May 29, 2015
+ *      Author: adagio
+ */
+
+#include "exTcpCtrl.h"
+
+namespace mtca4u {
+
+exTcpCtrl::exTcpCtrl(const std::string &_exMessage, unsigned int _exID) : Exception(_exMessage, _exID) { }
+
+} //namespace mtca4u
