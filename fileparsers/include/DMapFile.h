b006c7f8265da58905f9489842b5c17e6f095b03
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-11-19 12:26:26 +0000

    added DMapFile.h, a backward compatibility file which is never included and thus it was not noticed that it is missing.
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/branches/dmap_file_improvementsExt@1808 5f601a5c-88b9-47f3-b851-818f6741d00d

---
+++ fileparsers/include/DMapFile.h
@@ -0,0 +1,22 @@
+#ifndef _MTCA4U_DMAPFILE_H_
+#define _MTCA4U_DMAPFILE_H_
+
+#warning Using DMapFile is deprecated. The class has been renamed to DeviceInfoMap.
+
+#include "DeviceInfoMap.h"
+
+namespace mtca4u{
+
+  /** @deprecated Using DMapFile is deprecated. The class has been renamed to DeviceInfoMap.
+   */
+ class DMapFile: public DeviceInfoMap{
+ public:
+   /** @deprecated The name DRegisterInfo was a misnomer due to refactoring. Use DeviceInfo instead.
+    *  This alias does not make DRegisterInfo work the way it did because the members have also been renamed.
+    */
+   typedef DeviceInfo DRegisterInfo;
+ };
+
+}//namespace mtca4u
+
+#endif /* _MTCA4U_DMAPFILE_H_ */
