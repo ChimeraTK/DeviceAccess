30d32e3f62e627ed1bdd4a739a111c8f395776d3
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-11-27 17:11:25 +0000

    RebotTestClass now populates server ip and port from dmap
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/branches/rebotIntegration@1854 5f601a5c-88b9-47f3-b851-818f6741d00d

--- tests/executables_src/testRebotDevice.cpp
+++ tests/executables_src/testRebotDevice.cpp
@@ -1,44 +1,20 @@
 #include <boost/test/included/unit_test.hpp>
 #include "BackendFactory.h"
+#include "Utilities.h"
+#include "DMapFileParser.h"
 using namespace boost::unit_test_framework;
 
 typedef mtca4u::DeviceInfoMap::DeviceInfo DeviceInfo;
 
-class RebotTestClass;
-class RebotDeviceTestSuite;
-
+/******************************************************************************/
 struct RebotServerDetails{
   std::string ip;
   int port;
+
+  RebotServerDetails() : ip(), port(0) {};
+  RebotServerDetails(std::string& ipAddress, int portNumber)
+      : ip(ipAddress), port(portNumber) {};
 };
-
-/******************************************************************************/
-boost::unit_test::test_suite* init_unit_test_suite(int argc, char** argv) {
-  if (argc < 2) {
-
-    std::cout << "Usage: " << argv[0] << " cardAlias [dmapFile]" << std::endl;
-
-  } else if (bool hasSecondArgument = (argv[2] != nullptr)) {
-
-    // take dmap file location if given, else search for cardAlias in the
-    // factory default dmap file
-    mtca4u::BackendFactory::getInstance().setDMapFilePath(argv[2]);
-
-  }
-  return new RebotDeviceTestSuite(argv[1]);
-
-}
-/******************************************************************************/
-
-class RebotDeviceTestSuite : public test_suite {
-public:
-  RebotDeviceTestSuite(std::string const& cardAlias)
-          : test_suite("RebotDeviceTestSuite") {
-    boost::shared_ptr<RebotTestClass> rebotTest(new RebotTestClass(cardAlias));
-  }
-};
-
-/******************************************************************************/
 
 class RebotTestClass {
 private:
@@ -58,6 +34,34 @@ private:
   RebotServerDetails extractServerDetailsFromUri(std::string &uri);
 };
 
+/******************************************************************************/
+class RebotDeviceTestSuite : public test_suite {
+public:
+  RebotDeviceTestSuite(std::string const& cardAlias)
+          : test_suite("RebotDeviceTestSuite") {
+    boost::shared_ptr<RebotTestClass> rebotTest(new RebotTestClass(cardAlias));
+  }
+};
+
+boost::unit_test::test_suite* init_unit_test_suite(int argc, char** argv) {
+  if (argc < 2) {
+
+    std::cout << "Usage: " << argv[0] << " cardAlias [dmapFile]" << std::endl;
+
+  }
+  bool hasSecondArgument = (argv[2] != nullptr);
+  if (hasSecondArgument) {
+
+    // take dmap file location if given, else search for cardAlias in the
+    // factory default dmap file
+    mtca4u::BackendFactory::getInstance().setDMapFilePath(argv[2]);
+
+  }
+  return new RebotDeviceTestSuite(argv[1]);
+
+}
+
+/******************************************************************************/
 RebotTestClass::RebotTestClass(std::string const& cardAlias)
     : _cardAlias(cardAlias), _rebotServer(getServerDetails()) {}
 
@@ -78,7 +82,11 @@ DeviceInfo RebotTestClass::getDeviceDetailsFromDMap() {
   return deviceDetails;
 }
 
-inline RebotServerDetails RebotTestClass::extractServerDetailsFromUri(
-    std::string& uri) {
-
+inline RebotServerDetails RebotTestClass::extractServerDetailsFromUri(std::string& uri) {
+  mtca4u::Sdm parsedSDM = mtca4u::Utilities::parseSdm(uri);
+  std::list<std::string> &serverParameters = parsedSDM._Parameters;
+  std::list<std::string>::iterator it = serverParameters.begin();
+  std::string ip = *it;
+  int port = std::stoi(*(++it));
+  return RebotServerDetails(ip, port);
 }
