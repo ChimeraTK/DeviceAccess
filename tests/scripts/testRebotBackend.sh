02977e5ccd326a0c12b371064072314d34a54aae
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-12-01 14:02:41 +0000

    Set up the script to run the associated rebot testcases
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/branches/rebotIntegration@1875 5f601a5c-88b9-47f3-b851-818f6741d00d

--- tests/scripts/testRebotBackend.sh
+++ tests/scripts/testRebotBackend.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
-../bin/RebotDummyServer
+../bin/RebotDummyServer -m ./mtcadummy.map&
+sleep .1
+
+# run the test now
+../bin/testRebotDevice mskrebot ./dummies.dmap
+TEST_RESULT=$? 
+
+killall RebotDummyServer
+exit $TEST_RESULT
