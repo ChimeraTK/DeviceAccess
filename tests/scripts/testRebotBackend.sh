588bf4acf48023b6ea794d25a49169aa234ccb2c
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-12-01 17:10:44 +0000

    more gracefully terminating the RebotDummyServer than by using killall makes valgrind happy
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/trunk@1882 5f601a5c-88b9-47f3-b851-818f6741d00d

--- tests/scripts/testRebotBackend.sh
+++ tests/scripts/testRebotBackend.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
 ../bin/RebotDummyServer -m ./mtcadummy.map&
+SERVER_PID=$!
 sleep .1
 
 # run the test now
 ../bin/testRebotDevice mskrebot ./dummies.dmap
 TEST_RESULT=$? 
 
-killall RebotDummyServer
+kill $SERVER_PID
 exit $TEST_RESULT
