4fb5f8ed5e8fd636cb50880aa0bddd5bd32f637c
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-12-04 15:05:26 +0000

    fixed debian packaging scripts
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/trunk@1898 5f601a5c-88b9-47f3-b851-818f6741d00d

--- cmake/prepare_debian_package.cmake
+++ cmake/prepare_debian_package.cmake
@@ -7,16 +7,16 @@ set(mtca4u-deviceaccess_DEBVERSION ${mtca4u-deviceaccess_MAJOR_VERSION}-${mtca4u
 #Nothing to change, just copy
 file(COPY ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/compat
            ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/rules
-	   ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/mtca4u-deviceaccess-doc.install
-	   ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/mtca4u-deviceaccess-doc.doc-base
+	   ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/libmtca4u-deviceaccess-doc.install
+	   ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/libmtca4u-deviceaccess-doc.doc-base
      DESTINATION debian_from_template)
 
 file(COPY ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/source/format
      DESTINATION debian_from_template/source)
 
 #Adapt the file name
-configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/mtca4u-deviceaccessDEBVERSION.install
-               debian_from_template/mtca4u-deviceaccess${mtca4u-deviceaccess_DEBVERSION}.install COPYONLY)
+configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/libmtca4u-deviceaccessDEBVERSION.install
+               debian_from_template/libmtca4u-deviceaccess${mtca4u-deviceaccess_DEBVERSION}.install COPYONLY)
 
 #Adapt the file name and/or set the version number
 configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/control.in
@@ -25,11 +25,11 @@ configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/control.in
 configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/copyright.in
                debian_from_template/copyright @ONLY)
 
-configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/mtca4u-deviceaccessDEBVERSION.shlib.in
-               debian_from_template/mtca4u-deviceaccess${mtca4u-deviceaccess_DEBVERSION}.shlib @ONLY)
+configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/libmtca4u-deviceaccessDEBVERSION.shlib.in
+               debian_from_template/libmtca4u-deviceaccess${mtca4u-deviceaccess_DEBVERSION}.shlib @ONLY)
 
-configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/dev-mtca4u-deviceaccess.install.in
-               debian_from_template/dev-mtca4u-deviceaccess.install @ONLY)
+configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/libmtca4u-deviceaccess-dev.install.in
+               debian_from_template/libmtca4u-deviceaccess-dev.install @ONLY)
 
 #Copy and configure the shell script which performs the actual 
 #building of the package
@@ -43,11 +43,11 @@ add_custom_target(debian_package ${CMAKE_BINARY_DIR}/make_debian_package.sh
 
 #For convenience: Also create an install script for DESY
 #The shared library package has the version number in the package name
-set(PACKAGE_NAME "mtca4u-deviceaccess${mtca4u-deviceaccess_DEBVERSION}")
+set(PACKAGE_NAME "libmtca4u-deviceaccess${mtca4u-deviceaccess_DEBVERSION}")
 #The development package does not have the version in the name
-set(PACKAGE_DEV_NAME "dev-mtca4u-deviceaccess")
-set(PACKAGE_DOC_NAME "mtca4u-deviceaccess-doc")
-set(PACKAGE_FILES_WILDCARDS "${PACKAGE_NAME}_*.deb ${PACKAGE_DEV_NAME}_*.deb  ${PACKAGE_DOC_NAME}_*.deb mtca4u-deviceaccess_*.changes")
+set(PACKAGE_DEV_NAME "libmtca4u-deviceaccess-dev")
+set(PACKAGE_DOC_NAME "libmtca4u-deviceaccess-doc")
+set(PACKAGE_FILES_WILDCARDS "${PACKAGE_NAME}_*.deb ${PACKAGE_DEV_NAME}_*.deb  ${PACKAGE_DOC_NAME}_*.deb libmtca4u-deviceaccess_*.changes")
 
 configure_file(${CMAKE_SOURCE_DIR}/cmake/install_debian_package_at_DESY.sh.in
                install_debian_package_at_DESY.sh @ONLY)
