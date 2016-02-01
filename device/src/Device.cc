9c94a598f63aecdb35feb04e6201f79c08034b46
mhier <mhier@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-02-01 09:32:16 +0000

    separated the previously nested class RegisterAccessor out of Device
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/branches/moveMapppingToBackend@2109 5f601a5c-88b9-47f3-b851-818f6741d00d

--- device/src/Device.cc
+++ device/src/Device.cc
@@ -1,3 +1,4 @@
+#include "RegisterAccessor.h"
 #include "DummyBackend.h"
 #include "Device.h"
 #include "PcieBackend.h"
@@ -260,72 +261,6 @@ namespace mtca4u{
   std::string Device::readDeviceInfo() const {
     checkPointersAreNotNull();
     return _deviceBackendPointer->readDeviceInfo();
-  }
-
-  Device::RegisterAccessor::RegisterAccessor(const RegisterInfoMap::RegisterInfo &registerInfo,
-      DeviceBackendPointer deviceBackendPointer)
-  : _registerInfo(registerInfo),
-    _deviceBackendPointer(deviceBackendPointer),
-    _fixedPointConverter(_registerInfo.width, _registerInfo.nFractionalBits, _registerInfo.signedFlag) {}
-
-  void Device::RegisterAccessor::checkRegister(const RegisterInfoMap::RegisterInfo &registerInfo,
-      size_t dataSize,
-      uint32_t addRegOffset,
-      uint32_t &retDataSize,
-      uint32_t &retRegOff) {
-    if (addRegOffset % 4) {
-      throw DeviceException("Register offset must be divisible by 4",
-          DeviceException::EX_WRONG_PARAMETER);
-    }
-    if (dataSize) {
-      if (dataSize % 4) {
-        throw DeviceException("Data size must be divisible by 4",
-            DeviceException::EX_WRONG_PARAMETER);
-      }
-      if (dataSize > registerInfo.nBytes - addRegOffset) {
-        throw DeviceException("Data size exceed register size",
-            DeviceException::EX_WRONG_PARAMETER);
-      }
-      retDataSize = dataSize;
-    } else {
-      retDataSize = registerInfo.nBytes;
-    }
-    retRegOff = registerInfo.address + addRegOffset;
-  }
-
-  void Device::RegisterAccessor::readRaw(int32_t *data, size_t dataSize,
-      uint32_t addRegOffset) const {
-    uint32_t retDataSize;
-    uint32_t retRegOff;
-    checkRegister(_registerInfo, dataSize, addRegOffset, retDataSize, retRegOff);
-    _deviceBackendPointer->read(_registerInfo.bar, retRegOff, data, retDataSize);
-  }
-
-  void Device::RegisterAccessor::writeRaw(int32_t const *data, size_t dataSize,
-      uint32_t addRegOffset) {
-    uint32_t retDataSize;
-    uint32_t retRegOff;
-    checkRegister(_registerInfo, dataSize, addRegOffset, retDataSize, retRegOff);
-    _deviceBackendPointer->write(_registerInfo.bar, retRegOff, data, retDataSize);
-  }
-
-  void Device::RegisterAccessor::readDMA(int32_t *data, size_t dataSize,
-      uint32_t addRegOffset) const {
-    readRaw(data,dataSize,addRegOffset);
-  }
-
-  void Device::RegisterAccessor::writeDMA(int32_t const *data, size_t dataSize,
-      uint32_t addRegOffset) {
-    writeRaw(data,dataSize,addRegOffset);
-  }
-
-  RegisterInfoMap::RegisterInfo const &Device::RegisterAccessor::getRegisterInfo() const {
-    return _registerInfo;
-  }
-
-  FixedPointConverter const &Device::RegisterAccessor::getFixedPointConverter()
-  const {
-    return _fixedPointConverter;
   }
 
   void Device::checkPointersAreNotNull() const {
