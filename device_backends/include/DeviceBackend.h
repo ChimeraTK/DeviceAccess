afcf3b94f6c0ee3d503ace359a77dda85af57f49
mhier <mhier@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-02-08 12:58:41 +0000

    update documentation
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/trunk@2188 5f601a5c-88b9-47f3-b851-818f6741d00d

--- device_backends/include/DeviceBackend.h
+++ device_backends/include/DeviceBackend.h
@@ -39,57 +39,44 @@ namespace mtca4u {
        */
       virtual void close() = 0;
 
-      /** Read one or more words from the device.
+      /** Read one or more words from the named register.
        *  @attention In case you leave data size at 0, the full size of the register is
-       *  read, not just one word as in DeviceBackend::readArea! Make sure your buffer
-       *  is large enough!
+       *  read! Make sure your buffer is large enough!
        */
       virtual void read(const std::string &regModule, const std::string &regName,
           int32_t *data, size_t dataSize = 0, uint32_t addRegOffset = 0) = 0;
 
-      /** Write one or more words from the device.
+      /** Write one or more words to the name register.
        *  @attention In case you leave data size at 0, the full size of the register is
-       *  written, not just one word as in DeviceBackend::readArea! Make sure your buffer
-       *  is large enough!
+       *  written! Make sure your buffer is large enough!
        */
       virtual void write(const std::string &regName,
           const std::string &regModule, int32_t const *data,
           size_t dataSize = 0, uint32_t addRegOffset = 0) = 0;
 
-      /** TODO documentation missing
+      /** Read one or more words from the given memory address. Not all backends might support
+       *  memory addresses, thus a not-implemented exception might be thrown.
+       *  @attention In case you leave data size at 0, the full size of the register is
+       *  read! Make sure your buffer is large enough!
        */
       virtual void read(uint8_t bar, uint32_t address, int32_t* data,  size_t sizeInBytes) = 0;
 
-      /** TODO documentation missing
+      /** Write one or more words to the given memory address. Not all backends might support
+       *  memory addresses, thus a not-implemented exception might be thrown.
+       *  @attention In case you leave data size at 0, the full size of the register is
+       *  written! Make sure your buffer is large enough!
        */
       virtual void write(uint8_t bar, uint32_t address, int32_t const* data,  size_t sizeInBytes) = 0;
 
-      /** \deprecated {
-       *  This function is deprecated. Use read() instead!
-       *  @todo Add printed warning after release of version 0.2
-       *  }
+      /** TODO documentation missing
        */
-      virtual void readDMA(uint8_t bar, uint32_t address, int32_t* data,  size_t sizeInBytes) = 0;
-
-      /** \deprecated {
-       *  This function is deprecated. Use write() instead!
-       *  @todo Add printed warning after release of version 0.2
-       *  }
-       */
-      virtual void writeDMA(uint8_t bar, uint32_t address, int32_t const* data,  size_t sizeInBytes) = 0;
-
       virtual std::string readDeviceInfo() = 0;
 
       /** Return whether a device has been opened or not.
-       *  As the variable already exists in the base class we implement this
-       * function here to avoid
-       *  having to reimplement the same, trivial return function over and over
-       * again.
        */
       virtual bool isOpen() = 0;
 
-      /** Return whether a device has been connected or not.
-       * A device is considered connected when it is created.
+      /** Return whether a device has been connected or not. A device is considered connected when it is created.
        */
       virtual bool isConnected() = 0;
 
@@ -122,6 +109,20 @@ namespace mtca4u {
        */
       virtual std::list< boost::shared_ptr<mtca4u::RegisterAccessor> > getRegisterAccessorsInModule(
           const std::string &moduleName) = 0;
+
+      /** \deprecated {
+       *  This function is deprecated. Use read() instead!
+       *  @todo Add printed warning after release of version 0.2
+       *  }
+       */
+      virtual void readDMA(uint8_t bar, uint32_t address, int32_t* data,  size_t sizeInBytes) = 0;
+
+      /** \deprecated {
+       *  This function is deprecated. Use write() instead!
+       *  @todo Add printed warning after release of version 0.2
+       *  }
+       */
+      virtual void writeDMA(uint8_t bar, uint32_t address, int32_t const* data,  size_t sizeInBytes) = 0;
 
     protected:
 
