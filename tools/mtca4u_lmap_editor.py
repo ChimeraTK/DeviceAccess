42a90f946f453cbea3c69455b33393ec26327f40
mhier <mhier@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-02-08 18:12:17 +0000

    added Python GUI tool to create and edit logical name mapping files
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/trunk@2191 5f601a5c-88b9-47f3-b851-818f6741d00d

---
+++ tools/mtca4u_lmap_editor.py
@@ -0,0 +1,378 @@
+#!/usr/bin/python
+
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import xml.etree.ElementTree as ET
+
+class MainWindow(QMainWindow):
+
+#######################################################################################################################
+# constructor: create main window
+    def __init__(self):
+    
+        super(MainWindow, self).__init__()
+        self.grid = QGridLayout()
+        
+        # create empty XML tree
+        self.theData = ET.Element("logicalNameMap")
+
+        # create main list
+        self.listWidget = QListWidget()
+        self.listWidget.currentItemChanged.connect(self.selectListItem)
+        self.grid.addWidget(self.listWidget,1,1,1,5)
+        
+        #
+        # editing fields
+        #
+        
+        # name
+        self.grid.addWidget(QLabel('Name:'),2,1)
+        self.edName = QLineEdit()
+        self.grid.addWidget(self.edName,2,2,1,4)
+        self.edName.textChanged.connect(self.changeName)
+        
+        # target type
+        self.grid.addWidget(QLabel('Target type:'),3,1)
+        self.edType = QComboBox()
+        self.edType.setEditable(False)
+        self.edType.addItem("- select -", "")
+        self.edType.addItem("full register", "register")
+        self.edType.addItem("range of 1D register", "range")
+        self.edType.addItem("channel of 2D register","channel")
+        self.edType.addItem("integer constant","int_constant")
+        self.grid.addWidget(self.edType,3,2)
+        self.edType.currentIndexChanged.connect(self.changeTargetType)
+        
+        # target device
+        self.grid.addWidget(QLabel('Target device alias:'),4,1)
+        self.edDevice = QLineEdit()
+        self.edDevice.setEnabled(False)
+        self.grid.addWidget(self.edDevice,4,2,1,4)
+        
+        # target register
+        self.grid.addWidget(QLabel('Target register name:'),5,1)
+        self.edRegister = QLineEdit()
+        self.edRegister.setEnabled(False)
+        self.grid.addWidget(self.edRegister,5,2,1,4)
+        
+        # first index
+        self.grid.addWidget(QLabel('First index:'),6,1)
+        self.edFirstIndex = QSpinBox()
+        self.edFirstIndex.setEnabled(False)
+        self.grid.addWidget(self.edFirstIndex,6,2,1,1)
+        
+        # length (number of indices)
+        self.grid.addWidget(QLabel('Length (nb. of indices):'),7,1)
+        self.edLength = QSpinBox()
+        self.edLength.setEnabled(False)
+        self.grid.addWidget(self.edLength,7,2,1,1)
+        
+        # channel
+        self.grid.addWidget(QLabel('Channel:'),8,1)
+        self.edChannel = QSpinBox()
+        self.edChannel.setEnabled(False)
+        self.grid.addWidget(self.edChannel,8,2,1,1)
+        
+        # value
+        self.grid.addWidget(QLabel('Value:'),9,1)
+        self.edValue = QSpinBox()
+        self.edValue.setEnabled(False)
+        self.grid.addWidget(self.edValue,9,2,1,1)
+        
+        # buttons
+        self.edAdd = QPushButton('Save as new entry')
+        self.edAdd.setEnabled(False)
+        self.grid.addWidget(self.edAdd,10,1)
+        self.edAdd.clicked.connect(self.saveNewEntry)
+
+        self.edUpdate = QPushButton('Update entry')
+        self.edUpdate.setEnabled(False)
+        self.grid.addWidget(self.edUpdate,10,2)
+        self.edUpdate.clicked.connect(self.updateEntry)
+
+        self.edDelete = QPushButton('Delete entry')
+        self.edDelete.setEnabled(False)
+        self.grid.addWidget(self.edDelete,10,3)
+        self.edDelete.clicked.connect(self.deleteEntry)
+
+        # add the menu
+        self.makeMenu()
+
+        # add status bar
+        self.statusBar = QStatusBar()
+        self.setStatusBar(self.statusBar)
+        self.statusBar.showMessage('Ready.')
+
+        # add grid layout
+        self.widget = QWidget();
+        self.widget.setLayout(self.grid);
+        self.setCentralWidget(self.widget);
+
+        # set window title and size
+        self.setWindowTitle("mtca4u Logical Name Mapping editor")
+        self.resize(800, 800)
+
+#######################################################################################################################
+# create the menu
+    def makeMenu(self):
+
+        menuBar = self.menuBar()
+        fileMenu = menuBar.addMenu("&File")
+
+        self._fileOpenAction = QAction("&Open logical name mapping file...", None)
+        self.connect(self._fileOpenAction, SIGNAL('triggered()'), self.openFile)
+        fileMenu.addAction(self._fileOpenAction)
+
+        self._fileSaveAction = QAction("&Save logical name mapping file...", None)
+        self.connect(self._fileSaveAction, SIGNAL('triggered()'), self.saveFile)
+        fileMenu.addAction(self._fileSaveAction)
+
+        self._exitAction = QAction("&Quit", None)
+        self.connect(self._exitAction, SIGNAL('triggered()'), self.exit)
+        fileMenu.addAction(self._exitAction)
+
+#######################################################################################################################
+# save new entry to the list
+    def saveNewEntry(self):
+        
+        # create new entry element (with name attribute)
+        entry = ET.SubElement(self.theData,"entry")
+        entry.set("name", str(self.edName.text()) )
+        
+        # fill element with data
+        self.fillElement(entry)
+        
+        # add list entry
+        item = QListWidgetItem(self.edName.text())
+        self.listWidget.addItem(item)
+        
+        # select the new entry in list
+        self.listWidget.setCurrentItem(item)
+
+#######################################################################################################################
+# update existing entry in the list
+    def updateEntry(self):
+        
+        # get name of selected item
+        name = str(self.listWidget.currentItem().text())
+        
+        # find element
+        entry = self.theData.find("entry[@name='"+name+"']")
+        
+        # update name
+        entry.set("name", str(self.edName.text()) )
+        self.listWidget.currentItem().setText(self.edName.text())
+        
+        # strip content
+        for elem in entry: 
+            entry.remove(elem)
+        
+        # fill with new data
+        self.fillElement(entry)
+
+#######################################################################################################################
+# delete existing entry from the list
+    def deleteEntry(self):
+        
+        # get name of selected item
+        name = str(self.listWidget.currentItem().text())
+        
+        # find element
+        entry = self.theData.find("entry[@name='"+name+"']")
+        
+        # remove from root
+        self.theData.remove(entry)
+        
+        # remove from list
+        self.listWidget.takeItem(self.listWidget.currentRow())
+
+#######################################################################################################################
+# fill XML element with data from editor
+    def fillElement(self, entry):
+        
+        # add sub-element for the type
+        elem = ET.SubElement(entry, "type")
+        type = str( self.edType.itemData( self.edType.currentIndex() ).toString() )
+        elem.text = type
+        
+        # add type-dependent sub-elements
+        if type == "register":
+            elem = ET.SubElement(entry, "device")
+            elem.text = str( self.edDevice.text() )
+            elem = ET.SubElement(entry, "register")
+            elem.text = str( self.edRegister.text() )
+        elif type == "range":
+            elem = ET.SubElement(entry, "device")
+            elem.text = str( self.edDevice.text() )
+            elem = ET.SubElement(entry, "register")
+            elem.text = str( self.edRegister.text() )
+            elem = ET.SubElement(entry, "index")
+            elem.text = str( self.edFirstIndex.value() )
+            elem = ET.SubElement(entry, "length")
+            elem.text = str( self.edLength.value() )
+        elif type == "channel":
+            elem = ET.SubElement(entry, "device")
+            elem.text = str( self.edDevice.text() )
+            elem = ET.SubElement(entry, "register")
+            elem.text = str( self.edRegister.text() )
+            elem = ET.SubElement(entry, "channel")
+            elem.text = str( self.edChannel.value() )
+        elif type == "int_constant":
+            elem = ET.SubElement(entry, "value")
+            elem.text = str( self.edValue.value() )
+
+#######################################################################################################################
+# select a different list item
+    def selectListItem(self):
+        name = str(self.listWidget.currentItem().text())
+        
+        # find xml element "entry" with the given name
+        elem = self.theData.find("entry[@name='"+name+"']")
+        
+        # set editor fields
+        self.edName.setText(name)
+        type = elem.find("type").text
+        self.edType.setCurrentIndex( self.edType.findData(type) )
+        if type == "register":
+            self.edDevice.setText(elem.find("device").text)
+            self.edRegister.setText(elem.find("register").text)
+        elif type == "range":
+            self.edDevice.setText(elem.find("device").text)
+            self.edRegister.setText(elem.find("register").text)
+            self.edFirstIndex.setValue(int(elem.find("index").text))
+            self.edLength.setValue(int(elem.find("length").text))
+        elif type == "channel":
+            self.edDevice.setText(elem.find("device").text)
+            self.edRegister.setText(elem.find("register").text)
+            self.edChannel.setValue(int(elem.find("channel").text))
+        elif type == "int_constant":
+            self.edValue.setValue(int(elem.find("value").text))
+
+        # enable buttons
+        self.edAdd.setEnabled(False)
+        self.edUpdate.setEnabled(True)
+        self.edDelete.setEnabled(True)
+
+#######################################################################################################################
+# update existing entry in the list
+    def changeName(self):
+        name = str(self.edName.text())
+        if name == "":
+            self.edAdd.setEnabled(False)
+            self.edUpdate.setEnabled(False)
+            self.edDelete.setEnabled(False)
+        elif self.theData.find("entry[@name='"+name+"']"):
+            self.edAdd.setEnabled(False)
+            self.edUpdate.setEnabled(True)
+            self.edDelete.setEnabled(True)
+        elif self.listWidget.currentItem():
+            self.edAdd.setEnabled(True)
+            self.edUpdate.setEnabled(True)
+            self.edDelete.setEnabled(True)
+        else:
+            self.edAdd.setEnabled(True)
+            self.edUpdate.setEnabled(False)
+            self.edDelete.setEnabled(False)
+
+
+#######################################################################################################################
+# change target type: update disabled and enabled fields
+    def changeTargetType(self):
+        type = self.edType.itemData( self.edType.currentIndex() )
+        if type == "register":
+            self.edDevice.setEnabled(True)
+            self.edRegister.setEnabled(True)
+            self.edFirstIndex.setEnabled(False)
+            self.edLength.setEnabled(False)
+            self.edChannel.setEnabled(False)
+            self.edValue.setEnabled(False)
+        elif type == "range":
+            self.edDevice.setEnabled(True)
+            self.edRegister.setEnabled(True)
+            self.edFirstIndex.setEnabled(True)
+            self.edLength.setEnabled(True)
+            self.edChannel.setEnabled(False)
+            self.edValue.setEnabled(False)
+        elif type == "channel":
+            self.edDevice.setEnabled(True)
+            self.edRegister.setEnabled(True)
+            self.edFirstIndex.setEnabled(False)
+            self.edLength.setEnabled(False)
+            self.edChannel.setEnabled(True)
+            self.edValue.setEnabled(False)
+        elif type == "int_constant":
+            self.edDevice.setEnabled(False)
+            self.edRegister.setEnabled(False)
+            self.edFirstIndex.setEnabled(False)
+            self.edLength.setEnabled(False)
+            self.edChannel.setEnabled(False)
+            self.edValue.setEnabled(True)
+        else:
+            self.edDevice.setEnabled(False)
+            self.edRegister.setEnabled(False)
+            self.edFirstIndex.setEnabled(False)
+            self.edLength.setEnabled(False)
+            self.edChannel.setEnabled(False)
+            self.edValue.setEnabled(False)
+             
+
+#######################################################################################################################
+# open lmap file
+    def openFile(self):
+        
+        # create open device dialog
+        dlg = QFileDialog(self)
+        dlg.setAcceptMode(QFileDialog.AcceptOpen)
+        dlg.setWindowTitle('Open XML logical map file')
+        dlg.setViewMode( QFileDialog.Detail )
+        dlg.setNameFilters( [self.tr('XML Logical Map Files (*.xlmap)'), self.tr('All Files (*)')] )
+        dlg.setDefaultSuffix('xlmap')
+        
+        # show dialog, open only if user did not cancel
+        if dlg.exec_() :
+            # file name must be converted into standard python string
+            name = str(dlg.selectedFiles()[0])
+            # parse xml file
+            tree = ET.parse(name)
+            self.theData = tree.getroot()
+            # populate list
+            self.listWidget.clear()
+            for entry in self.theData.findall("entry"):
+                self.listWidget.addItem(QListWidgetItem(entry.get("name")))
+
+#######################################################################################################################
+# save lmap file
+    def saveFile(self):
+        # create file-save dialog
+        dlg = QFileDialog(self)
+        dlg.setAcceptMode(QFileDialog.AcceptSave)
+        dlg.setWindowTitle('Save XML logical map file')
+        dlg.setViewMode( QFileDialog.Detail )
+        dlg.setNameFilters( [self.tr('XML Logical Map Files (*.xlmap)'), self.tr('All Files (*)')] )
+        dlg.setDefaultSuffix('xlmap')
+        
+        # show dialog, save only if user did not cancel
+        if dlg.exec_() :
+            # file name must be converted into standard python string
+            name = str(dlg.selectedFiles()[0])
+            # create element tree
+            tree = ET.ElementTree()
+            tree._setroot(self.theData)
+            tree.write(name)
+            # show message of success
+            QMessageBox.information(self, 'Map saved', 'Logical name mapping has been saved to file "'+name+'".')
+        
+
+#######################################################################################################################
+# open lmap file
+    def exit(self):
+        self.close()
+
+#######################################################################################################################
+# main function: initialise app
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
