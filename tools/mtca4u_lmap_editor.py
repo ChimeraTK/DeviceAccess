415c639c475ff0ab049271c91796a445f9dc8f6e
mhier <mhier@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-02-09 10:00:31 +0000

    - allow opening file from command line - menu option to save file under same name - added opened file name to window title
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/trunk@2198 5f601a5c-88b9-47f3-b851-818f6741d00d

--- tools/mtca4u_lmap_editor.py
+++ tools/mtca4u_lmap_editor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import os
 import sys
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
@@ -120,13 +121,18 @@ class MainWindow(QMainWindow):
         menuBar = self.menuBar()
         fileMenu = menuBar.addMenu("&File")
 
-        self._fileOpenAction = QAction("&Open logical name mapping file...", None)
-        self.connect(self._fileOpenAction, SIGNAL('triggered()'), self.openFile)
+        self._fileOpenAction = QAction("&Open...", None)
+        self.connect(self._fileOpenAction, SIGNAL('triggered()'), self.openFileDialog)
         fileMenu.addAction(self._fileOpenAction)
 
-        self._fileSaveAction = QAction("&Save logical name mapping file...", None)
-        self.connect(self._fileSaveAction, SIGNAL('triggered()'), self.saveFile)
+        self._fileSaveAction = QAction("Save &As...", None)
+        self.connect(self._fileSaveAction, SIGNAL('triggered()'), self.saveFileDialog)
         fileMenu.addAction(self._fileSaveAction)
+
+        self._fileSaveAgainAction = QAction("&Save", None)
+        self._fileSaveAgainAction.setEnabled(False)
+        self.connect(self._fileSaveAgainAction, SIGNAL('triggered()'), self.saveFileAgain)
+        fileMenu.addAction(self._fileSaveAgainAction)
 
         self._exitAction = QAction("&Quit", None)
         self.connect(self._exitAction, SIGNAL('triggered()'), self.exit)
@@ -318,8 +324,8 @@ class MainWindow(QMainWindow):
              
 
 #######################################################################################################################
-# open lmap file
-    def openFile(self):
+# show open lmap file dialogue
+    def openFileDialog(self):
         
         # create open device dialog
         dlg = QFileDialog(self)
@@ -328,22 +334,35 @@ class MainWindow(QMainWindow):
         dlg.setViewMode( QFileDialog.Detail )
         dlg.setNameFilters( [self.tr('XML Logical Map Files (*.xlmap)'), self.tr('All Files (*)')] )
         dlg.setDefaultSuffix('xlmap')
+        if self.fileName != "":
+            dlg.setDirectory( os.path.dirname(self.fileName) )
         
         # show dialog, open only if user did not cancel
         if dlg.exec_() :
             # file name must be converted into standard python string
             name = str(dlg.selectedFiles()[0])
-            # parse xml file
-            tree = ET.parse(name)
-            self.theData = tree.getroot()
-            # populate list
-            self.listWidget.clear()
-            for entry in self.theData.findall("entry"):
-                self.listWidget.addItem(QListWidgetItem(entry.get("name")))
+            # open the file
+            self.openFile(name)
 
 #######################################################################################################################
-# save lmap file
-    def saveFile(self):
+# open lmap file
+    def openFile(self, fileName):
+        # parse xml file
+        tree = ET.parse(fileName)
+        self.theData = tree.getroot()
+        # populate list
+        self.listWidget.clear()
+        for entry in self.theData.findall("entry"):
+            self.listWidget.addItem(QListWidgetItem(entry.get("name")))
+            
+        # store file name and change window title
+        self.fileName = os.path.abspath(fileName)
+        self.setWindowTitle(os.path.basename(self.fileName)+" ("+os.path.dirname(self.fileName)+") - mtca4u Logical Name Mapping editor")
+        self._fileSaveAgainAction.setEnabled(True)
+        
+#######################################################################################################################
+# show save lmap file dialogue
+    def saveFileDialog(self):
         # create file-save dialog
         dlg = QFileDialog(self)
         dlg.setAcceptMode(QFileDialog.AcceptSave)
@@ -351,18 +370,35 @@ class MainWindow(QMainWindow):
         dlg.setViewMode( QFileDialog.Detail )
         dlg.setNameFilters( [self.tr('XML Logical Map Files (*.xlmap)'), self.tr('All Files (*)')] )
         dlg.setDefaultSuffix('xlmap')
+        if self.fileName != "":
+            dlg.setDirectory( os.path.dirname(self.fileName) )
         
         # show dialog, save only if user did not cancel
         if dlg.exec_() :
             # file name must be converted into standard python string
             name = str(dlg.selectedFiles()[0])
-            # create element tree
-            tree = ET.ElementTree()
-            tree._setroot(self.theData)
-            tree.write(name)
-            # show message of success
-            QMessageBox.information(self, 'Map saved', 'Logical name mapping has been saved to file "'+name+'".')
+            # save the file
+            self.saveFile(name)
+
+#########################################################################################
+# save lmap file
+    def saveFile(self, fileName):
+        # create element tree
+        tree = ET.ElementTree()
+        tree._setroot(self.theData)
+        tree.write(fileName)
+        # show message of success
+        QMessageBox.information(self, 'Map saved', 'Logical name mapping has been saved to file "'+name+'".')
         
+        # store file name and change window title
+        self.fileName = os.path.abspath(fileName)
+        self.setWindowTitle(os.path.basename(self.fileName)+" ("+os.path.dirname(self.fileName)+") - mtca4u Logical Name Mapping editor")
+        self._fileSaveAgainAction.setEnabled(True)
+
+#########################################################################################
+# save lmap file with same name
+    def saveFileAgain(self):
+        self.saveFile(self.fileName)
 
 #######################################################################################################################
 # open lmap file
@@ -374,5 +410,7 @@ class MainWindow(QMainWindow):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
+    if sys.argv[1] != "":
+        window.openFile(sys.argv[1])
     window.show()
     sys.exit(app.exec_())
