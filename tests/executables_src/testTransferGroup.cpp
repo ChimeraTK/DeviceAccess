dc0e2dd138d28984369a179eda5f479825796282
mhier <mhier@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-02-12 16:04:40 +0000

    added tests for the TransferGroup
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/trunk@2231 5f601a5c-88b9-47f3-b851-818f6741d00d

--- tests/executables_src/testTransferGroup.cpp
+++ tests/executables_src/testTransferGroup.cpp
@@ -42,18 +42,84 @@ void TransferGroupTest::testSimpleCase() {
 
   device.open("PCIE2");
 
-  BufferingRegisterAccessor<int> a1 = device.getBufferingRegisterAccessor<int>("ADC","AREA_DMA_VIA_DMA");
-  BufferingRegisterAccessor<int> a2 = device.getBufferingRegisterAccessor<int>("ADC","AREA_DMA_VIA_DMA");
-  BufferingRegisterAccessor<int> a3 = device.getBufferingRegisterAccessor<int>("ADC","AREA_DMAABLE");
-  BufferingRegisterAccessor<double> a4 = device.getBufferingRegisterAccessor<double>("ADC","AREA_DMA_VIA_DMA");
+  BufferingRegisterAccessor<int> a1 = device.getBufferingRegisterAccessor<int>("ADC","AREA_DMAABLE");
+  BufferingRegisterAccessor<int> a2 = device.getBufferingRegisterAccessor<int>("ADC","AREA_DMAABLE");
+  BufferingRegisterAccessor<int> a3 = device.getBufferingRegisterAccessor<int>("BOARD","WORD_STATUS");
+  BufferingRegisterAccessor<unsigned int> a4 = device.getBufferingRegisterAccessor<unsigned int>("ADC","AREA_DMAABLE");
 
+  // slightly redundant to do this test here, this is just a control test still independent of the TransferGroup
+  a1[0] = 42;
+  a2[0] = 120;
+  a3[0] = 123;
+  a4[0] = 456;
+  BOOST_CHECK(a1[0] == 42);
+  a1.write();
+  a3.write();
+  a3[0] = 654;
+  BOOST_CHECK(a2[0] == 120);
+  BOOST_CHECK(a3[0] == 654);
+  BOOST_CHECK(a4[0] == 456);
+  a2.read();
+  BOOST_CHECK(a1[0] == 42);
+  BOOST_CHECK(a2[0] == 42);
+  BOOST_CHECK(a3[0] == 654);
+  BOOST_CHECK(a4[0] == 456);
+  a3.read();
+  BOOST_CHECK(a1[0] == 42);
+  BOOST_CHECK(a2[0] == 42);
+  BOOST_CHECK(a3[0] == 123);
+  BOOST_CHECK(a4[0] == 456);
+  a4.read();
+  BOOST_CHECK(a1[0] == 42);
+  BOOST_CHECK(a2[0] == 42);
+  BOOST_CHECK(a3[0] == 123);
+  BOOST_CHECK(a4[0] == 42);
+
+  // add accessors to the transfer group
   TransferGroup group;
   group.addAccessor(a1);
   group.addAccessor(a2);
   group.addAccessor(a3);
   group.addAccessor(a4);
 
-  group.read();
+  // check if a1 and a2 now share the same buffer, and the others not
+  a1[0] = 333;
+  BOOST_CHECK(a1[0] == 333);
+  BOOST_CHECK(a2[0] == 333);
+  BOOST_CHECK(a3[0] == 123);
+  BOOST_CHECK(a4[0] == 42);
+  a2[0] = 666;
+  BOOST_CHECK(a1[0] == 666);
+  BOOST_CHECK(a2[0] == 666);
+  BOOST_CHECK(a3[0] == 123);
+  BOOST_CHECK(a4[0] == 42);
+  a3[0] = 999;
+  BOOST_CHECK(a1[0] == 666);
+  BOOST_CHECK(a2[0] == 666);
+  BOOST_CHECK(a3[0] == 999);
+  BOOST_CHECK(a4[0] == 42);
+  a4[0] = 111;
+  BOOST_CHECK(a1[0] == 666);
+  BOOST_CHECK(a2[0] == 666);
+  BOOST_CHECK(a3[0] == 999);
+  BOOST_CHECK(a4[0] == 111);
 
+ // write will trigger writes of the underlying accessors in sequence, so a4 will write at last
+  group.write();
+  BOOST_CHECK(a1[0] == 666);
+  BOOST_CHECK(a2[0] == 666);
+  BOOST_CHECK(a3[0] == 999);
+  BOOST_CHECK(a4[0] == 111);
+  a1[0] = 0;
+  a2[0] = 0;
+  a3[0] = 0;
+  a4[0] = 0;
+
+  // now read and check if values are as expected
+  group.read();
+  BOOST_CHECK(a1[0] == 111);
+  BOOST_CHECK(a2[0] == 111);
+  BOOST_CHECK(a3[0] == 999);
+  BOOST_CHECK(a4[0] == 111);
 
 }
