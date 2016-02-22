9ae6f6acb719116de381bccf9d0a1a657765a333
mhier <mhier@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-02-22 14:22:38 +0000

    changed the type conversion operator from RegisterPath to std::string to convert into a const reference properly (see last commit)
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/trunk@2313 5f601a5c-88b9-47f3-b851-818f6741d00d

--- fileparsers/include/LogicalNameMap.h
+++ fileparsers/include/LogicalNameMap.h
@@ -193,7 +193,7 @@ namespace mtca4u {
           RegisterPath(const RegisterPath &_path) : path(_path.path) {removeExtraSeparators();}
 
           /** type conversion operators into std::string */
-          operator std::string() const { return path; }
+          operator const std::string&() const { return path; }
 
           /** / operator: add a new element to the path (without modifying this object) */
           RegisterPath operator/(const std::string &rightHandSide) const {
