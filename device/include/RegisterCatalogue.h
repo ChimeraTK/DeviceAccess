311210ce78fe9ccb00f59bcf597a22f47b92f927
mhier <mhier@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-03-10 11:04:16 +0000

    added iterator to iterate over registers in the catalogue
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/branches/register_plugins@2434 5f601a5c-88b9-47f3-b851-818f6741d00d

--- device/include/RegisterCatalogue.h
+++ device/include/RegisterCatalogue.h
@@ -21,11 +21,55 @@ namespace mtca4u {
     public:
     
       /** Get register information for a given full path name. */
-      boost::shared_ptr<RegisterInfo> getRegister(const RegisterPath& registerPathName);
+      boost::shared_ptr<RegisterInfo> getRegister(const RegisterPath& registerPathName) const;
     
       /** Add register information to the catalogue. The full path name of the register is taken from the
        *  RegisterInfo structure. */
       void addRegister(boost::shared_ptr<RegisterInfo> registerInfo);
+
+      /** Iterators for iterating through the registers in the catalogue */
+      class const_iterator {
+        public:
+          const_iterator& operator++() {    // ++it
+            ++iterator;
+            return *this;
+          }
+          const_iterator operator++(int) { // it++
+            const_iterator temp(*this);
+            ++iterator;
+            return temp;
+          }
+          const RegisterInfo& operator*() {
+            return *(iterator->second);
+          }
+          boost::shared_ptr<RegisterInfo> operator->() {
+            return iterator->second;
+          }
+          operator const boost::shared_ptr<RegisterInfo>&() {
+            return iterator->second;
+          }
+          bool operator==(const const_iterator &rightHandSide) {
+            return rightHandSide.iterator == iterator;
+          }
+          bool operator!=(const const_iterator &rightHandSide) {
+            return rightHandSide.iterator != iterator;
+          }
+        protected:
+          std::map< RegisterPath, boost::shared_ptr<RegisterInfo> >::const_iterator iterator;
+          friend class RegisterCatalogue;
+      };
+      
+      /** Return iterators for iterating through the registers in the catalogue */
+      const_iterator begin() const {
+        const_iterator it;
+        it.iterator = catalogue.cbegin();
+        return it;
+      }
+      const_iterator end() const {
+        const_iterator it;
+        it.iterator = catalogue.cend();
+        return it;
+      }
   
     protected:
     
