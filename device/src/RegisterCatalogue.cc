d392e7760a08c4cd9e98993dea9b6a4f17815ee9
mhier <mhier@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-03-10 11:03:54 +0000

    properly obtain the register from the map with checking if the register does not exist
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/branches/register_plugins@2433 5f601a5c-88b9-47f3-b851-818f6741d00d

--- device/src/RegisterCatalogue.cc
+++ device/src/RegisterCatalogue.cc
@@ -3,8 +3,14 @@
 
 namespace mtca4u {
 
-  boost::shared_ptr<RegisterInfo> RegisterCatalogue::getRegister(const RegisterPath& registerPathName) {
-    return catalogue[registerPathName];   /// @todo TODO catch non-existing registers
+  boost::shared_ptr<RegisterInfo> RegisterCatalogue::getRegister(const RegisterPath& registerPathName) const {
+    try {
+      return catalogue.at(registerPathName);
+    }
+    catch(std::out_of_range &e) {
+      throw DeviceException("Register '"+(registerPathName)+"' was not found in the catalogue ("+e.what()+").",
+          DeviceException::REGISTER_DOES_NOT_EXIST);
+    }
   }
     
   void RegisterCatalogue::addRegister(boost::shared_ptr<RegisterInfo> registerInfo) {
