a432cf7e0b0df0def3956a13ecf87f5b40f5fbd8
mhier <mhier@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-03-14 18:28:50 +0000

    fixed swap() being incorrectly implemented
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/trunk@2480 5f601a5c-88b9-47f3-b851-818f6741d00d

--- device_backends/include/LNMBackendBufferingRegisterAccessor.h
+++ device_backends/include/LNMBackendBufferingRegisterAccessor.h
@@ -85,14 +85,15 @@ namespace mtca4u {
       virtual iterator begin() { return _accessor.begin() + index_begin; }
       virtual const_iterator cbegin() const { return _accessor.cbegin() + index_begin; }
       virtual iterator end() { return _accessor.begin() + index_end; }
-      virtual const_iterator cend() const { return _accessor.cbegin() + _info.firstIndex + _info.length; }
+      virtual const_iterator cend() const { return _accessor.cbegin() + index_end; }
       virtual reverse_iterator rbegin() { return _accessor.rbegin() + index_rbegin; }
       virtual const_reverse_iterator crbegin() const { return _accessor.crbegin() + index_rbegin; }
       virtual reverse_iterator rend() { return _accessor.rbegin() + index_rend; }
       virtual const_reverse_iterator crend() const { return _accessor.crbegin() + index_rend; }
 
       virtual void swap(std::vector<T> &x) {
-        std::swap_ranges( x.begin(), x.end(), begin() );
+        x.resize(_info.length);
+        std::swap_ranges( begin(), end(), x.begin() );
       }
 
       virtual bool isSameRegister(const boost::shared_ptr<TransferElement const> &other) const {
