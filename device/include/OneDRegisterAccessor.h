69f4189c50d195124d0ac555fc4c29bccab4bc84
mhier <mhier@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-03-23 17:29:01 +0000

    added OneDRegisterAccessor
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/trunk@2573 5f601a5c-88b9-47f3-b851-818f6741d00d

---
+++ device/include/OneDRegisterAccessor.h
@@ -0,0 +1,108 @@
+/*
+ * OneDRegisterAccessor.h
+ *
+ *  Created on: Mar 23, 2016
+ *      Author: Martin Hierholzer <martin.hierholzer@desy.de>
+ */
+
+#ifndef MTCA4U_ONE_D_REGISTER_ACCESSOR_H
+#define MTCA4U_ONE_D_REGISTER_ACCESSOR_H
+
+#include "NDRegisterAccessorBridge.h"
+#include "DeviceException.h"
+
+namespace mtca4u {
+
+  /*********************************************************************************************************************/
+  /** Accessor class to read and write registers transparently by using the accessor object like a vector of the
+   *  type UserType. Conversion to and from the UserType will be handled by the FixedPointConverter matching the
+   *  register description in the map. Obtain the accessor using the Device::getBufferingRegisterAccessor() function.
+   *
+   *  Note: Transfers between the device and the internal buffer need to be triggered using the read() and write()
+   *  functions before reading from resp. after writing to the buffer using the operators.
+   */
+  template<typename UserType>
+  class OneDRegisterAccessor : public  NDRegisterAccessorBridge<UserType> {
+    public:
+
+      /** Constructer. @attention Do not normally use directly.
+       *  Users should call Device::getOneDRegisterAccessor() to obtain an instance instead. */
+      OneDRegisterAccessor(boost::shared_ptr< NDRegisterAccessor<UserType> > impl)
+      : NDRegisterAccessorBridge<UserType>(impl)
+      {
+        if(NDRegisterAccessorBridge<UserType>::_impl->getNumberOfChannels() != 1) {
+          throw DeviceException("The OneDRegisterAccessor has a too low dimension to access this register.",
+              DeviceException::WRONG_ACCESSOR);
+        }
+      }
+
+      /** Placeholder constructer, to allow late initialisation of the accessor, e.g. in the open function.
+       *  @attention Accessors created with this constructors will be dysfunctional, calling any member function
+       *  will throw an exception (by the boost::shared_ptr)! */
+      OneDRegisterAccessor() {}
+
+
+      /** Read the data from the device, convert it and store in buffer. */
+      void read() {
+        NDRegisterAccessorBridge<UserType>::_impl->read();
+      }
+
+      /** Convert data from the buffer and write to device. */
+      void write() {
+        NDRegisterAccessorBridge<UserType>::_impl->write();
+      }
+
+      /** Get or set buffer content by [] operator.
+       *  @attention No bounds checking is performed, use getNumberOfElements() to obtain the number of elements in
+       *  the register.
+       *  Note: Using the iterators is slightly more efficient than using this operator! */
+      UserType& operator[](unsigned int sample) {
+        return NDRegisterAccessorBridge<UserType>::_impl->accessData(0,sample);
+      }
+
+      /** Return number of elements / samples in the register */
+      unsigned int getNumberOfSamples() {
+        return NDRegisterAccessorBridge<UserType>::_impl->getNumberOfSamples();
+      }
+
+      /** Access data with std::vector-like iterators */
+      typedef typename std::vector<UserType>::iterator iterator;
+      typedef typename std::vector<UserType>::const_iterator const_iterator;
+      typedef typename std::vector<UserType>::reverse_iterator reverse_iterator;
+      typedef typename std::vector<UserType>::const_reverse_iterator const_reverse_iterator;
+      iterator begin() { return NDRegisterAccessorBridge<UserType>::_impl->accessChannel(0).begin(); }
+      const_iterator begin() const { return NDRegisterAccessorBridge<UserType>::_impl->accessChannel(0).cbegin(); }
+      const_iterator cbegin() const { return NDRegisterAccessorBridge<UserType>::_impl->accessChannel(0).cbegin(); }
+      iterator end() { return NDRegisterAccessorBridge<UserType>::_impl->accessChannel(0).end(); }
+      const_iterator end() const { return NDRegisterAccessorBridge<UserType>::_impl->accessChannel(0).cend(); }
+      const_iterator cend() const { return NDRegisterAccessorBridge<UserType>::_impl->accessChannel(0).cend(); }
+      reverse_iterator rbegin() { return NDRegisterAccessorBridge<UserType>::_impl->accessChannel(0).rbegin(); }
+      const_reverse_iterator rbegin() const { return NDRegisterAccessorBridge<UserType>::_impl->accessChannel(0).crbegin(); }
+      const_reverse_iterator crbegin() const { return NDRegisterAccessorBridge<UserType>::_impl->accessChannel(0).crbegin(); }
+      reverse_iterator rend() { return NDRegisterAccessorBridge<UserType>::_impl->accessChannel(0).rend(); }
+      const_reverse_iterator rend() const { return NDRegisterAccessorBridge<UserType>::_impl->accessChannel(0).crend(); }
+      const_reverse_iterator crend() const { return NDRegisterAccessorBridge<UserType>::_impl->accessChannel(0).crend(); }
+
+      /* Swap content of (cooked) buffer with std::vector */
+      void swap(std::vector<UserType> &x) {
+        NDRegisterAccessorBridge<UserType>::_impl->accessChannel(0).swap(x);
+      }
+
+      /** Return if the register accessor allows only reading */
+      bool isReadOnly() const {
+        return NDRegisterAccessorBridge<UserType>::_impl->isReadOnly();
+      }
+
+      /** Return if the accessor is properly initialised. It is initialised if it was constructed passing the pointer
+       *  to an implementation (a NDRegisterAccessor), it is not initialised if it was constructed only using the
+       *  placeholder constructor without arguments. */
+      bool isInitialised() const {
+        return NDRegisterAccessorBridge<UserType>::_impl != NULL;
+      }
+
+      friend class TransferGroup;
+  };
+
+}    // namespace mtca4u
+
+#endif /* MTCA4U_ONE_D_REGISTER_ACCESSOR_H */
---
+++ tests/executables_src/testOneDRegisterAccessor.cpp
@@ -0,0 +1,162 @@
+#include <algorithm>
+#include <math.h>
+#include <boost/test/included/unit_test.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include "Device.h"
+#include "OneDRegisterAccessor.h"
+#include "DMapFileDefaults.h"
+#include "BackendFactory.h"
+
+#include "accessPrivateData.h"
+
+using namespace boost::unit_test_framework;
+using namespace mtca4u;
+
+// we need to access the private implementation of the accessor (see accessPrivateData.h)
+struct OneDRegisterAccessor_double_impl {
+    typedef boost::shared_ptr< NDRegisterAccessor<double> >(NDRegisterAccessorBridge<double>::*type);
+};
+template class accessPrivateData::stow_private<OneDRegisterAccessor_double_impl, &mtca4u::NDRegisterAccessorBridge<double>::_impl>;
+
+/**********************************************************************************************************************/
+class OneDRegisterTest {
+  public:
+    OneDRegisterTest(){
+      device = boost::shared_ptr<Device>( new Device() );
+      device->open("DUMMYD1");
+    }
+
+    /// test the register accessor
+    void testRegisterAccessor();
+
+  private:
+    boost::shared_ptr<Device> device;
+    friend class OneDRegisterTestSuite;
+
+
+};
+
+/**********************************************************************************************************************/
+class  OneDRegisterTestSuite : public test_suite {
+  public:
+    OneDRegisterTestSuite() : test_suite("OneDRegisterAccessor test suite") {
+      BackendFactory::getInstance().setDMapFilePath(TEST_DMAP_FILE_PATH);
+      boost::shared_ptr<OneDRegisterTest> bufferingRegisterTest( new OneDRegisterTest );
+
+      add( BOOST_CLASS_TEST_CASE( &OneDRegisterTest::testRegisterAccessor, bufferingRegisterTest ) );
+    }};
+
+/**********************************************************************************************************************/
+test_suite* init_unit_test_suite( int /*argc*/, char* /*argv*/ [] )
+{
+  framework::master_test_suite().p_name.value = "DummyRegister test suite";
+  framework::master_test_suite().add(new OneDRegisterTestSuite);
+
+  return NULL;
+}
+
+
+/**********************************************************************************************************************/
+void OneDRegisterTest::testRegisterAccessor() {
+  std::cout << "testRegisterAccessor" << std::endl;
+
+  // obtain register accessor with integral type
+  OneDRegisterAccessor<int> intRegister = device->getOneDRegisterAccessor<int>("APP0/MODULE0");
+
+  // variable to read to for comparison
+  int compare;
+
+  // check number of elements getter
+  BOOST_CHECK( intRegister.getNumberOfSamples() == 2 );
+
+  // test operator[] on r.h.s.
+  compare = 5;
+  device->writeReg("MODULE0","APP0", &compare, sizeof(int), 0);
+  compare = -77;
+  device->writeReg("MODULE0","APP0", &compare, sizeof(int), sizeof(int));
+  intRegister.read();
+  BOOST_CHECK( intRegister[0] == 5 );
+  BOOST_CHECK( intRegister[1] == -77 );
+
+  // test operator[] on l.h.s.
+  intRegister[0] = -666;
+  intRegister[1] = 999;
+  intRegister.write();
+  device->readReg("MODULE0","APP0", &compare, sizeof(int), 0);
+  BOOST_CHECK( compare == -666 );
+  device->readReg("MODULE0","APP0", &compare, sizeof(int), sizeof(int));
+  BOOST_CHECK( compare == 999 );
+
+  // test iterators with begin and end
+  int ic = 0;
+  for(OneDRegisterAccessor<int>::iterator it = intRegister.begin(); it != intRegister.end(); ++it) {
+    *it = 1000*(ic+1);
+    ic++;
+  }
+  intRegister.write();
+  device->readReg("MODULE0","APP0", &compare, sizeof(int), 0);
+  BOOST_CHECK( compare == 1000 );
+  device->readReg("MODULE0","APP0", &compare, sizeof(int), sizeof(int));
+  BOOST_CHECK( compare == 2000 );
+
+  // test iterators with rbegin and rend
+  ic = 0;
+  for(OneDRegisterAccessor<int>::reverse_iterator it = intRegister.rbegin(); it != intRegister.rend(); ++it) {
+    *it = 333*(ic+1);
+    ic++;
+  }
+  intRegister.write();
+  device->readReg("MODULE0","APP0", &compare, sizeof(int), 0);
+  BOOST_CHECK( compare == 666 );
+  device->readReg("MODULE0","APP0", &compare, sizeof(int), sizeof(int));
+  BOOST_CHECK( compare == 333 );
+
+  // test const iterators in both directions
+  compare = 1234;
+  device->writeReg("MODULE0","APP0", &compare, sizeof(int), 0);
+  compare = 2468;
+  device->writeReg("MODULE0","APP0", &compare, sizeof(int), sizeof(int));
+  intRegister.read();
+  const OneDRegisterAccessor<int> const_intRegister = intRegister;
+  ic = 0;
+  for(OneDRegisterAccessor<int>::const_iterator it = const_intRegister.begin(); it != const_intRegister.end(); ++it) {
+    BOOST_CHECK( *it == 1234*(ic+1) );
+    ic++;
+  }
+  ic = 0;
+  for(OneDRegisterAccessor<int>::const_reverse_iterator it = const_intRegister.rbegin(); it != const_intRegister.rend(); ++it) {
+    BOOST_CHECK( *it == 1234*(2-ic) );
+    ic++;
+  }
+
+  // test swap with std::vector
+  std::vector<int> x(2);
+  x[0] = 11;
+  x[1] = 22;
+  intRegister.swap(x);
+  BOOST_CHECK( x[0] == 1234 );
+  BOOST_CHECK( x[1] == 2468 );
+  BOOST_CHECK( intRegister[0] == 11 );
+  BOOST_CHECK( intRegister[1] == 22 );
+
+  // obtain register accessor with fractional type, to check if fixed-point conversion is working (3 fractional bits)
+  OneDRegisterAccessor<double> floatRegister = device->getOneDRegisterAccessor<double>("MODULE0/WORD_USER1");
+
+  // test operator[] on r.h.s.
+  compare = -120;
+  device->writeReg("WORD_USER1","MODULE0", &compare, sizeof(int), 0);
+  floatRegister.read();
+  BOOST_CHECK( floatRegister[0] == -120./8. );
+
+  // test operator[] on l.h.s.
+  floatRegister[0] = 42. / 8.;
+  floatRegister.write();
+  device->readReg("WORD_USER1","MODULE0", &compare, sizeof(int), 0);
+  BOOST_CHECK( compare == 42 );
+
+}
