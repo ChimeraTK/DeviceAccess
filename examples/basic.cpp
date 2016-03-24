135bb83e2b4f7744758eaa05f45b4a23f1fdcc50
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-03-24 17:01:35 +0000

    improved basic scalar example
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/trunk@2598 5f601a5c-88b9-47f3-b851-818f6741d00d

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -25,23 +25,25 @@ int main(){
    *  names separated by '/'. In this is example it is Module/Register.
    *  In this basic example we use a register which contains a single value
    *  (a scalar).
+   *
+   *  The example device has a temperature controller with a set value.
    */
-    mtca4u::ScalarRegisterAccessor<float> accessor
-    = myDevice.getScalarRegisterAccessor<float>("BOARD/WORD_USER");
+    mtca4u::ScalarRegisterAccessor<float> temperatureSetPoint
+    = myDevice.getScalarRegisterAccessor<float>("TEMPERATURE_CONTROLLER/SET_POINT");
 
   /* To get the value from the device call read.
    */
-  accessor.read();
+  temperatureSetPoint.read();
 
-  /* Now you can treat the accessor as if it was a float.
+  /* Now you can treat the accessor as if it was a regular float variable.
    */
-  std::cout << "Data as float is " << accessor << std::endl;
-  accessor += 1.5;
-  std::cout << "Data now is  " << accessor << std::endl;
+  std::cout << "Current temperature set point is " << temperatureSetPoint << std::endl;
+  temperatureSetPoint += 1.5;
+  std::cout << "Temperature set point changed to " << temperatureSetPoint << std::endl;
 
   /* After you are done manipulating the accessor write it to the hardware.
    */
-  accessor.write();
+  temperatureSetPoint.write();
 
   /* It is good style to close the device when you are done, although
    * this would happen automatically once the device goes out of scope.
