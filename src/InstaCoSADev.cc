66a7ad991fba0baaad0f686cce24fb123a7d00c2
mhier <mhier@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-04-01 15:25:25 +0000

    added a first test implementation
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/InstaCoSADev/trunk@2637 5f601a5c-88b9-47f3-b851-818f6741d00d

---
+++ src/InstaCoSADev.cc
@@ -0,0 +1,76 @@
+/*
+ * InstaCoSADev.cc
+ *
+ *  Created on: Mar 29, 2016
+ *      Author: Martin Hierholzer
+ */
+
+#include <map>
+#include <string>
+
+#include <mtca4u/Device.h>
+#include <ControlSystemAdapter/ProcessScalar.h>
+#include <ControlSystemAdapter/ProcessArray.h>
+
+#include "InstaCoSADev.h"
+
+namespace mtca4u {
+
+  /*******************************************************************************************************************/
+
+  class InstaCoSADev_impl {
+    // only allow access by InstaCoSADev
+    private:
+      friend class InstaCoSADev;
+
+      InstaCoSADev_impl() {};
+
+      // pointer to the process variable manager
+      boost::shared_ptr<mtca4u::DevicePVManager> processVariableManager;
+
+      // map for double-typed scalars
+      std::map< std::string, std::pair< ScalarRegisterAccessor<double>, boost::shared_ptr< ProcessScalar<double> > > > scalarDoubleMap;
+
+      // map for int-typed scalars
+      std::map< std::string, std::pair< ScalarRegisterAccessor<int>, boost::shared_ptr< ProcessScalar<int> > > > scalarIntMap;
+
+  };
+
+  /*******************************************************************************************************************/
+
+  InstaCoSADev::InstaCoSADev(boost::shared_ptr<mtca4u::DevicePVManager> const &processVariableManager) {
+    impl.reset(new InstaCoSADev_impl());
+    impl->processVariableManager = processVariableManager;
+  }
+
+  /*******************************************************************************************************************/
+
+  void InstaCoSADev::addModule(mtca4u::Device &device, const mtca4u::RegisterPath &module, const std::string &pvBaseName) {
+    auto catalogue = device.getRegisterCatalogue();
+    for(auto &reg : catalogue) {
+      mtca4u::RegisterPath regName = reg.getRegisterName();
+      if(!regName.startsWith(std::string(module)+"/")) continue;  // wrong module
+      std::string baseName = std::string(regName).substr(module.length());
+      std::string pvName = mtca4u::RegisterPath(pvBaseName) / baseName;
+      if(reg.getNumberOfDimensions() == 0) {
+        ScalarRegisterAccessor<int> accessor = device.getScalarRegisterAccessor<int>(reg.getRegisterName());
+        boost::shared_ptr< ProcessScalar<int> > pv = impl->processVariableManager->createProcessScalar<int>(mtca4u::deviceToControlSystem, pvName);
+        impl->scalarIntMap[baseName] = std::make_pair(accessor,pv);
+      }
+      else if(reg.getNumberOfDimensions() == 1) {
+        std::cout << "HIER 1" << std::endl;
+        //throw std::string("not yet implemented.");
+      }
+      else {
+        std::cout << "HIER >1" << std::endl;
+        //throw std::string("2D register found in module "+module+" but not supported by InstaCoSADev."); // TODO @todo replace by proper exception
+      }
+    }
+  }
+
+  /*******************************************************************************************************************/
+
+  void InstaCoSADev::transferData(mtca4u::SynchronizationDirection direction) {
+  }
+
+} /* namespace mtca4u */
