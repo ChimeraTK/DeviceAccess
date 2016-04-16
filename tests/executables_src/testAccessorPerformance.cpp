d31e8ce4c895190333556cefd7ae90a965678835
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2016-04-16 22:57:59 +0000

    extended performance test with write, readRaw and writeRaw. The raw stuff is still slow.
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/deviceaccess/branches/accessor_performace@2726 5f601a5c-88b9-47f3-b851-818f6741d00d

--- tests/executables_src/testAccessorPerformance.cpp
+++ tests/executables_src/testAccessorPerformance.cpp
@@ -16,6 +16,22 @@ int main(){
       accessor->read<int>();
   }
 
+  std::cout <<" writing " << std::endl;
+  for(int i= 0; i < 100000; ++i){
+      accessor->write<int>(i);
+  }
+
+  std::cout <<" reading raw " << std::endl;
+  for(int i= 0; i < 100000; ++i){
+    int buffer;
+    accessor->readRaw(&buffer,sizeof(int));
+  }
+
+  std::cout <<" writing raw" << std::endl;
+  for(int i= 0; i < 100000; ++i){
+    accessor->writeRaw(&i, sizeof(int));
+  }
+
   auto accessor2 = device.getRegisterAccessor("AREA_DMAABLE","ADC");
   for(int i= 0; i < 25; ++i){
       int buffer;
