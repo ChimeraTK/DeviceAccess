c1611380a86fcb38f8fc49e8ce3a776f3f787580
Martin Hierholzer <martin.hierholzer@desy.de>
2016-07-04 11:44:20 +0200

    removed a redundant and unused function

--- include/Accessor.h
+++ include/Accessor.h
@@ -30,11 +30,6 @@ namespace ChimeraTK {
     public:
       virtual ~AccessorBase() {}
 
-      /** Check if this accessor is a output or input variable, from the point-of-view of the ApplicationModule
-       *  owning the instance of the Accessor. A feeding accessor feeds values to its peers and thus is an output
-       *  accessor. */
-      virtual bool isFeeding() = 0;
-
       /** Return if the accessor is properly initialised. It is initialised if it was constructed passing the pointer
        *  to an implementation (a NDRegisterAccessor), it is not initialised if it was constructed only using the
        *  placeholder constructor without arguments. */
@@ -91,8 +86,6 @@ namespace ChimeraTK {
         owner->registerAccessor(this);
       }
 
-      virtual bool isFeeding();
-
       VariableDirection getDirection() const {return _direction;}
 
       UpdateMode getUpdateMode() const {return _mode;}
@@ -130,13 +123,6 @@ namespace ChimeraTK {
       VariableNetworkNode node;
 
 
-  };
-
-  /*********************************************************************************************************************/
-
-  template< typename UserType >
-  bool Accessor<UserType>::isFeeding() {
-    return _direction == VariableDirection::feeding;
   };
 
 } /* namespace ChimeraTK */
