7850f2cbc886c05ab17b9e89cf3244c839e73a40
Martin Hierholzer <martin.hierholzer@desy.de>
2016-08-15 17:49:22 +0200

    replace boost::chrono by gettimeofday, since boost:chrono is not even available by default on Ubuntu 12.04

--- tests/executables_src/testAccessorPerformance.cpp
+++ tests/executables_src/testAccessorPerformance.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <fstream>
 
-#include <boost/chrono.hpp>
-using namespace boost::chrono;
+#include <sys/time.h>
 
 #include "Device.h"
 #include "Utilities.h"
@@ -18,9 +17,9 @@ using namespace mtca4u;
  * acceptable also on slower machines in debug build mode).
  *
 */
-int main(int argc, char **argv){
-  steady_clock::time_point t0;
-  duration<double> tdur;
+int main(int argc, char **argv) {
+  struct timeval tv;
+  int64_t t0, tdur;
 
   setDMapFilePath("dummies.dmap");
 
@@ -44,26 +43,30 @@ int main(int argc, char **argv){
   std::cout <<" Tests with the OneDRegisterAccessor:" << std::endl;
 
   auto acc1D = device.getOneDRegisterAccessor<int>("ADC/AREA_DMA_VIA_DMA");
-  t0 = steady_clock::now();
+  gettimeofday(&tv,NULL);
+  t0 = tv.tv_sec * 1000000 + tv.tv_usec;
   std::cout <<" reading block ";
   for(int i= 0; i < niterBlock; ++i){
       acc1D.read();
       sum += acc1D[i];
   }
-  tdur = steady_clock::now()-t0;
-  std::cout << "took " << tdur.count()*1000./niterBlock << " ms per block" << std::endl;
-  fresult << "1D_COOKEDus=" << std::round(tdur.count()*1000000./niterBlock) << std::endl;
+  gettimeofday(&tv,NULL);
+  tdur = ( tv.tv_sec * 1000000 + tv.tv_usec ) - t0;
+  std::cout << "took " << (double)tdur/1000./niterBlock << " ms per block" << std::endl;
+  fresult << "1D_COOKEDus=" << std::round((double)tdur/niterBlock) << std::endl;
 
   auto acc1Draw = device.getOneDRegisterAccessor<int>("ADC/AREA_DMA_VIA_DMA",0,0, {AccessMode::raw});
-  t0 = steady_clock::now();
+  gettimeofday(&tv,NULL);
+  t0 = tv.tv_sec * 1000000 + tv.tv_usec;
   std::cout <<" raw-reading block ";
   for(int i= 0; i < niterBlock; ++i){
     acc1Draw.read();
       sum += acc1Draw[i];
   }
-  tdur = steady_clock::now()-t0;
-  std::cout << "took " << tdur.count()*1000./niterBlock << " ms per block" << std::endl;
-  fresult << "1D_RAWus=" << std::round(tdur.count()*1000000./niterBlock) << std::endl;
+  gettimeofday(&tv,NULL);
+  tdur = ( tv.tv_sec * 1000000 + tv.tv_usec ) - t0;
+  std::cout << "took " << (double)tdur/1000./niterBlock << " ms per block" << std::endl;
+  fresult << "1D_RAWus=" << std::round((double)tdur/niterBlock) << std::endl;
 
   std::cout <<" ***************************************************************************" << std::endl;
   std::cout <<" Tests with the compatibility RegisterAccessor:" << std::endl;
@@ -72,62 +75,68 @@ int main(int argc, char **argv){
 
   std::cout <<" reading ";
   sum += accessor->read<int>();
-  t0 = steady_clock::now();
+  gettimeofday(&tv,NULL);
+  t0 = tv.tv_sec * 1000000 + tv.tv_usec;
   for(int i= 0; i < niter; ++i){
     sum += accessor->read<int>();
   }
-  tdur = steady_clock::now()-t0;
-  std::cout << "took " << tdur.count()*1000000./niter << " us per word" << std::endl;
-  fresult << "RACOMPAT_READns=" << std::round(tdur.count()*1000000000./niter) << std::endl;
+  gettimeofday(&tv,NULL);
+  tdur = ( tv.tv_sec * 1000000 + tv.tv_usec ) - t0;
+  std::cout << "took " << (double)tdur/niter << " us per word" << std::endl;
+  fresult << "RACOMPAT_READns=" << std::round(tdur*1000./niter) << std::endl;
 
   std::cout <<" writing ";
-  t0 = steady_clock::now();
+  gettimeofday(&tv,NULL);
+  t0 = tv.tv_sec * 1000000 + tv.tv_usec;
   for(int i= 0; i < niter; ++i){
     accessor->write<int>(i);
   }
-  tdur = steady_clock::now()-t0;
-  std::cout << "took " << tdur.count()*1000000./niter << " us per word" << std::endl;
-  fresult << "RACOMPAT_WRITEns=" << std::round(tdur.count()*1000000000./niter) << std::endl;
+  gettimeofday(&tv,NULL);
+  tdur = ( tv.tv_sec * 1000000 + tv.tv_usec ) - t0;
+  std::cout << "took " << (double)tdur/niter << " us per word" << std::endl;
+  fresult << "RACOMPAT_WRITEns=" << std::round(tdur*1000./niter) << std::endl;
 
   std::cout <<" reading raw ";
-  t0 = steady_clock::now();
+  gettimeofday(&tv,NULL);
+  t0 = tv.tv_sec * 1000000 + tv.tv_usec;
   for(int i= 0; i < niter; ++i){
     int buffer;
     accessor->readRaw(&buffer,sizeof(int));
     sum += buffer;
   }
-  tdur = steady_clock::now()-t0;
-  std::cout << "took " << tdur.count()*1000000./niter << " us per word" << std::endl;
-  fresult << "RACOMPAT_READRAWns=" << std::round(tdur.count()*1000000000./niter) << std::endl;
+  gettimeofday(&tv,NULL);
+  tdur = ( tv.tv_sec * 1000000 + tv.tv_usec ) - t0;
+  std::cout << "took " << (double)tdur/niter << " us per word" << std::endl;
+  fresult << "RACOMPAT_READRAWns=" << std::round(tdur*1000./niter) << std::endl;
 
   std::cout <<" writing raw ";
-  t0 = steady_clock::now();
+  gettimeofday(&tv,NULL);
+  t0 = tv.tv_sec * 1000000 + tv.tv_usec;
   for(int i= 0; i < niter; ++i){
     accessor->writeRaw(&i, sizeof(int));
   }
-  tdur = steady_clock::now()-t0;
-  std::cout << "took " << tdur.count()*1000000./niter << " us per word" << std::endl;
-  fresult << "RACOMPAT_WRITERAWns=" << std::round(tdur.count()*1000000000./niter) << std::endl;
+  gettimeofday(&tv,NULL);
+  tdur = ( tv.tv_sec * 1000000 + tv.tv_usec ) - t0;
+  std::cout << "took " << (double)tdur/niter << " us per word" << std::endl;
+  fresult << "RACOMPAT_WRITERAWns=" << std::round(tdur*1000./niter) << std::endl;
 
   auto accessor2 = device.getRegisterAccessor("AREA_DMAABLE","ADC");
-  t0 = steady_clock::now();
+  gettimeofday(&tv,NULL);
+  t0 = tv.tv_sec * 1000000 + tv.tv_usec;
   std::cout <<" reading moving word in block ";
   for(int i= 0; i < niterBlock; ++i){
       int buffer;
       accessor2->read<int>(&buffer, 1, i);
       sum += buffer;
   }
-  tdur = steady_clock::now()-t0;
-  std::cout << "took " << tdur.count()*1000./niterBlock << " ms per transfer" << std::endl;
-  fresult << "RACOMPAT_READMOVINGWORDus=" << std::round(tdur.count()*1000000./niterBlock) << std::endl;
+  gettimeofday(&tv,NULL);
+  tdur = ( tv.tv_sec * 1000000 + tv.tv_usec ) - t0;
+  std::cout << "took " << (double)tdur/niterBlock << " us per transfer" << std::endl;
+  fresult << "RACOMPAT_READMOVINGWORDus=" << std::round((double)tdur/niterBlock) << std::endl;
 
 
   std::cout <<" ***************************************************************************" << std::endl;
-  t0 = steady_clock::now();
   std::cout << " Sum of all read data: " << sum << std::endl;
-  tdur = steady_clock::now()-t0;
-  std::cout << " Printing the sum took " << tdur.count()*1000. << " ms" << std::endl;
-  std::cout <<" ***************************************************************************" << std::endl;
 
   fresult.close();
 
