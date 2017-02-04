322bdfb418cfb841b0b3312b53d138c23cd6c17f
Martin Killenberg <martin.killenberg@desy.de>
2017-02-04 01:49:42 +0100

    excluded a few lines in a test that can never be reached from the coverage  report

--- tests/BackendTestLibs/WrongVersionBackend.cc
+++ tests/BackendTestLibs/WrongVersionBackend.cc
@@ -5,12 +5,14 @@
 using namespace mtca4u;
 #define WRONG_VERSION "00.18"
 
+// LCOV_EXCL_START these lines cannot be reached because the backend cannot be registered, thus it is never instantiated
 struct WrongVersionBackend : public DummyBackend{
   using DummyBackend::DummyBackend;
-  
   static boost::shared_ptr<DeviceBackend> createInstance(std::string /*host*/, std::string instance, std::list<std::string> parameters, std::string /*mapFileName*/){
     return returnInstance<WrongVersionBackend>(instance, convertPathRelativeToDmapToAbs(parameters.front()));
   }
+
+// LCOV_EXCL_STOP The registerern and the version functions have to be called
 
   struct BackendRegisterer{
     BackendRegisterer(){
