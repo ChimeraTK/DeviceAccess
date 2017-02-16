3d3c9c37aef1418fa847767176e8d4d4d6ff3637
Martin Hierholzer <martin.hierholzer@desy.de>
2017-02-16 14:30:23 +0100

    fixed typos

--- tests/executables_src/testTrigger.cc
+++ tests/executables_src/testTrigger.cc
@@ -81,16 +81,16 @@ template<typename T>
 struct TestModule : public ctk::ApplicationModule {
     TestModule(ctk::EntityOwner *owner, const std::string &name) : ctk::ApplicationModule(owner,name) {}
 
-    ctk::ScalarPushInput<T> consumingPush{this, "consumingPush", "MV/m", "Descrption"};
-    ctk::ScalarPushInput<T> consumingPush2{this, "consumingPush2", "MV/m", "Descrption"};
-    ctk::ScalarPushInput<T> consumingPush3{this, "consumingPush3", "MV/m", "Descrption"};
+    ctk::ScalarPushInput<T> consumingPush{this, "consumingPush", "MV/m", "Description"};
+    ctk::ScalarPushInput<T> consumingPush2{this, "consumingPush2", "MV/m", "Description"};
+    ctk::ScalarPushInput<T> consumingPush3{this, "consumingPush3", "MV/m", "Description"};
 
-    ctk::ScalarPollInput<T> consumingPoll{this, "consumingPoll", "MV/m", "Descrption"};
-    ctk::ScalarPollInput<T> consumingPoll2{this, "consumingPoll2", "MV/m", "Descrption"};
-    ctk::ScalarPollInput<T> consumingPoll3{this, "consumingPoll3", "MV/m", "Descrption"};
+    ctk::ScalarPollInput<T> consumingPoll{this, "consumingPoll", "MV/m", "Description"};
+    ctk::ScalarPollInput<T> consumingPoll2{this, "consumingPoll2", "MV/m", "Description"};
+    ctk::ScalarPollInput<T> consumingPoll3{this, "consumingPoll3", "MV/m", "Description"};
 
-    ctk::ScalarOutput<T> theTrigger{this, "theTrigger", "MV/m", "Descrption"};
-    ctk::ScalarOutput<T> feedingToDevice{this, "feedingToDevice", "MV/m", "Descrption"};
+    ctk::ScalarOutput<T> theTrigger{this, "theTrigger", "MV/m", "Description"};
+    ctk::ScalarOutput<T> feedingToDevice{this, "feedingToDevice", "MV/m", "Description"};
 
     void mainLoop() {}
 };
