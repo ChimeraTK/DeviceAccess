9bc8ef1df6e837abf57cd9fe0db17e8c8ff78287
Martin Killenberg <martin.killenberg@desy.de>
2017-03-25 08:37:00 +0100

    moved implementation of testableRebotSleep into a separate compilation unit to avoid inlining

--- device_backends/src/testableRebotSleep.cc
+++ device_backends/src/testableRebotSleep.cc
@@ -1,20 +1,19 @@
-#ifndef TESTABLE_REBOT_SLEEP_H
-#define TESTABLE_REBOT_SLEEP_H
-
-#include <boost/thread.hpp>
-#include <boost/chrono.hpp>
-
+#include "testableRebotSleep.h"
 
 namespace ChimeraTK{
   namespace testable_rebot_sleep{
-    boost::chrono::steady_clock::time_point now();
+     boost::chrono::steady_clock::time_point now(){
+      return  boost::chrono::steady_clock::now();
+    }
+   
 
     /** There are two implementations with the same signature:
      *  One that calls boost::thread::this_thread::sleep_until, which is used in the application.
      *  The one for testing has a lock and is synchronised manually with the test thread.
      */
-    void sleep_until(boost::chrono::steady_clock::time_point t);
+    void sleep_until(boost::chrono::steady_clock::time_point t){
+      boost::this_thread::sleep_until(t);
+    }
   }
 }
 
-#endif// TESTABLE_REBOT_SLEEP_H
