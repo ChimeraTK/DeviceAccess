69b105a20904770b6f5cf9bcd9cac470bd0b6f6a
Martin Killenberg <martin.killenberg@desy.de>
2017-06-29 13:52:43 +0200

    extended the tests for raw accessor

--- tests/executables_src/testNumericAddressedBackendRegisterAccessor.cpp
+++ tests/executables_src/testNumericAddressedBackendRegisterAccessor.cpp
@@ -5,6 +5,7 @@
 using namespace boost::unit_test_framework;
 
 #include "Device.h"
+#include "TransferGroup.h"
 using namespace mtca4u;
 
 // Create a test suite which holds all your tests.
@@ -98,6 +99,127 @@ BOOST_AUTO_TEST_CASE( testRawWrite ){
   } 
   
 }
+
+BOOST_AUTO_TEST_CASE( testRawWithTransferGroup ){
+  Device device;
+  device.open("sdm://./dummy=goodMapFile.map");
+
+  // the whole register
+  auto a1 = device.getOneDRegisterAccessor<int>("MODULE1/TEST_AREA", 2, 0,  {AccessMode::raw});
+  auto a2 = device.getOneDRegisterAccessor<int>("MODULE1/TEST_AREA", 2, 2,  {AccessMode::raw});
+
+  // the whole register in a separate accessor which is not in the group
+  auto standalone = device.getOneDRegisterAccessor<int>("MODULE1/TEST_AREA", 0, 0,  {AccessMode::raw});
+
+  // start with a single accessor so the low level transfer element is not shared
+  TransferGroup group;
+  group.addAccessor(a1);
+
+  for (auto & value : a1){
+    value = 0x77;
+  }
+  group.write();
+  
+  standalone.read();
+  BOOST_CHECK(standalone[0] == 0x77);
+  BOOST_CHECK(standalone[1] == 0x77);
+
+  // check that the swapping works as intended
+  for (auto & value : a1){
+    value = 0xFF;
+  }
+
+  //writing twice without modifying the buffer certainly has to work
+  //In case the old values have accidentally been swapped out and not back in this is not the case, which would be a bug
+  for (int i=0; i<2; ++i){
+    group.write();
+    // writing must not swap away the buffer
+    for (auto & value : a1){
+      BOOST_CHECK(value == 0xFF);
+    }
+    standalone.read();
+    BOOST_CHECK(standalone[0] == 0xFF);
+    BOOST_CHECK(standalone[1] == 0xFF);
+  }
+
+  //test reading and mixed reading/writing
+  standalone[0] = 0xAA;
+  standalone[1] = 0xAA;
+  standalone.write();
+  
+  for (int i=0; i<2; ++i){
+    group.read();
+    for (auto & value : a1){
+      BOOST_CHECK(value == 0xAA);
+    }
+  }
+
+  standalone[0] = 0xAB;
+  standalone[1] = 0xAB;
+  standalone.write();
+
+  group.read();
+  group.write();
+  for (auto & value : a1){
+    BOOST_CHECK(value == 0xAB);
+  }
+    
+  standalone.read();
+  BOOST_CHECK(standalone[0] == 0xAB);
+  BOOST_CHECK(standalone[1] == 0xAB);
+
+  //initialise the words pointed to by a2
+  for (auto & value : a2){
+    value = 0x77;
+  }
+  a2.write();
+  
+  // Now add the second accessor of the same register to the group and repeat the tests
+  // They will share the same low level transfer element.
+  group.addAccessor(a2);
+  for (auto & value : a1){
+    value = 0xFD;
+  }
+  for (auto & value : a2){
+    value = 0xFE;
+  }
+
+  for (int i=0; i<2; ++i){
+    group.write();
+    for (auto & value : a1){
+      BOOST_CHECK(value == 0xFD);
+    }
+    for (auto & value : a2){
+      BOOST_CHECK(value == 0xFE);
+    }
+    standalone.read();
+    BOOST_CHECK(standalone[0] == 0xFD);
+    BOOST_CHECK(standalone[1] == 0xFD);
+    BOOST_CHECK(standalone[2] == 0xFE);
+    BOOST_CHECK(standalone[3] == 0xFE);
+  }
+
+  standalone[0] = 0xA1;
+  standalone[1] = 0xA2;
+  standalone[2] = 0xA3;
+  standalone[3] = 0xA4;
+  standalone.write();
+
+  group.read();
+  group.write();
+  BOOST_CHECK(a1[0] == 0xA1);
+  BOOST_CHECK(a1[1] == 0xA2); 
+  BOOST_CHECK(a2[0] == 0xA3);
+  BOOST_CHECK(a2[1] == 0xA4);
+   
+  standalone.read();
+  BOOST_CHECK(standalone[0] == 0xA1);
+  BOOST_CHECK(standalone[1] == 0xA2);
+  BOOST_CHECK(standalone[2] == 0xA3);
+  BOOST_CHECK(standalone[3] == 0xA4);
+
+}
+
 
 // After you finished all test you have to end the test suite.
 BOOST_AUTO_TEST_SUITE_END()
