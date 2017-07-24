9c41f24bfd116378f07775f89ec80bab22935ab8
Martin Hierholzer <martin.hierholzer@desy.de>
2017-07-24 15:22:33 +0200

    made the VersionNumberSource static, so version numbers are unique within the process

--- device/include/VersionNumberSource.h
+++ device/include/VersionNumberSource.h
@@ -9,41 +9,40 @@
 namespace ChimeraTK {
 
   /**
-   * Source generating version numbers. Version numbers are used to resolve
-   * competing updates that are applied to the same process variable. For
-   * example, it they can help in breaking an infinite update loop that might
-   * occur when two process variables are related and update each other.
-   *
-   * Each version number returned by the same source is unique, but different
-   * sources may return the same version numbers.
+   * Source generating version numbers. Version numbers are used to resolve competing updates that are applied to the
+   * same process variable. For example, it they can help in breaking an infinite update loop that might occur when
+   * two process variables are related and update each other.
+   * 
+   * They are also used to determine the order of updates made to different process variables, e.g. to make sure that
+   * TransferElement::readAny() always returns the oldest change first.
+   * 
+   * The class only has static members, so there is no need to create instances of this class.
    */
   class VersionNumberSource {
 
-  public:
-    /**
-     * Shared pointer to this type.
-     */
-    typedef boost::shared_ptr<VersionNumberSource> SharedPtr;
+    public:
 
-    /**
-     * Returns the next version number. The next version number is determined in
-     * an atomic way, so that it is guaranteed that this method never returns
-     * the same version number twice (unless the counter overflows, which is
-     * very unlikely). The first version number returned by this method is one.
-     * The version number that is returned is guaranteed to be greater than the
-     * version numbers returned for earlier calls to this method. This method
-     * may safely be called by any thread without any synchronization.
-     */
-    inline VersionNumber nextVersionNumber() {
-      return ++_lastReturnedVersionNumber;
-    }
+      /**
+      * Returns the next version number. The next version number is determined in an atomic way, so that it is
+      * guaranteed that this method never returns the same version number twice (unless the counter overflows, which
+      * is very unlikely). The first version number returned by this method is one. The version number that is
+      * returned is guaranteed to be greater than the version numbers returned for earlier calls to this method. This
+      * method may safely be called by any thread without any synchronization.
+      */
+      static VersionNumber nextVersionNumber() {
+        return ++_lastReturnedVersionNumber;
+      }
 
-  private:
-    /**
-     * Last version number that was returned by a call to
-     * {@link nextVersionNumber()}.
-     */
-    std::atomic<VersionNumber::UnderlyingDataType> _lastReturnedVersionNumber;
+    private:
+      
+      /** No instances needed. */
+      VersionNumberSource();
+
+      /**
+      * Last version number that was returned by a call to
+      * {@link nextVersionNumber()}.
+      */
+      static std::atomic<VersionNumber::UnderlyingDataType> _lastReturnedVersionNumber;
 
   };
 
