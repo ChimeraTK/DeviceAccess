c09e096240b7ef79576043a3198ad86683e42093
Martin Hierholzer <martin.hierholzer@desy.de>
2017-11-20 09:55:35 +0100

    added test for group-wise read and write operations

--- tests/executables_src/testVariableGroup.cc
+++ tests/executables_src/testVariableGroup.cc
@@ -64,11 +64,154 @@ struct TestApplication : public ctk::Application {
 };
 
 /*********************************************************************************************************************/
+/* test module-wide read/write operations */
+
+BOOST_AUTO_TEST_CASE( testModuleReadWrite ) {
+  std::cout << "*** testModuleReadWrite" << std::endl;
+
+  TestApplication app;
+
+  app.testModule.feedingPush >> app.testModule.mixedGroup.consumingPush;
+  app.testModule.feedingPush2 >> app.testModule.mixedGroup.consumingPush2;
+  app.testModule.feedingPush3 >> app.testModule.mixedGroup.consumingPush3;
+  app.testModule.feedingPoll >> app.testModule.mixedGroup.consumingPoll;
+  app.testModule.feedingPoll2 >> app.testModule.mixedGroup.consumingPoll2;
+  app.testModule.feedingPoll3 >> app.testModule.mixedGroup.consumingPoll3;
+  app.initialise();
+
+  // single theaded test
+  app.testModule.mixedGroup.consumingPush = 666;
+  app.testModule.mixedGroup.consumingPush2 = 666;
+  app.testModule.mixedGroup.consumingPush3 = 666;
+  app.testModule.mixedGroup.consumingPoll = 666;
+  app.testModule.mixedGroup.consumingPoll2 = 666;
+  app.testModule.mixedGroup.consumingPoll3 = 666;
+  app.testModule.feedingPush = 18;
+  app.testModule.feedingPush2 = 20;
+  app.testModule.feedingPush3 = 22;
+  app.testModule.feedingPoll = 23;
+  app.testModule.feedingPoll2 = 24;
+  app.testModule.feedingPoll3 = 27;
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush == 666);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush2 == 666);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush3 == 666);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll == 666);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll2 == 666);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll3 == 666);
+  app.testModule.writeAll();
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush == 666);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush2 == 666);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush3 == 666);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll == 666);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll2 == 666);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll3 == 666);
+  app.testModule.readAll();
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush == 18);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush2 == 20);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush3 == 22);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll == 23);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll2 == 24);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll3 == 27);
+
+  app.testModule.readAllNonBlocking();
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush == 18);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush2 == 20);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush3 == 22);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll == 23);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll2 == 24);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll3 == 27);
+
+  app.testModule.feedingPush2 = 30;
+  app.testModule.feedingPoll2 = 33;
+  app.testModule.writeAll();
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush == 18);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush2 == 20);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush3 == 22);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll == 23);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll2 == 24);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll3 == 27);
+  app.testModule.readAllNonBlocking();
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush == 18);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush2 == 30);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush3 == 22);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll == 23);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll2 == 33);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll3 == 27);
+  app.testModule.readAllNonBlocking();
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush == 18);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush2 == 30);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush3 == 22);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll == 23);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll2 == 33);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll3 == 27);
+
+  app.testModule.feedingPush = 35;
+  app.testModule.feedingPoll3 = 40;
+  app.testModule.writeAll();
+  app.testModule.feedingPush = 36;
+  app.testModule.feedingPoll3 = 44;
+  app.testModule.writeAll();
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush == 18);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush2 == 30);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush3 == 22);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll == 23);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll2 == 33);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll3 == 27);
+  app.testModule.readAllNonBlocking();
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush == 35);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush2 == 30);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush3 == 22);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll == 23);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll2 == 33);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll3 == 44);
+  app.testModule.readAllNonBlocking();
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush == 36);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush2 == 30);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush3 == 22);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll == 23);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll2 == 33);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll3 == 44);
+  app.testModule.readAllNonBlocking();
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush == 36);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush2 == 30);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush3 == 22);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll == 23);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll2 == 33);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll3 == 44);
+
+  app.testModule.feedingPush = 45;
+  app.testModule.writeAll();
+  app.testModule.feedingPush = 46;
+  app.testModule.writeAll();
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush == 36);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush2 == 30);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush3 == 22);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll == 23);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll2 == 33);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll3 == 44);
+  app.testModule.readAllLatest();
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush == 46);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush2 == 30);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush3 == 22);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll == 23);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll2 == 33);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll3 == 44);
+  app.testModule.readAllLatest();
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush == 46);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush2 == 30);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPush3 == 22);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll == 23);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll2 == 33);
+  BOOST_CHECK(app.testModule.mixedGroup.consumingPoll3 == 44);
+
+}
+
+/*********************************************************************************************************************/
 /* test trigger by app variable when connecting a polled device register to an app variable */
 
-BOOST_AUTO_TEST_CASE( testMixedGroup ) {
+BOOST_AUTO_TEST_CASE( testReadAny ) {
   std::cout << "*********************************************************************************************************************" << std::endl;
-  std::cout << "==> testMixedGroup" << std::endl;
+  std::cout << "==> testReadAny" << std::endl;
   
   TestApplication app;
   
