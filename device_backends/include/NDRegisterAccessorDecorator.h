3ab8970afd5119963e7469816c922546543525d1
Martin Hierholzer <martin.hierholzer@desy.de>
2017-11-24 11:41:08 +0100

    Allow using the NDRegisterAccessorDecorator also if the target accessor has a different UserType.

--- device_backends/include/NDRegisterAccessorDecorator.h
+++ device_backends/include/NDRegisterAccessorDecorator.h
@@ -14,30 +14,90 @@
 
 namespace mtca4u {
 
+  namespace detail {
+
+    /** Intermediate class just to make implementations of postRead/preWrite/postWrite depending on whether
+     *  TargetUserType is equal or unequal to UserType. Default implementations for these functions are provided only
+     *  in case TargetUserType is equal to UserType. The functions must be implemented by the actual decorator, if the
+     *  types are unequal. Otherwise they still can be implemented, but the default provides a useful behavior for many
+     *  cases (buffers are simply swapped). */
+    template<typename UserType, typename TargetUserType=UserType>
+    class NDRegisterAccessorDecoratorImpl : public NDRegisterAccessor<UserType> {
+
+      public:
+
+        using NDRegisterAccessor<UserType>::NDRegisterAccessor;
+
+        void postRead() override = 0;
+
+        void preWrite() override = 0;
+
+        void postWrite() override = 0;
+
+      protected:
+
+        /// The accessor to be decorated
+        boost::shared_ptr<mtca4u::NDRegisterAccessor<TargetUserType>> _target;
+
+    };
+
+    template<typename UserType>
+    class NDRegisterAccessorDecoratorImpl<UserType,UserType> : public NDRegisterAccessor<UserType> {
+
+      public:
+
+        using NDRegisterAccessor<UserType>::NDRegisterAccessor;
+
+        void postRead() override {
+          _target->postRead();
+          for(size_t i=0; i<_target->getNumberOfChannels(); ++i) buffer_2D[i].swap(_target->accessChannel(i));
+        }
+
+        void preWrite() override {
+          for(size_t i=0; i<_target->getNumberOfChannels(); ++i) buffer_2D[i].swap(_target->accessChannel(i));
+        }
+
+        void postWrite() override {
+          for(size_t i=0; i<_target->getNumberOfChannels(); ++i) buffer_2D[i].swap(_target->accessChannel(i));
+        }
+
+      protected:
+
+        using mtca4u::NDRegisterAccessor<UserType>::buffer_2D;
+
+        /// The accessor to be decorated
+        boost::shared_ptr<mtca4u::NDRegisterAccessor<UserType>> _target;
+
+    };
+
+  }
+
   /** Base class for decorators of the NDRegisterAccessor. This is basically an empty decorator, so implementations can
    *  easily extend by overriding only single functions (and usually calling the implementations of this class inside
    *  the overrides). */
   template<typename UserType, typename TargetUserType=UserType>
-  class NDRegisterAccessorDecorator : public NDRegisterAccessor<UserType> {
+  class NDRegisterAccessorDecorator : public detail::NDRegisterAccessorDecoratorImpl<UserType,TargetUserType> {
 
     public:
 
       NDRegisterAccessorDecorator(const boost::shared_ptr<mtca4u::NDRegisterAccessor<TargetUserType>> &target)
-      : NDRegisterAccessor<UserType>(target->getName(), target->getUnit(), target->getDescription()),
-        _target(target)
+      :  detail::NDRegisterAccessorDecoratorImpl<UserType,TargetUserType>( target->getName(), target->getUnit(),
+                                                                           target->getDescription()              )
       {
+        _target = target;
+
         // set ID to match the decorated accessor
         this->_id = _target->getId();
 
         // initialise buffers
         buffer_2D.resize(_target->getNumberOfChannels());
-        for(size_t i=0; i<_target->getNumberOfChannels(); ++i) buffer_2D[i] = _target->accessChannel(i);
+        for(size_t i=0; i<_target->getNumberOfChannels(); ++i) buffer_2D[i].resize(_target->getNumberOfSamples());
       }
 
       bool write(ChimeraTK::VersionNumber versionNumber={}) override {
-        preWrite();
+        this->preWrite();
         bool dataLost = _target->write(versionNumber);
-        postWrite();
+        this->postWrite();
         return dataLost;
       }
 
@@ -67,18 +127,6 @@ namespace mtca4u {
 
       void transferFutureWaitCallback() override {
         _target->transferFutureWaitCallback();
-      }
-
-      void postRead() override {
-        throw std::logic_error("You must implement this function, if UserType is unequal TargetUserType.");
-      }
-
-      void preWrite() override {
-        throw std::logic_error("You must implement this function, if UserType is unequal TargetUserType.");
-      }
-
-      void postWrite() override {
-        throw std::logic_error("You must implement this function, if UserType is unequal TargetUserType.");
       }
 
       void clearAsyncTransferActive() override {
@@ -122,121 +170,8 @@ namespace mtca4u {
 
       using mtca4u::NDRegisterAccessor<UserType>::buffer_2D;
 
-      /// The accessor to be decorated
-      boost::shared_ptr<mtca4u::NDRegisterAccessor<TargetUserType>> _target;
+      using detail::NDRegisterAccessorDecoratorImpl<UserType,TargetUserType>::_target;
 
-  };
-
-  /** Partial template specialisation for TargetUserType = UserType
-   *  @todo is there any other way than copying basically the entire code??? */
-  template<typename UserType>
-  class NDRegisterAccessorDecorator<UserType, UserType> : public NDRegisterAccessor<UserType> {
-
-    public:
-
-      NDRegisterAccessorDecorator(const boost::shared_ptr<mtca4u::NDRegisterAccessor<UserType>> &target)
-      : NDRegisterAccessor<UserType>(target->getName(), target->getUnit(), target->getDescription()),
-        _target(target)
-      {
-        // set ID to match the decorated accessor
-        this->_id = _target->getId();
-
-        // initialise buffers
-        buffer_2D.resize(_target->getNumberOfChannels());
-        for(size_t i=0; i<_target->getNumberOfChannels(); ++i) buffer_2D[i] = _target->accessChannel(i);
-      }
-
-      bool write(ChimeraTK::VersionNumber versionNumber={}) override {
-        preWrite();
-        bool dataLost = _target->write(versionNumber);
-        postWrite();
-        return dataLost;
-      }
-
-      void doReadTransfer() override {
-        _target->doReadTransfer();
-      }
-
-      bool doReadTransferNonBlocking() override {
-        return _target->doReadTransferNonBlocking();
-      }
-
-      bool doReadTransferLatest() override {
-        return _target->doReadTransferLatest();
-      }
-
-      TransferFuture readAsync() override {
-        return TransferFuture(_target->readAsync(), this);
-      }
-
-      bool asyncTransferActive() override {
-        return _target->asyncTransferActive();
-      }
-
-      void preRead() override {
-        _target->preRead();
-      }
-
-      void transferFutureWaitCallback() override {
-        _target->transferFutureWaitCallback();
-      }
-
-      void postRead() override {
-        _target->postRead();
-        for(size_t i=0; i<_target->getNumberOfChannels(); ++i) buffer_2D[i].swap(_target->accessChannel(i));
-      }
-
-      void preWrite() override {
-        for(size_t i=0; i<_target->getNumberOfChannels(); ++i) buffer_2D[i].swap(_target->accessChannel(i));
-      }
-
-      void postWrite() override {
-        for(size_t i=0; i<_target->getNumberOfChannels(); ++i) buffer_2D[i].swap(_target->accessChannel(i));
-      }
-
-      void clearAsyncTransferActive() override {
-        _target->clearAsyncTransferActive();
-      }
-
-      bool isSameRegister(const boost::shared_ptr<mtca4u::TransferElement const> &other) const override {
-        return _target->isSameRegister(other);
-      }
-
-      bool isReadOnly() const override {
-        return _target->isReadOnly();
-      }
-
-      bool isReadable() const override {
-        return _target->isReadable();
-      }
-
-      bool isWriteable() const override {
-        return _target->isWriteable();
-      }
-
-      std::vector< boost::shared_ptr<mtca4u::TransferElement> > getHardwareAccessingElements() override {
-        return _target->getHardwareAccessingElements();
-      }
-
-      void replaceTransferElement(boost::shared_ptr<TransferElement> newElement) override {
-        if(_target->isSameRegister(newElement)) {
-          _target = boost::static_pointer_cast< NDRegisterAccessor<UserType> >(newElement);
-        }
-        else {
-        _target->replaceTransferElement(newElement);
-        }
-      }
-
-      void setPersistentDataStorage(boost::shared_ptr<ChimeraTK::PersistentDataStorage> storage) override {
-        _target->setPersistentDataStorage(storage);
-      }
-
-    protected:
-
-      using mtca4u::NDRegisterAccessor<UserType>::buffer_2D;
-
-      /// The accessor to be decorated
-      boost::shared_ptr<mtca4u::NDRegisterAccessor<UserType>> _target;
   };
 
 }
