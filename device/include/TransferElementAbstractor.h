ChimeraTK/DeviceAccess

commit 4eedf5b6604a843b15117ee2e515bc546b1bbd3d

Transforming the TransferElementAbstractor into a non-virtual class, since this should in principle not be neccessary.
NOTE: Tests are still failing, debugging needed!

device/include/TransferElementAbstractor.h
@@ -48,25 +48,22 @@ namespace ChimeraTK {
       /** Create an uninitialised abstractor - just for late initialisation */
       TransferElementAbstractor() {}
 
-      /** Abstract base classes need a virtual destructor. */
-      virtual ~TransferElementAbstractor() {}
-
       /** Returns the name that identifies the process variable. */
-      const std::string& getName() const { return _implUntyped->getName(); }
+      const std::string& getName() const { return _impl->getName(); }
 
       /** Returns the engineering unit. If none was specified, it will default to "n./a." */
-      const std::string& getUnit() const { return _implUntyped->getUnit(); }
+      const std::string& getUnit() const { return _impl->getUnit(); }
 
       /** Returns the description of this variable/register */
-      const std::string& getDescription() const { return _implUntyped->getDescription(); }
+      const std::string& getDescription() const { return _impl->getDescription(); }
 
       /** Returns the \c std::type_info for the value type of this transfer element.
        *  This can be used to determine the type at runtime. */
-      const std::type_info& getValueType() const { return _implUntyped->getValueType(); }
+      const std::type_info& getValueType() const { return _impl->getValueType(); }
 
       /** Read the data from the device. If AccessMode::wait_for_new_data was set, this function will block until new
        *  data has arrived. Otherwise it still might block for a short time until the data transfer was complete. */
-      void read() { _implUntyped->read(); }
+      void read() { _impl->read(); }
 
       /** Read the next value, if available in the input buffer.
        *
@@ -77,12 +74,12 @@ namespace ChimeraTK {
        *  quickly. Depending on the actual transfer implementation, the backend might need to transfer data to obtain
        *  the current value before returning. Also this function is not guaranteed to be lock free. The return value
        *  will be always true in this mode. */
-      bool readNonBlocking() { return _implUntyped->readNonBlocking(); }
+      bool readNonBlocking() { return _impl->readNonBlocking(); }
 
       /** Read the latest value, discarding any other update since the last read if present. Otherwise this function
        *  is identical to readNonBlocking(), i.e. it will never wait for new values and it will return whether a
        *  new value was available if AccessMode::wait_for_new_data is set. */
-      bool readLatest() { return _implUntyped->readLatest(); }
+      bool readLatest() { return _impl->readLatest(); }
 
       /** Read data from the device in the background and return a future which will be fulfilled when the data is
        *  ready. When the future is fulfilled, the transfer element will already contain the new data, there is no
@@ -112,28 +109,28 @@ namespace ChimeraTK {
        *  preRead() in their implementations of readAsync()!
        *
        *  Note: This feature is still experimental. Expect API changes without notice! */
-      TransferFuture readAsync() { return _implUntyped->readAsync(); }
+      TransferFuture readAsync() { return _impl->readAsync(); }
 
       /**
       * Returns the version number that is associated with the last transfer (i.e. last read or write). See
       * ChimeraTK::VersionNumber for details.
       */
-      ChimeraTK::VersionNumber getVersionNumber() const { return _implUntyped->getVersionNumber(); }
+      ChimeraTK::VersionNumber getVersionNumber() const { return _impl->getVersionNumber(); }
 
       /** Write the data to device. The return value is true, old data was lost on the write transfer (e.g. due to an
        *  buffer overflow). In case of an unbuffered write transfer, the return value will always be false. */
-      bool write(ChimeraTK::VersionNumber versionNumber={}) { return _implUntyped->write(versionNumber); }
+      bool write(ChimeraTK::VersionNumber versionNumber={}) { return _impl->write(versionNumber); }
 
       /** Check if transfer element is read only, i\.e\. it is readable but not writeable. */
-      bool isReadOnly() const { return _implUntyped->isReadOnly(); }
+      bool isReadOnly() const { return _impl->isReadOnly(); }
 
       /** Check if transfer element is readable. It throws an acception if you try to read and
        *  isReadable() is not true.*/
-      bool isReadable() const { return _implUntyped->isReadable(); }
+      bool isReadable() const { return _impl->isReadable(); }
 
       /** Check if transfer element is writeable. It throws an acception if you try to write and
        *  isWriteable() is not true.*/
-      bool isWriteable() const { return _implUntyped->isWriteable(); }
+      bool isWriteable() const { return _impl->isWriteable(); }
 
       /**
        *  Obtain the underlying TransferElements with actual hardware access. If this transfer element
@@ -143,7 +140,7 @@ namespace ChimeraTK {
        *  Note: Avoid using this in application code, since it will break the abstraction!
        */
       std::vector< boost::shared_ptr<TransferElement> > getHardwareAccessingElements() {
-        return _implUntyped->getHardwareAccessingElements();
+        return _impl->getHardwareAccessingElements();
       }
 
       /**
@@ -161,8 +158,8 @@ namespace ChimeraTK {
        *  Note: Avoid using this in application code, since it will break the abstraction!
        */
       std::list< boost::shared_ptr<TransferElement> > getInternalElements() {
-        auto result = _implUntyped->getInternalElements();
-        result.push_front(_implUntyped);
+        auto result = _impl->getInternalElements();
+        result.push_front(_impl);
         return result;
       }
 
@@ -173,21 +170,44 @@ namespace ChimeraTK {
        *
        *  Note: Avoid using this in application code, since it will break the abstraction!
        */
-      boost::shared_ptr<TransferElement> getHighLevelImplElement() { return _implUntyped; }
+      boost::shared_ptr<TransferElement> getHighLevelImplElement() { return _impl; }
 
       /** Return if the accessor is properly initialised. It is initialised if it was constructed passing the pointer
        *  to an implementation (a NDRegisterAccessor), it is not initialised if it was constructed only using the
        *  placeholder constructor without arguments. */
       bool isInitialised() const {
-        return _implUntyped != nullptr;
+        return _impl != nullptr;
+      }
+
+      /** Assign a new accessor to this TransferElementAbstractor. Since another TransferElementAbstractor is passed as
+       *  argument, both TransferElementAbstractor will then point to the same accessor and thus are sharing the
+       *  same buffer. To obtain a new copy of the accessor with a distinct buffer, the corresponding
+       *  getXXRegisterAccessor() function of Device must be called. */
+      void replace(const TransferElementAbstractor &newAccessor) {
+        _impl = newAccessor._impl;
+      }
+
+      /** Alternative signature of relace() with the same functionality, used when a pointer to the implementation
+       *  has been obtained directly (instead of a TransferElementAbstractor). */
+      void replace(boost::shared_ptr<TransferElement> newImpl) {
+        _impl = newImpl;
       }
 
       /**
-       *  Search for all underlying TransferElements which are considered identicel (see sameRegister()) with
+       *  Search for all underlying TransferElements which are considered identicel (see mayReplaceOther()) with
        *  the given TransferElement. These TransferElements are then replaced with the new element. If no underlying
        *  element matches the new element, this function has no effect.
        */
-      virtual void replaceTransferElement(boost::shared_ptr<TransferElement> newElement) = 0;
+      void replaceTransferElement(boost::shared_ptr<TransferElement> newElement) {
+        if(newElement->mayReplaceOther(_impl)) {
+          if(newElement != _impl) {
+            _impl = newElement;
+          }
+        }
+        else {
+          _impl->replaceTransferElement(newElement);
+        }
+      }
 
       /**
       *  Associate a persistent data storage object to be updated on each write operation of this ProcessArray. If no
@@ -198,8 +218,8 @@ namespace ChimeraTK {
       *  storage (e.g. read-only variables or device registers) @todo TODO does this make sense?
       */
       void setPersistentDataStorage(boost::shared_ptr<ChimeraTK::PersistentDataStorage> storage) {
-        _implUntyped->setPersistentDataStorage(storage);
-      };
+        _impl->setPersistentDataStorage(storage);
+      }
 
       /**
        * Obtain unique ID for this TransferElement. If this TransferElement is the abstractor side of the bridge, this
@@ -209,17 +229,17 @@ namespace ChimeraTK {
        * obtained by to difference calls to Device::getScalarRegisterAccessor() will have a different ID even when
        * accessing the very same register.
        */
-      TransferElementID getId() const { return _implUntyped->getId(); };
+      TransferElementID getId() const { return _impl->getId(); }
 
     protected:
 
       /** Construct from TransferElement implementation */
       explicit TransferElementAbstractor(boost::shared_ptr< TransferElement > impl)
-      : _implUntyped(impl)
+      : _impl(impl)
       {}
 
       /** Untyped pointer to implementation */
-      boost::shared_ptr< TransferElement > _implUntyped;
+      boost::shared_ptr< TransferElement > _impl;
   };
 
 } /* namespace ChimeraTK */

tests/executables_src/testLMapBackend.cpp
@@ -4,19 +4,11 @@
 #include "Device.h"
 #include "BufferingRegisterAccessor.h"
 
-#include "accessPrivateData.h"
-
 using namespace boost::unit_test_framework;
 namespace mtca4u{
   using namespace ChimeraTK;
 }
 using namespace mtca4u;
-
-// we need to access the private implementation of the accessor (see accessPrivateData.h)
-struct BufferingRegisterAccessor_int32t_impl {
-    typedef boost::shared_ptr< NDRegisterAccessor<int32_t> >(NDRegisterAccessorAbstractor<int32_t>::*type);
-};
-template struct accessPrivateData::stow_private<BufferingRegisterAccessor_int32t_impl, &mtca4u::NDRegisterAccessorAbstractor<int32_t>::_impl>;
 
 
 class LMapBackendTest {
@@ -212,9 +204,9 @@ void LMapBackendTest::testExceptions() {
     mtca4u::BufferingRegisterAccessor<int32_t> acc3 = device.getBufferingRegisterAccessor<int32_t>("","Constant2");
 
     boost::shared_ptr< NDRegisterAccessor<int32_t> > impl,impl2, impl3;
-    impl = acc.*accessPrivateData::stowed< BufferingRegisterAccessor_int32t_impl >::value;
-    impl2 = acc2.*accessPrivateData::stowed< BufferingRegisterAccessor_int32t_impl >::value;
-    impl3 = acc3.*accessPrivateData::stowed< BufferingRegisterAccessor_int32t_impl >::value;
+    impl = boost::dynamic_pointer_cast<NDRegisterAccessor<int32_t>>(acc.getHighLevelImplElement());
+    impl2 = boost::dynamic_pointer_cast<NDRegisterAccessor<int32_t>>(acc2.getHighLevelImplElement());
+    impl3 = boost::dynamic_pointer_cast<NDRegisterAccessor<int32_t>>(acc3.getHighLevelImplElement());
 
     //BOOST_CHECK( impl->mayReplaceOther(impl2) == true );    // this is currently always set to false, since it doesn't really make any difference...
     BOOST_CHECK( impl->mayReplaceOther(impl3) == false );
@@ -387,8 +379,8 @@ void LMapBackendTest::testRegisterAccessorForRegister() {
   mtca4u::BufferingRegisterAccessor<int32_t> acc2 = device.getBufferingRegisterAccessor<int32_t>("","PartOfArea");
 
   boost::shared_ptr< NDRegisterAccessor<int32_t> > impl,impl2;
-  impl = acc.*accessPrivateData::stowed< BufferingRegisterAccessor_int32t_impl >::value;
-  impl2 = acc2.*accessPrivateData::stowed< BufferingRegisterAccessor_int32t_impl >::value;
+  impl = boost::dynamic_pointer_cast<NDRegisterAccessor<int32_t>>(acc.getHighLevelImplElement());
+  impl2 = boost::dynamic_pointer_cast<NDRegisterAccessor<int32_t>>(acc.getHighLevelImplElement());
 
   BOOST_CHECK( impl->mayReplaceOther( impl ) == true );
   BOOST_CHECK( impl2->mayReplaceOther( impl ) == false );
@@ -554,9 +546,9 @@ void LMapBackendTest::testRegisterAccessorForRange() {
   mtca4u::BufferingRegisterAccessor<int32_t> acc3_2 = device.getBufferingRegisterAccessor<int32_t>("","Channel3");
 
   boost::shared_ptr< NDRegisterAccessor<int32_t> > impl3,impl4,impl3_2;
-  impl3 = acc3.*accessPrivateData::stowed< BufferingRegisterAccessor_int32t_impl >::value;
-  impl4 = acc4.*accessPrivateData::stowed< BufferingRegisterAccessor_int32t_impl >::value;
-  impl3_2 = acc3_2.*accessPrivateData::stowed< BufferingRegisterAccessor_int32t_impl >::value;
+  impl3 = boost::dynamic_pointer_cast<NDRegisterAccessor<int32_t>>(acc3.getHighLevelImplElement());
+  impl4 = boost::dynamic_pointer_cast<NDRegisterAccessor<int32_t>>(acc4.getHighLevelImplElement());
+  impl3_2 = boost::dynamic_pointer_cast<NDRegisterAccessor<int32_t>>(acc3_2.getHighLevelImplElement());
   BOOST_CHECK( impl3->mayReplaceOther( impl3_2 ) == true );
   BOOST_CHECK( impl3->mayReplaceOther( impl4 ) == false );
 

tests/executables_src/testTransferGroup.cpp
@@ -15,27 +15,7 @@ namespace mtca4u{
 }
 using namespace mtca4u;
 
-// we need to access the private implementation of the accessor (see accessPrivateData.h)
-struct BufferingRegisterAccessor_int_impl {
-    typedef boost::shared_ptr< NDRegisterAccessor<int> >(NDRegisterAccessorAbstractor<int>::*type);
-};
-template struct accessPrivateData::stow_private<BufferingRegisterAccessor_int_impl, &mtca4u::NDRegisterAccessorAbstractor<int>::_impl>;
-
-struct BufferingRegisterAccessor_int32_impl {
-    typedef boost::shared_ptr< NDRegisterAccessor<int32_t> >(NDRegisterAccessorAbstractor<int32_t>::*type);
-};
-template struct accessPrivateData::stow_private<BufferingRegisterAccessor_int32_impl, &mtca4u::NDRegisterAccessorAbstractor<int32_t>::_impl>;
-
-struct BufferingRegisterAccessor_uint16_impl {
-    typedef boost::shared_ptr< NDRegisterAccessor<uint16_t> >(NDRegisterAccessorAbstractor<uint16_t>::*type);
-};
-template struct accessPrivateData::stow_private<BufferingRegisterAccessor_uint16_impl, &mtca4u::NDRegisterAccessorAbstractor<uint16_t>::_impl>;
-
-struct BufferingRegisterAccessor_int64_impl {
-    typedef boost::shared_ptr< NDRegisterAccessor<int64_t> >(NDRegisterAccessorAbstractor<int64_t>::*type);
-};
-template struct accessPrivateData::stow_private<BufferingRegisterAccessor_int64_impl, &mtca4u::NDRegisterAccessorAbstractor<int64_t>::_impl>;
-
+// we need to access some private data of the low level transfer element
 struct NumericAddressedLowLevelTransferElement_startAddress {
     typedef size_t (NumericAddressedLowLevelTransferElement::*type);
 };
@@ -219,7 +199,7 @@ void TransferGroupTest::testLogicalNameMappedRegister() {
   // obtain the private pointers to the implementation of the accessor
   boost::shared_ptr< NDRegisterAccessor<int> > impl[6];
   for(int i=0; i<6; i++) {
-    impl[i] = a[i].*accessPrivateData::stowed<BufferingRegisterAccessor_int_impl>::value;
+    impl[i] = boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(a[i].getHighLevelImplElement());
   }
 
   // somewhat redundant check: underlying hardware accessors are different for all accessors
@@ -333,10 +313,10 @@ void TransferGroupTest::testMergeNumericRegisters() {
   auto mux3b = device.getScalarRegisterAccessor<int>("/ADC/WORD_CLK_MUX_3");
 
   // obtain the private pointers to the implementation of the accessor
-  auto mux0i = mux0.*accessPrivateData::stowed<BufferingRegisterAccessor_int_impl>::value;
-  auto mux1i = mux1.*accessPrivateData::stowed<BufferingRegisterAccessor_int_impl>::value;
-  auto mux2i = mux2.*accessPrivateData::stowed<BufferingRegisterAccessor_int_impl>::value;
-  auto mux3i = mux3.*accessPrivateData::stowed<BufferingRegisterAccessor_int_impl>::value;
+  auto mux0i = boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux0.getHighLevelImplElement());
+  auto mux1i = boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux1.getHighLevelImplElement());
+  auto mux2i = boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux2.getHighLevelImplElement());
+  auto mux3i = boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux3.getHighLevelImplElement());
 
   // check that all underlying raw accessors are still different
   BOOST_CHECK( mux0i->getHardwareAccessingElements()[0] != mux1i->getHardwareAccessingElements()[0] );
@@ -449,10 +429,10 @@ void TransferGroupTest::testMergeNumericRegistersDifferentTypes() {
   auto mux3b = device.getScalarRegisterAccessor<int64_t>("/ADC/WORD_CLK_MUX_3");
 
   // obtain the private pointers to the implementation of the accessor
-  auto mux0i = mux0.*accessPrivateData::stowed<BufferingRegisterAccessor_uint16_impl>::value;
-  auto mux1i = mux1.*accessPrivateData::stowed<BufferingRegisterAccessor_uint16_impl>::value;
-  auto mux2i = mux2.*accessPrivateData::stowed<BufferingRegisterAccessor_int32_impl>::value;
-  auto mux3i = mux3.*accessPrivateData::stowed<BufferingRegisterAccessor_int64_impl>::value;
+  auto mux0i = boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux0.getHighLevelImplElement());
+  auto mux1i = boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux1.getHighLevelImplElement());
+  auto mux2i = boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux2.getHighLevelImplElement());
+  auto mux3i = boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux3.getHighLevelImplElement());
 
   // check that all underlying raw accessors are still different
   BOOST_CHECK( mux0i->getHardwareAccessingElements()[0] != mux1i->getHardwareAccessingElements()[0] );
@@ -476,10 +456,10 @@ void TransferGroupTest::testMergeNumericRegistersDifferentTypes() {
   BOOST_CHECK( mux0i->getHardwareAccessingElements()[0] == mux3i->getHardwareAccessingElements()[0] );
 
   // also check that all high-level implementations are still the same as previously
-  BOOST_CHECK( mux0i == mux0.*accessPrivateData::stowed<BufferingRegisterAccessor_uint16_impl>::value );
-  BOOST_CHECK( mux1i == mux1.*accessPrivateData::stowed<BufferingRegisterAccessor_uint16_impl>::value );
-  BOOST_CHECK( mux2i == mux2.*accessPrivateData::stowed<BufferingRegisterAccessor_int32_impl>::value );
-  BOOST_CHECK( mux3i == mux3.*accessPrivateData::stowed<BufferingRegisterAccessor_int64_impl>::value );
+  BOOST_CHECK( mux0i == boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux0.getHighLevelImplElement()) );
+  BOOST_CHECK( mux1i == boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux1.getHighLevelImplElement()) );
+  BOOST_CHECK( mux2i == boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux2.getHighLevelImplElement()) );
+  BOOST_CHECK( mux3i == boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux3.getHighLevelImplElement()) );
 
   // check that reading and writing works
   mux0 = 42;

commit 7fd243b5e416e92b80c574c7ce25d79048d82b24

fixed the bugs introduced with commit 4eedf5b6604a843b15117ee2e515bc546b1bbd3d ("Transforming the TransferElementAbstractor into a non-virtual class, since this should in principle not be neccessary.")

device/include/TransferElementAbstractor.h
@@ -201,7 +201,7 @@ namespace ChimeraTK {
       void replaceTransferElement(boost::shared_ptr<TransferElement> newElement) {
         if(newElement->mayReplaceOther(_impl)) {
           if(newElement != _impl) {
-            _impl = newElement;
+            _impl = newElement->makeCopyRegisterDecorator();
           }
         }
         else {

device_backends/include/NDRegisterAccessor.h
@@ -85,6 +85,9 @@ namespace ChimeraTK {
       template<typename COOCKED_TYPE>
       void setAsCoocked_impl(unsigned int channel, unsigned int sample, COOCKED_TYPE value);
 
+      boost::shared_ptr<TransferElement> makeCopyRegisterDecorator() override;
+
+
       /** DEPRECATED DO NOT USE! Instead make a call to readNonBlocking() and check the return value.
        *  \deprecated This function is deprecated, remove it at some point!
        *
@@ -137,7 +140,7 @@ namespace ChimeraTK {
   COOCKED_TYPE NDRegisterAccessor<UserType>::getAsCoocked_impl(unsigned int /*channel*/, unsigned int /*sample*/){
     throw DeviceException("Reading as coocked is not available for this accessor", DeviceException::NOT_AVAILABLE);
   }
-  
+
   template<typename UserType> template<typename COOCKED_TYPE>
   void NDRegisterAccessor<UserType>::setAsCoocked(unsigned int channel, unsigned int sample, COOCKED_TYPE value){
     CALL_VIRTUAL_FUNCTION_TEMPLATE(setAsCoocked_impl, COOCKED_TYPE, channel, sample, value);
@@ -149,7 +152,7 @@ namespace ChimeraTK {
   }
 
   DECLARE_TEMPLATE_FOR_CHIMERATK_USER_TYPES(NDRegisterAccessor);
-  
-}
+
+} /* namespace ChimeraTK */
 
 #endif /* CHIMERA_TK_N_D_REGISTER_ACCESSOR_H */

device_backends/include/NumericAddressedLowLevelTransferElement.h
@@ -118,6 +118,11 @@ namespace ChimeraTK {
         isShared = true;
       }
 
+      boost::shared_ptr<TransferElement> makeCopyRegisterDecorator() override {                             // LCOV_EXCL_LINE
+        throw DeviceException("NumericAddressedLowLevelTransferElement::makeCopyRegisterDecorator() is not implemented",    // LCOV_EXCL_LINE
+                              DeviceException::NOT_IMPLEMENTED);                                            // LCOV_EXCL_LINE
+      }                                                                                                     // LCOV_EXCL_LINE
+
     protected:
 
       /** Set the start address (inside the bar given in the constructor) and number of words of this accessor. */

device_backends/include/TransferElement.h
@@ -422,6 +422,11 @@ namespace ChimeraTK {
         (void) newElement; // prevent warning
       }
 
+      /** Create a CopyRegisterDecorator of the right type decorating this TransferElement. This is used by
+       *  TransferElementAbstractor::replaceTransferElement() to decouple two accessors which are replaced on the
+       *  abstractor level. */
+      virtual boost::shared_ptr<TransferElement> makeCopyRegisterDecorator() = 0;
+
       /** Constant string to be used as a unit when the unit is not provided or known */
       static constexpr char unitNotSet[] = "n./a.";
 

device_backends/src/NDRegisterAccessor.cc
@@ -1,5 +1,17 @@
 #include "NDRegisterAccessor.h"
+#include "CopyRegisterDecorator.h"
 
 namespace ChimeraTK {
+
+  template<typename UserType>
+  boost::shared_ptr<TransferElement> NDRegisterAccessor<UserType>::makeCopyRegisterDecorator() {
+    auto casted = boost::static_pointer_cast<NDRegisterAccessor<UserType>>(enable_shared_from_this::shared_from_this());
+    return boost::make_shared<CopyRegisterDecorator<UserType>>(casted);
+  }
+
+
+/**********************************************************************************************************************/
+
   INSTANTIATE_TEMPLATE_FOR_CHIMERATK_USER_TYPES( NDRegisterAccessor );
-}
+
+} /* namespace ChimeraTK */

tests/executables_src/testLMapBackend.cpp
@@ -380,8 +380,9 @@ void LMapBackendTest::testRegisterAccessorForRegister() {
 
   boost::shared_ptr< NDRegisterAccessor<int32_t> > impl,impl2;
   impl = boost::dynamic_pointer_cast<NDRegisterAccessor<int32_t>>(acc.getHighLevelImplElement());
-  impl2 = boost::dynamic_pointer_cast<NDRegisterAccessor<int32_t>>(acc.getHighLevelImplElement());
+  impl2 = boost::dynamic_pointer_cast<NDRegisterAccessor<int32_t>>(acc2.getHighLevelImplElement());
 
+  BOOST_CHECK( impl != impl2 );
   BOOST_CHECK( impl->mayReplaceOther( impl ) == true );
   BOOST_CHECK( impl2->mayReplaceOther( impl ) == false );
   BOOST_CHECK( impl->mayReplaceOther( impl2 ) == false );

tests/executables_src/testTransferGroup.cpp
@@ -429,10 +429,10 @@ void TransferGroupTest::testMergeNumericRegistersDifferentTypes() {
   auto mux3b = device.getScalarRegisterAccessor<int64_t>("/ADC/WORD_CLK_MUX_3");
 
   // obtain the private pointers to the implementation of the accessor
-  auto mux0i = boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux0.getHighLevelImplElement());
-  auto mux1i = boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux1.getHighLevelImplElement());
-  auto mux2i = boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux2.getHighLevelImplElement());
-  auto mux3i = boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux3.getHighLevelImplElement());
+  auto mux0i = boost::dynamic_pointer_cast<NDRegisterAccessor<uint16_t>>(mux0.getHighLevelImplElement());
+  auto mux1i = boost::dynamic_pointer_cast<NDRegisterAccessor<uint16_t>>(mux1.getHighLevelImplElement());
+  auto mux2i = boost::dynamic_pointer_cast<NDRegisterAccessor<int32_t>>(mux2.getHighLevelImplElement());
+  auto mux3i = boost::dynamic_pointer_cast<NDRegisterAccessor<int64_t>>(mux3.getHighLevelImplElement());
 
   // check that all underlying raw accessors are still different
   BOOST_CHECK( mux0i->getHardwareAccessingElements()[0] != mux1i->getHardwareAccessingElements()[0] );
@@ -456,10 +456,10 @@ void TransferGroupTest::testMergeNumericRegistersDifferentTypes() {
   BOOST_CHECK( mux0i->getHardwareAccessingElements()[0] == mux3i->getHardwareAccessingElements()[0] );
 
   // also check that all high-level implementations are still the same as previously
-  BOOST_CHECK( mux0i == boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux0.getHighLevelImplElement()) );
-  BOOST_CHECK( mux1i == boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux1.getHighLevelImplElement()) );
-  BOOST_CHECK( mux2i == boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux2.getHighLevelImplElement()) );
-  BOOST_CHECK( mux3i == boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(mux3.getHighLevelImplElement()) );
+  BOOST_CHECK( mux0i == boost::dynamic_pointer_cast<NDRegisterAccessor<uint16_t>>(mux0.getHighLevelImplElement()) );
+  BOOST_CHECK( mux1i == boost::dynamic_pointer_cast<NDRegisterAccessor<uint16_t>>(mux1.getHighLevelImplElement()) );
+  BOOST_CHECK( mux2i == boost::dynamic_pointer_cast<NDRegisterAccessor<int32_t>>(mux2.getHighLevelImplElement()) );
+  BOOST_CHECK( mux3i == boost::dynamic_pointer_cast<NDRegisterAccessor<int64_t>>(mux3.getHighLevelImplElement()) );
 
   // check that reading and writing works
   mux0 = 42;