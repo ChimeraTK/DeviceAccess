46bc038ca09e02ef4624ad6403e6b295936bc402
Martin Hierholzer <martin.hierholzer@desy.de>
2018-05-28 14:39:14 +0200

    define interface of the new ReadAnyGroup - implementation still missing

--- device/include/ReadAny.h
+++ device/include/ReadAny.h
@@ -8,126 +8,72 @@
 #ifndef CHIMERATK_READ_ANY_H
 #define CHIMERATK_READ_ANY_H
 
+#include <ChimeraTK/cppext/future_queue.hpp>
+
 #include "TransferElementAbstractor.h"
 
 namespace ChimeraTK {
 
-  /** Read data asynchronously from all given TransferElements and wait until one of the TransferElements has
-    *  new data. The ID of the TransferElement which received new data is returned as a reference. In case multiple
-    *  TransferElements receive new data simultaneously (or already have new data available before the call to
-    *  readAny()), the ID of the TransferElement with the oldest VersionNumber (see getVersionNumber()) will be returned
-    *  by this function. This ensures that data is received in the order of sending (unless data is "dated back"
-    *  and sent with an older version number, which might be the case e.g. when using the ControlSystemAdapter).
-    *
-    *  Note that the behaviour is undefined when putting the same TransferElement into the list - a result might
-    *  be e.g. that it blocks for ever. This is due to a limitation in the underlying boost::wait_for_any()
-    *  function. */
-  ChimeraTK::TransferElementID readAny(std::list<std::reference_wrapper<ChimeraTK::TransferElementAbstractor>> elementsToRead);
-  ChimeraTK::TransferElementID readAny(std::list<std::reference_wrapper<ChimeraTK::TransferElement>> elementsToRead);
+  /** Group several TransferElements to allow waiting for an update of any of the TransferElements. The TransferElements
+   *  must be created with the AccessModeFlag::wait_for_new_data and must be readable. */
+  class ReadAnyGroup {
 
-/*******************************************************************************************************************/
-#if 0
-  namespace detail {
+    public:
 
-    // Note: the %iterator in the third line prevents doxygen from creating a link which it cannot resolve.
-    // It reads: std::list<TransferFuture::PlainFutureType>::iterator
-    /** Internal class needed for TransferElement::readAny(): Provide a wrapper around the list iterator to effectivly
-    *  allow passing std::list<TransferFuture>::iterator to boost::wait_for_any() which otherwise expects e.g.
-    *  std::list<TransferFuture::PlainFutureType>::%iterator. This class provides the same interface as an interator of
-    *  the expected type but adds the function getTransferFuture(), so we can obtain the TransferElement from the
-    *  return value of boost::wait_for_any(). */
-    class TransferFutureIterator {
-      public:
-        TransferFutureIterator(const std::list<TransferFuture>::iterator &it) : _it(it) {}
-        TransferFutureIterator operator++() { TransferFutureIterator ret(_it); ++_it; return ret; }
-        TransferFutureIterator operator++(int) { ++_it; return *this; }
-        bool operator!=(const TransferFutureIterator &rhs) {return _it != rhs._it;}
-        bool operator==(const TransferFutureIterator &rhs) {return _it == rhs._it;}
-        TransferFuture::PlainFutureType& operator*() {return _it->getBoostFuture();}
-        TransferFuture::PlainFutureType& operator->() {return _it->getBoostFuture();}
-        TransferFuture& getTransferFuture() const {return *_it;}
-      private:
-        std::list<TransferFuture>::iterator _it;
-    };
+      /** Construct empty group */
+      ReadAnyGroup();
 
-  } // namespace detail
+      /** Add TransferElement to group. Note that calling this function is only allowed before finalise() has been
+       *  called. The given TransferElement may not yet be part of a ReadAnyGroup or a TransferGroup, otherwise an
+       *  exception is thrown. */
+      void add(TransferElementAbstractor &element);
 
-  } /* namespace ChimeraTK */
+      /** Finalise the group. From this point on, add() may no longer be called. Only after the group has been finalised
+       *  the read functions of this group may be called. Also, after the group has been finalised, read functions may
+       *  no longer be called directly on the participating elements (including other copies of the same element).
+       *
+       *  The order of update notifications will only be well-defined for updates which happen after the call to
+       *  finalise(). Any unread values which are present in the TransferElements when this function is called will not
+       *  be processed in the correct sequence. Only the sequence within each TransferElement can be guaranteed. For any
+       *  updates which arrive after the call to finalise() the correct sequence will be guaranteed even accross
+       *  TransferElements. */
+      void finalise();
 
-/*******************************************************************************************************************/
+      /** Wait until one of the elements in this group has received an update. The function will return the
+       *  TransferElementID of the element which has received the update. If multiple updates are received at the same
+       *  time or if multiple updates were already present before the call to this function, the ID of the first element
+       *  receiving an update will be returned.
+       *
+       *  Before returning, the postRead action will be called on the TransferElement whose ID is returned, so the read
+       *  data will already be present in the user buffer. All other TransferElements in this group will not be
+       *  altered. */
+      TransferElementID waitAny();
 
-namespace std {
+      /** Wait until the given TransferElement has received an update and store it to its user buffer. All updates of
+       *  other elements which are received before the update of the given element will be processed and are thus
+       *  visible in the user buffers when this function returns.
+       *
+       *  The specified TransferElement must be part of this ReadAnyGroup, otherwise an exception is thrown.
+       *
+       *  This is merely a convenience function calling waitAny() in a loop until the ID of the given element is
+       *  returned. */
+      void waitUntil(TransferElementAbstractor &element);
+      void waitUntil(TransferElementID id);
 
-  template<>
-  struct iterator_traits<ChimeraTK::detail::TransferFutureIterator> {
-      typedef ChimeraTK::TransferFuture::PlainFutureType value_type;
-      typedef size_t difference_type;
-      typedef std::forward_iterator_tag iterator_category;
+    private:
+
+      bool isFinalised{false};
+
+
+
+      cppext::future_queue<void> notification_queue;
+
   };
 
-} /* namespace std */
+  /********************************************************************************************************************/
 
-namespace ChimeraTK {
+  inline ReadAnyGroup::ReadAnyGroup() {}
 
-  /*******************************************************************************************************************/
-
-  namespace detail {
-
-    template<class TransferElementType>
-    ChimeraTK::TransferElementID readAny(
-              std::list<std::reference_wrapper<TransferElementType>> elementsToRead) {
-
-      // build list of TransferFutures for all elements. Since readAsync() is a virtual call and we need to visit all
-      // elements at least twice (once in wait_for_any and a second time for sorting by version number), this is assumed
-      // to be less expensive than calling readAsync() on the fly in the TransferFutureIterator instead.
-      std::list<TransferFuture> futureList;
-      for(auto &elem : elementsToRead) {
-        futureList.push_back(elem.get().readAsync());
-      }
-      // wait until any future is ready
-      auto iter = boost::wait_for_any(detail::TransferFutureIterator(futureList.begin()),
-                                      detail::TransferFutureIterator(futureList.end()));
-
-      // Find the variable which has the oldest version number (to guarantee the order of updates).
-      // Start with assuming that the future returned by boost::wait_for_any() has the oldes version number.
-      TransferFuture theUpdate = iter.getTransferFuture();
-      for(auto future : futureList) {
-        // skip if this is the future returned by boost::wait_for_any()
-        if(future == theUpdate) continue;
-        // also skip if the future is not yet ready
-        if(!future.hasNewData()) continue;
-        // compare  version number with the version number of the stored future
-        if(future.getBoostFuture().get()->_versionNumber < theUpdate.getBoostFuture().get()->_versionNumber) {
-          theUpdate = future;
-        }
-      }
-
-      // complete the transfer (i.e. run postRead())
-      theUpdate.wait();
-
-      // return the transfer element as a shared pointer
-      return theUpdate.getTransferElementID();
-
-    }
-
-  } // namespace detail
-#endif
-
-  /*******************************************************************************************************************/
-
-  inline ChimeraTK::TransferElementID readAny(
-          std::list<std::reference_wrapper<ChimeraTK::TransferElement>> /* elementsToRead */) {
-    throw;
-    //return detail::readAny(elementsToRead);
-  }
-
-  /*******************************************************************************************************************/
-
-  inline ChimeraTK::TransferElementID readAny(
-          std::list<std::reference_wrapper<ChimeraTK::TransferElementAbstractor>> /* elementsToRead */) {
-    throw;
-    //return detail::readAny(elementsToRead);
-  }
 
 } /* namespace ChimeraTK */
 
