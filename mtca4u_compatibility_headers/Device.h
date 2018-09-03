027364c71c3204a2746ce626dd70bfd7e4038097
Martin Hierholzer <martin.hierholzer@desy.de>
2018-09-03 16:06:58 +0200

    added Exception header files in the compatibility layer, since they no longer are included in the new header files

--- mtca4u_compatibility_headers/Device.h
+++ mtca4u_compatibility_headers/Device.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <ChimeraTK/Device.h>
+#include <mtca4u/DeviceException.h>
 
 //#warning You are using the deprecated namespace 'mtca4u'. Please change to namespace 'ChimeraTK'.
 
