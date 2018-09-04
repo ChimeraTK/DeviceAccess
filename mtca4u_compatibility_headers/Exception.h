478d538ac418c62729c144b72f80315a443034b4
Martin Hierholzer <martin.hierholzer@desy.de>
2018-09-04 17:03:06 +0200

    enable warnings in mtca4u compatibility headers

--- mtca4u_compatibility_headers/Exception.h
+++ mtca4u_compatibility_headers/Exception.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <ChimeraTK/Exception.h>
 
-//#warning You are using the deprecated namespace 'mtca4u'. Please change to namespace 'ChimeraTK'.
+#warning You are using the deprecated namespace 'mtca4u'. Please change to namespace 'ChimeraTK'.
 
 namespace mtca4u{
   using namespace ChimeraTK;
--- mtca4u_compatibility_headers/MapFileParser.h
+++ mtca4u_compatibility_headers/MapFileParser.h
@@ -2,7 +2,7 @@
 #include <ChimeraTK/MapFileParser.h>
 #include <mtca4u/MapException.h>
 
-//#warning You are using the deprecated namespace 'mtca4u'. Please change to namespace 'ChimeraTK'.
+#warning You are using the deprecated namespace 'mtca4u'. Please change to namespace 'ChimeraTK'.
 
 namespace mtca4u{
   using namespace ChimeraTK;
