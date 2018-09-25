e84ba3cc3b1d59f72bc9f94c5370410000a3d34e
Martin Hierholzer <martin.hierholzer@desy.de>
2018-09-25 13:01:57 +0200

    added second demo application

---
+++ example2/demoApp2.cc
@@ -0,0 +1,45 @@
+#include <ApplicationCore.h>
+#include <PeriodicTrigger.h>
+
+namespace ctk = ChimeraTK;
+
+struct Controller : public ctk::ApplicationModule {
+    using ctk::ApplicationModule::ApplicationModule;
+    ctk::ScalarPollInput<double> sp{this, "sp", "degC", "Description", {"CS"}};
+    ctk::ScalarPushInput<double> rb{this, "rb", "degC", "...", {"DEV", "CS"}};
+    ctk::ScalarOutput<double> cur{this, "cur", "mA", "...", {"DEV"}};
+
+    void mainLoop() {
+      const double gain = 100.0;
+      while(true) {
+        readAll();     // waits until rb updated, then reads sp
+
+        cur = gain * (sp - rb);
+        writeAll();    // writes any outputs
+      }
+    }
+};
+
+
+struct ExampleApp : public ctk::Application {
+    ExampleApp() : Application("exampleApp") {}
+    ~ExampleApp() { shutdown(); }
+
+    Controller controller{this, "Controller", "The Controller"};
+
+    ctk::PeriodicTrigger timer{this, "Timer", "Periodic timer for the controller", 1000};
+
+    ctk::DeviceModule heater{"oven","heater"};
+    ctk::ControlSystemModule cs{"Bakery"};
+
+    void defineConnections();
+};
+static ExampleApp theExampleApp;
+
+
+void ExampleApp::defineConnections() {
+    ChimeraTK::setDMapFilePath("example2.dmap");
+
+    controller.findTag("DEV").connectTo(heater, timer.tick);
+    controller.findTag("CS").connectTo(cs);
+}
