152913a50b43d29f965fea03f211af7763d3b759
Zenker, Dr. Klaus (FWKE) - 126506 <k.zenker@hzdr.de>
2018-11-09 11:40:03 +0100

    Use templates for the history.
    This is still not nice. Now for every data type an individual module needs to be created.

--- Modules/include/ServerHistory.h
+++ Modules/include/ServerHistory.h
@@ -1,25 +1,31 @@
 /*!
  * \author Klaus Zenker (HZDR)
  * \date 10.08.2018
- * \page historydoc Server based history module
+ * \page historydoc Server based history modules
  * \section historyintro Server based history
  *  Some control systems offer a variable history but some do not. In this case the \c ServerHistory can be used to
  *  create a history ring buffer on the server. If only a local history is needed consider to use the \c MicroDAQ module.
- * In order to do so you connect the variable that should have a history on the server
- *  to the \c ServerHistory module. In addition you need to specify the history length. Every time the variable is updated
+ *  In order to do so you connect the variable that should have a history on the server
+ *  to the \c ScalarServerHistory module or the \c ArrayServerHistory for input scalars or input arrays.
+ *  In addition you need to specify the history length. Every time the variable is updated
  *  it will be filled into the history buffer. The buffer length (history length) can not be changed during runtime and
  *  requires a server restart.
- *  Output variables created by the \c ServerHistory module are named: \c "module_name"/"variable_name"_hist, where
- *  the variable_name can be set different from the original feeding variable.
+ *
+ *  So far you need to create a ServerHistory for each variable type.
+ *
+ *  Output variables created by the \c ScalarServerHistory module are named: \c "module_name"/"variable_name"_hist.
+ *  In case of \c ArrayServerHistory the outputs are named: \c "module_name"/"variable_name"_i_hist, where i
+ *  is the position in the input array. In consequence an input array of length i will result in i
+ *  output history arrays.
+ *  In general the variable_name can be set different from the original feeding variable.
  *  The following tags are added to the history output variable:
  *  - CS
  *  - history
  *  - "module_name"
  *
- *  The \c ServerHistory module also includes a Logger, which can be used to feed a \c LoggingModule
- *  So far the \c ServerHistory is just implemented for float variables, but it could easily be extended using a template approach.
+ *  The ServerHistory modules also includes a Logger, which can be used to feed a \c LoggingModule
  *
- *  The following example shows how to integrate use the \c ServerHistory module
+ *  The following example shows how to integrate use the \c ScalarServerHistory module
  *  messages (TestModule).
  *  \code
  *  sruct TestModule: public ChimeraTK::ApplicationModule{
@@ -28,7 +34,7 @@
  *  };
  *  struct myApp : public ChimeraTK::Application{
  *
- *  history::ServerHistory history{this, "ServerHistory", "History for certain variables"};
+ *  history::ScalarServerHistory<float> history{this, "ServerHistory", "History for certain scalara float variables"};
  *
  *  ChimeraTK::ControlSystemModule cs;
  *
@@ -64,16 +70,92 @@
 #include "Logging.h"
 
 #include <vector>
+#include <tuple>
 
-namespace ctk = ChimeraTK;
-
+namespace ChimeraTK{
 namespace history{
 
-struct ServerHistory: public ctk::ApplicationModule{
-  using ctk::ApplicationModule::ApplicationModule;
-  using histEntry = std::pair<ctk::ScalarPushInput<float>,ctk::ArrayOutput<float> > ;
+/**
+ * \brief Module adding a history for an scalar output variable.
+ *
+ * A history array with a specified length will be created.
+ * The name of the output variable is: module_name/par_name_hist,
+ * where module_name and par_name are set when adding an output variable.
+ *
+ */
+template<typename UserType>
+struct ScalarServerHistory: public ApplicationModule{
+  using ApplicationModule::ApplicationModule;
+  using histEntry = std::pair<ScalarPushInput<UserType>,ArrayOutput<UserType> > ;
 
-  //\ToDo: Make this flexible in type -> templated map with different template types
+  std::vector<histEntry> v_history;
+
+  /* List of inputs to the history module used to avoid duplicates */
+  std::vector<std::string> inputs;
+
+  logging::Logger logger{this};
+  /*
+   * This method should be called in defineConnections() of the application.
+   * \param in A pointer to the ScalarOutput you want to have a history for.
+   * \param module_name The name of the Module that contains in
+   * \param par_name The name of the parameter (the created history variables will be named par_name_hist)
+   * \param length This is the length of the history arrays.
+   */
+  void addHistory(ScalarOutput<UserType> *in, const std::string &module_name, const std::string &par_name, const uint &length);
+  void mainLoop() override;
+};
+
+template<typename UserType>
+void ScalarServerHistory<UserType>::addHistory(ScalarOutput<UserType> *in, const std::string &module_name, const std::string &par_name, const uint &length){
+  if(find(inputs.begin(), inputs.end(), std::string(module_name + "/" + par_name)) == inputs.end()){
+    std::string path = module_name + "/" + par_name + "_hist";
+    v_history.emplace_back(
+        std::piecewise_construct,
+        std::forward_as_tuple(ScalarPushInput<UserType>{this, par_name + "_hist_in", "", "",
+        { "history", module_name }}),
+        std::forward_as_tuple(ArrayOutput<UserType>{this, path, "", length, "",
+        { "CS", "history", module_name }}));
+    *in >> v_history.back().first;
+    inputs.push_back(module_name + "/" + par_name);
+    logger.sendMessage(std::string("Added history for " + inputs.back() + " with path: " + path),logging::DEBUG);
+  } else {
+    throw ChimeraTK::logic_error("Cannot add history for variable "+par_name+
+      " since history was already added for this variable.");
+  }
+}
+
+template<typename UserType>
+void ScalarServerHistory<UserType>::mainLoop(){
+  if(inputs.size() == 0)
+    throw ChimeraTK::logic_error("ServerHistory module has no inputs. Did you forget to add some in defineConnections()?");
+  auto group = readAnyGroup();
+  while(true){
+    auto id = group.readAny();
+    for(auto &element: v_history){
+      if(id == element.first.getId()){
+        logger.sendMessage(std::string("Update history for variable:") + element.first.getName(), logging::DEBUG);
+        std::rotate(element.second.begin(), element.second.begin()+1, element.second.end());
+        *(element.second.end()-1) = element.first;
+        element.second.write();
+      }
+    }
+  }
+}
+
+/**
+ * \brief Module adding a history for an ArrayOutput variable.
+ *
+ * For each element in the array a separate history array with a specified length will be created.
+ * The name of the output variables is: module_name/par_name_i_hist,
+ * where module_name and par_name are set when adding an output variable and i is the element index
+ * of the input array.
+ *
+ */
+template<typename UserType>
+struct ArrayServerHistory: public ApplicationModule{
+  using ApplicationModule::ApplicationModule;
+  using histEntry = std::pair<ArrayPushInput<UserType>,std::vector<ArrayOutput<UserType> > > ;
+
   std::vector<histEntry> v_history;
 
   /* List of inputs to the history module used to avoid duplicates */
@@ -81,10 +163,104 @@ struct ServerHistory: public ctk::ApplicationModule{
 
   logging::Logger logger{this};
 
-  void addHistory(ctk::ScalarOutput<float> *in, const std::string &module_name, const std::string &par_name, const uint &length);
+  /*
+   * Since this method is to be called in defineConnections() of the application the array length is not yet available
+   * from the input Array and needs to be set in addition.
+   * \param in A pointer to the Array you want to have a history for.
+   * \param arrLength The length of the array in
+   * \param module_name The name of the Module that contains in
+   * \param par_name The name of the parameter (the created history variables will be named par_name_i_hist
+   * where i is the position in the array i)
+   * \param length This is the length of the history arrays.
+   */
+  void addHistory(ArrayOutput<UserType> *in, const size_t arrLength, const std::string &module_name, const std::string &par_name, const uint &length);
   void mainLoop() override;
 };
 
+template<typename UserType>
+void ArrayServerHistory<UserType>::addHistory(ArrayOutput<UserType> *in, const size_t arrLength, const std::string &module_name, const std::string &par_name, const uint &length){
+  if(in == nullptr)
+    throw runtime_error("Nullpointer passed to ArrrayServerHistory module");
+  if(find(inputs.begin(), inputs.end(), std::string(module_name + "/" + par_name)) == inputs.end()){
+    v_history.emplace_back(
+        std::piecewise_construct,
+        std::forward_as_tuple(ArrayPushInput<UserType>{this, par_name + "_hist_in", "", arrLength, "",
+        { "history", module_name }}),
+        std::forward_as_tuple(std::vector<ArrayOutput<UserType> >{}));
+    (*in) >> v_history.back().first;
+    for(size_t i =0; i < arrLength; i++){
+      v_history.back().second.emplace_back(ArrayOutput<UserType>{this, module_name + "/" + par_name + "_" + std::to_string(i) + "_hist", "", length, "",
+        { "CS", "history", module_name }});
+    }
+    inputs.push_back(module_name + "/" + par_name);
+    logger.sendMessage(std::string("Added history for " + inputs.back()),logging::DEBUG);
+  } else {
+    throw ChimeraTK::logic_error("Cannot add history for variable "+par_name+
+      " since history was already added for this variable.");
+  }
 }
 
+template<typename UserType>
+void ArrayServerHistory<UserType>::mainLoop(){
+  if(inputs.size() == 0)
+    throw ChimeraTK::logic_error("ServerHistory module has no inputs. Did you forget to add some in defineConnections()?");
+  auto group = readAnyGroup();
+  while(true){
+    auto id = group.readAny();
+    for(auto &element: v_history){
+      if(id == element.first.getId()){
+        logger.sendMessage(std::string("Update history for variable:") + element.first.getName(), logging::DEBUG);
+        for(size_t i = 0; i < element.first.getNElements(); i++){
+          std::rotate(element.second.at(i).begin(), element.second.at(i).begin()+1, element.second.at(i).end());
+          *(element.second.at(i).end()-1) = element.first[i];
+          element.second.at(i).write();
+        }
+      }
+    }
+  }
+}
+
+struct ServerHistory : public ModuleGroup{
+  using ModuleGroup::ModuleGroup;
+
+  ScalarServerHistory<float> v_float;
+  ScalarServerHistory<double> v_double;
+
+  bool has_float{false};
+  bool has_double{false};
+
+  template<typename UserType>
+  void addHistory(ScalarOutput<UserType> *in, const std::string &module_name, const std::string &par_name, const uint &length);
+
+  /**
+   * Use this method in defineConnections() of your application after all variables to the history.
+   */
+  void connctAll(ChimeraTK::ControlSystemModule *cs, std::string subfolder);
+
+};
+
+template<typename UserType>
+void ServerHistory::addHistory(ScalarOutput<UserType> *in, const std::string &module_name, const std::string &par_name, const uint &length){
+  throw ChimeraTK::runtime_error("ServerHistory: Type not supported.");
+}
+
+template<>
+void ServerHistory::addHistory(ScalarOutput<float> *in, const std::string &module_name, const std::string &par_name, const uint &length){
+  if(!has_float){
+    v_float = ScalarServerHistory<float>{this, "ScalarServerHistory", "History of floats"};
+    has_float = true;
+  }
+  v_float.addHistory(in, module_name, par_name, length);
+}
+
+template<>
+void ServerHistory::addHistory(ScalarOutput<double> *in, const std::string &module_name, const std::string &par_name, const uint &length){
+  if(!has_double){
+    v_double = ScalarServerHistory<double>{this, "ScalarServerHistory", "History of doubles"};
+    has_double = true;
+  }
+  v_double.addHistory(in, module_name, par_name, length);
+}
+}
+}
 #endif /* MODULES_SERVERHISTORY_H_ */
--- tests/executables_src/testHistory.C
+++ tests/executables_src/testHistory.C
@@ -18,49 +18,117 @@
 #include "TestFacility.h"
 
 using namespace boost::unit_test_framework;
-
+template<typename UserType>
 struct Dummy: public ChimeraTK::ApplicationModule{
   using ChimeraTK::ApplicationModule::ApplicationModule;
-  ChimeraTK::ScalarPushInput<float> dummy_in {this, "dummy_in", "", "Dummy input"};
-  ChimeraTK::ScalarOutput<float> dummy_out {this, "dummy_out", "", "Dummy output"};
+  ChimeraTK::ScalarPushInput<UserType> in {this, "in", "", "Dummy input"};
+  ChimeraTK::ScalarOutput<UserType> out {this, "out", "", "Dummy output"};
 
   void mainLoop() override{
     while(true){
-      dummy_in.read();
-      dummy_out = 1.0 * dummy_in;
-      dummy_out.write();
+      in.read();
+      out = 1.0 * in;
+      out.write();
     }
   }
 };
 
+template<typename UserType>
+struct DummyArray: public ChimeraTK::ApplicationModule{
+  using ChimeraTK::ApplicationModule::ApplicationModule;
+  ChimeraTK::ArrayPushInput<UserType> in {this, "in", "", 3, "Dummy input"};
+  ChimeraTK::ArrayOutput<UserType> out {this, "out", "", 3, "Dummy output"};
+
+  void mainLoop() override{
+    while(true){
+      in.read();
+      for(size_t i = 0; i < 3; i++)
+        out[i] = 1.0 * in[i];
+      out.write();
+    }
+  }
+};
 
 /**
- * Define a test app to test the SoftwareMasterModule.
+ * Define a test app to test the scalar History Module.
  */
+template<typename UserType>
 struct testApp : public ChimeraTK::Application {
   testApp() : Application("test"){ }
   ~testApp() {
     shutdown();
   }
 
-  Dummy dummy{this, "Dummy", "Dummy module"};
-  history::ServerHistory hist { this, "ServerHistory", "History of selected process variables." };
+  Dummy<UserType> dummy{this, "Dummy", "Dummy module"};
+  ChimeraTK::history::ScalarServerHistory<UserType> hist { this, "ServerHistory", "History of selected process variables." };
 
   logging::LoggingModule log { this, "LoggingModule",
       "LoggingModule test" };
   ChimeraTK::ControlSystemModule cs;
 
   void defineConnections() override{
-    hist.addHistory(&dummy.dummy_out, dummy.getName(), "out", 20);
+    hist.addHistory(&dummy.out, dummy.getName(), "out", 20);
     hist.findTag("CS").connectTo(cs["history"]);
-    dummy.dummy_in >> cs("out");
-    cs("in") >> dummy.dummy_in;
+    dummy.in >> cs("out");
+    cs("in") >> dummy.in;
     log.addSource(&hist.logger);
    }
 };
 
-BOOST_AUTO_TEST_CASE( testHistory) {
-  testApp app;
+/**
+ * Define a test app to test the array History Module.
+ */
+template<typename UserType>
+struct testAppArray : public ChimeraTK::Application {
+  testAppArray() : Application("test"){ }
+  ~testAppArray() {
+    shutdown();
+  }
+
+  DummyArray<UserType> dummy{this, "Dummy", "Dummy module"};
+  ChimeraTK::history::ArrayServerHistory<UserType> hist { this, "ServerHistory", "History of selected process variables." };
+
+  logging::LoggingModule log { this, "LoggingModule",
+      "LoggingModule array test" };
+  ChimeraTK::ControlSystemModule cs;
+
+  void defineConnections() override{
+    hist.addHistory(&dummy.out, 3, dummy.getName(), "out", 20);
+    hist.findTag("CS").connectTo(cs["history"]);
+    dummy.in >> cs("out");
+    cs("in") >> dummy.in;
+    log.addSource(&hist.logger);
+   }
+};
+
+/**
+ * Define a test app to test the array History Module.
+ */
+template<typename UserType>
+struct testServerHistoryApp : public ChimeraTK::Application {
+  testServerHistoryApp() : Application("test"){ }
+  ~testServerHistoryApp() {
+    shutdown();
+  }
+
+  Dummy<UserType> dummy{this, "Dummy", "Dummy module"};
+  ChimeraTK::history::ServerHistory hist { this, "ServerHistory", "History of selected process variables." };
+
+  logging::LoggingModule log { this, "LoggingModule",
+      "LoggingModule array test" };
+  ChimeraTK::ControlSystemModule cs;
+
+  void defineConnections() override{
+    hist.addHistory<UserType>(&dummy.out, dummy.getName(), "out", 20);
+    hist.v_float.findTag("CS").connectTo(cs["history"]);
+    dummy.in >> cs("out");
+    cs("in") >> dummy.in;
+    log.addSource(&hist.v_float.logger);
+   }
+};
+
+BOOST_AUTO_TEST_CASE( testHistory_float) {
+  testApp<float> app;
   ChimeraTK::TestFacility tf;
   auto i = tf.getScalar<float>("in");
   tf.runApplication();
@@ -70,7 +138,126 @@ BOOST_AUTO_TEST_CASE( testHistory) {
   BOOST_CHECK_EQUAL(tf.readScalar<float>("out"), 42.0);
   std::vector<float> v_ref(20);
   v_ref.back() = 42.0;
-  BOOST_CHECK_EQUAL(tf.readArray<float>("history/Dummy/out_hist").back(), v_ref.back());
+  auto v = tf.readArray<float>("history/Dummy/out_hist");
+  BOOST_CHECK_EQUAL_COLLECTIONS(v.begin(), v.end(),
+      v_ref.begin(), v_ref.end());
+  i = 42.0;
+  i.write();
+  tf.stepApplication();
+  *(v_ref.end()-2) = 42.0;
+  v = tf.readArray<float>("history/Dummy/out_hist");
+  BOOST_CHECK_EQUAL_COLLECTIONS(v.begin(), v.end(),
+      v_ref.begin(), v_ref.end());
+
+}
+
+BOOST_AUTO_TEST_CASE( testHistory_double) {
+  testApp<double> app;
+  ChimeraTK::TestFacility tf;
+  auto i = tf.getScalar<double>("in");
+  tf.runApplication();
+  i = 42.;
+  i.write();
+  tf.stepApplication();
+  BOOST_CHECK_EQUAL(tf.readScalar<double>("out"), 42.0);
+  std::vector<double> v_ref(20);
+  v_ref.back() = 42.0;
+  auto v = tf.readArray<double>("history/Dummy/out_hist");
+  BOOST_CHECK_EQUAL_COLLECTIONS(v.begin(), v.end(),
+      v_ref.begin(), v_ref.end());
+  i = 42.0;
+  i.write();
+  tf.stepApplication();
+  *(v_ref.end()-2) = 42.0;
+  v = tf.readArray<double>("history/Dummy/out_hist");
+  BOOST_CHECK_EQUAL_COLLECTIONS(v.begin(), v.end(),
+      v_ref.begin(), v_ref.end());
+
+}
+
+BOOST_AUTO_TEST_CASE( testHistory_floatArray) {
+  testAppArray<float> app;
+  ChimeraTK::TestFacility tf;
+  auto i = tf.getArray<float>("in");
+  tf.runApplication();
+  i[0] = 42.;
+  i[1] = 43.;
+  i[2] = 44.;
+  i.write();
+  tf.stepApplication();
+  BOOST_CHECK_EQUAL(tf.readArray<float>("out")[0], 42.0);
+  BOOST_CHECK_EQUAL(tf.readArray<float>("out")[1], 43.0);
+  BOOST_CHECK_EQUAL(tf.readArray<float>("out")[2], 44.0);
+  std::vector<float> v_ref(20);
+  for(size_t i = 0; i < 3; i++){
+    v_ref.back() = 42.0 + i;
+    auto v = tf.readArray<float>("history/Dummy/out_" + std::to_string(i) + "_hist");
+    BOOST_CHECK_EQUAL_COLLECTIONS(v.begin(), v.end(),
+      v_ref.begin(), v_ref.end());
+  }
+
+  i[0] = 1.0;
+  i[1] = 2.0;
+  i[2] = 3.0;
+  i.write();
+  tf.stepApplication();
+  for(size_t i = 0; i < 3; i++){
+    *(v_ref.end()-2) = 42.0 + i;
+    *(v_ref.end()-1) = 1.0 + i;
+    auto v = tf.readArray<float>("history/Dummy/out_" + std::to_string(i) + "_hist");
+    BOOST_CHECK_EQUAL_COLLECTIONS(v.begin(), v.end(),
+      v_ref.begin(), v_ref.end());
+
+  }
+
+}
+
+BOOST_AUTO_TEST_CASE( testHistory_doubleArray) {
+  testAppArray<double> app;
+  ChimeraTK::TestFacility tf;
+  auto i = tf.getArray<double>("in");
+  tf.runApplication();
+  i[0] = 42.;
+  i[1] = 43.;
+  i[2] = 44.;
+  i.write();
+  tf.stepApplication();
+  BOOST_CHECK_EQUAL(tf.readArray<double>("out")[0], 42.0);
+  BOOST_CHECK_EQUAL(tf.readArray<double>("out")[1], 43.0);
+  BOOST_CHECK_EQUAL(tf.readArray<double>("out")[2], 44.0);
+  std::vector<double> v_ref(20);
+  for(size_t i = 0; i < 3; i++){
+    v_ref.back() = 42.0 + i;
+    auto v = tf.readArray<double>("history/Dummy/out_" + std::to_string(i) + "_hist");
+    BOOST_CHECK_EQUAL_COLLECTIONS(v.begin(), v.end(),
+      v_ref.begin(), v_ref.end());
+  }
+
+  i[0] = 1.0;
+  i[1] = 2.0;
+  i[2] = 3.0;
+  i.write();
+  tf.stepApplication();
+  for(size_t i = 0; i < 3; i++){
+    *(v_ref.end()-2) = 42.0 + i;
+    *(v_ref.end()-1) = 1.0 + i;
+    auto v = tf.readArray<double>("history/Dummy/out_" + std::to_string(i) + "_hist");
+    BOOST_CHECK_EQUAL_COLLECTIONS(v.begin(), v.end(),
+      v_ref.begin(), v_ref.end());
+  }
+}
+
+BOOST_AUTO_TEST_CASE( testServerHistory) {
+  testServerHistoryApp<float> app;
+  ChimeraTK::TestFacility tf;
+  auto i = tf.getScalar<float>("in");
+  tf.runApplication();
+  i = 42.;
+  i.write();
+  tf.stepApplication();
+  BOOST_CHECK_EQUAL(tf.readScalar<float>("out"), 42.0);
+  std::vector<float> v_ref(20);
+  v_ref.back() = 42.0;
   auto v = tf.readArray<float>("history/Dummy/out_hist");
   BOOST_CHECK_EQUAL_COLLECTIONS(v.begin(), v.end(),
       v_ref.begin(), v_ref.end());
