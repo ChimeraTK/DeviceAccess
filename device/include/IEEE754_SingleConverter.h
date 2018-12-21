c7d76efec235d29ca22bed836cf195aab2c62ada
Martin Killenberg <martin.killenberg@desy.de>
2018-12-21 17:51:12 +0100

    moved template-specialisation to .cc file. They don't belong to the header.

--- device/include/IEEE754_SingleConverter.h
+++ device/include/IEEE754_SingleConverter.h
@@ -59,16 +59,6 @@ namespace ChimeraTK{
     return cooked;
   }
 
-  template<>
-  std::string IEEE754_SingleConverter::toCooked<std::string>(uint32_t rawValue) const{
-    //Step 1 as in the normal template: reinterpret cast
-    void * warningAvoider = &rawValue;
-    float genericRepresentation = *(reinterpret_cast<float *>(warningAvoider));
-
-    //Step 2: convert to string
-    return std::to_string(genericRepresentation);
-  }
-    
   template<typename CookedType>
   uint32_t IEEE754_SingleConverter::toRaw(CookedType cookedValue) const{
     // step 1: convert from cooked to the generic representation in the CPU (float)
@@ -89,23 +79,10 @@ namespace ChimeraTK{
   }
 
   template<>
-  uint32_t IEEE754_SingleConverter::toRaw(std::string cookedValue) const{
-    // step 1: convert string to float
-    float genericRepresentation;
-    try{
-      genericRepresentation = std::stof(cookedValue);
-    }catch(std::exception &e){
-      //FIXME: VariableName
-      // Note: We cannot do out of range limitations because we don't know if too large or too small
-      throw ChimeraTK::logic_error( e.what() );
-    }
-
-    // step 2 as in the normal template: reinterpret cast
-    void * warningAvoider = &genericRepresentation;
-    int32_t rawValue = *(reinterpret_cast<int32_t *>(warningAvoider));
-
-    return rawValue;
-  }
+  std::string IEEE754_SingleConverter::toCooked<std::string>(uint32_t rawValue) const;
+    
+  template<>
+  uint32_t IEEE754_SingleConverter::toRaw(std::string cookedValue) const;
   
 }// namespace ChimeraTK
 
