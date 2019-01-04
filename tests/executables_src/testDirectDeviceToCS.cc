ChimeraTK/DeviceAccess

commit 936d7c0d8502742336b590412c9fc5539df1153f

Fixed wrongly created hierarchies when directly connecting a device to the controlsystem using connectTo(). Now proper virtual submodules are created for each hierarchy level, insted of keeping the slash in the variable name.

src/VirtualModule.cc
@@ -107,5 +107,34 @@ namespace ChimeraTK {
     return *this;
   }
 
+/*********************************************************************************************************************/
+
+  VirtualModule& VirtualModule::createAndGetSubmodule(const std::string& moduleName) {
+    for(auto &sm : submodules) {
+      if(sm.getName() == moduleName) return sm;
+    }
+    addSubModule(VirtualModule(moduleName, getDescription(), getModuleType()));
+    return submodules.back();
+  }
+
+/*********************************************************************************************************************/
+
+  VirtualModule& VirtualModule::createAndGetSubmoduleRecursive(const std::string& moduleName) {
+    auto slash = moduleName.find_first_of("/");
+    if(slash == std::string::npos) {
+      return createAndGetSubmodule(moduleName);
+    }
+    else {
+      auto firstSubmodule = moduleName.substr(0,slash);
+      auto remainingSubmodules = moduleName.substr(slash+1);
+      auto &sm = createAndGetSubmodule(firstSubmodule);
+      sm.createAndGetSubmoduleRecursive(remainingSubmodules);
+      return sm;
+    }
+  }
+
+/*********************************************************************************************************************/
+
+
 } /* namespace ChimeraTK */
 

include/VirtualModule.h
@@ -44,6 +44,13 @@ namespace ChimeraTK {
       /** Add a virtual sub-module. The module instance will be added to an internal list. */
       void addSubModule(VirtualModule module);
 
+      /** Return the submodule with the given name. If it doesn't exist, create it first. */
+      VirtualModule& createAndGetSubmodule(const std::string& moduleName);
+
+      /** Like createAndGetSubmodule(), but recursively create a hierarchy of submodules separated by "/" in the
+       *  moduleName. */
+      VirtualModule& createAndGetSubmoduleRecursive(const std::string& moduleName);
+
       ModuleType getModuleType() const override { return _moduleType; }
 
       const Module& virtualise() const override;

src/DeviceModule.cc
@@ -131,9 +131,12 @@ namespace ChimeraTK {
       }
 
       auto name = std::string(reg.getRegisterName()).substr(prefixLength);
-      VariableNetworkNode node(name, deviceAliasOrURI, reg.getRegisterName(), updateMode,
+      auto lastSlash = name.find_last_of("/");
+      auto dirname = name.substr(0,lastSlash);
+      auto basename = name.substr(lastSlash+1);
+      VariableNetworkNode node(basename, deviceAliasOrURI, reg.getRegisterName(), updateMode,
                                direction, *valTyp, reg.getNumberOfElements());
-      virtualisedModuleFromCatalog.getAccessorList().push_back(node);
+      virtualisedModuleFromCatalog.createAndGetSubmoduleRecursive(dirname).getAccessorList().push_back(node);
 
     }
 

commit c194ac96dec4b4703db5b6016c5941f93a03b20f

fixed bug introduced in 936d7c0d8502742336b590412c9fc5539df1153f

src/VirtualModule.cc
@@ -127,9 +127,7 @@ namespace ChimeraTK {
     else {
       auto firstSubmodule = moduleName.substr(0,slash);
       auto remainingSubmodules = moduleName.substr(slash+1);
-      auto &sm = createAndGetSubmodule(firstSubmodule);
-      sm.createAndGetSubmoduleRecursive(remainingSubmodules);
-      return sm;
+      return createAndGetSubmodule(firstSubmodule).createAndGetSubmoduleRecursive(remainingSubmodules);
     }
   }
 

tests/executables_src/testDirectDeviceToCS.cc
@@ -209,6 +209,8 @@ BOOST_AUTO_TEST_CASE( testConnectTo ) {
   auto devint8 = dev.getScalarRegisterAccessor<int8_t>("/Integers/signed8");
   auto devuint8 = dev.getScalarRegisterAccessor<uint8_t>("/Integers/unsigned8");
   auto devfloat = dev.getScalarRegisterAccessor<double>("/FixedPoint/value");
+  auto devDeep1 = dev.getScalarRegisterAccessor<int32_t>("/Deep/Hierarchies/Need/Tests/As/well");
+  auto devDeep2 = dev.getScalarRegisterAccessor<int32_t>("/Deep/Hierarchies/Need/Another/test");
   auto csActuator = test.getScalar<int32_t>("/MyModule/actuator");
   auto csReadback = test.getScalar<int32_t>("/MyModule/readBack");
   auto csint32 = test.getScalar<int32_t>("/Integers/signed32");
@@ -218,6 +220,8 @@ BOOST_AUTO_TEST_CASE( testConnectTo ) {
   auto csint8 = test.getScalar<int8_t>("/Integers/signed8");
   auto csuint8 = test.getScalar<uint8_t>("/Integers/unsigned8");
   auto csfloat = test.getScalar<double>("/FixedPoint/value");
+  auto csDeep1 = test.getScalar<int32_t>("/Deep/Hierarchies/Need/Tests/As/well");
+  auto csDeep2 = test.getScalar<int32_t>("/Deep/Hierarchies/Need/Another/test");
   test.runApplication();
 
   testDirectRegister(test, csActuator, devActuator, []{});
@@ -229,5 +233,7 @@ BOOST_AUTO_TEST_CASE( testConnectTo ) {
   testDirectRegister(test, csint8, devint8, []{});
   testDirectRegister(test, csuint8, devuint8, []{});
   testDirectRegister(test, csfloat, devfloat, []{}, false);
+  testDirectRegister(test, csDeep1, devDeep1, []{});
+  testDirectRegister(test, csDeep2, devDeep2, []{});
 
 }

tests/test3.map
@@ -8,3 +8,5 @@
 /Integers/signed8                 1 24 4 1  8 0 1 RW
 /Integers/unsigned8               1 28 4 1  8 0 0 RW
 /FixedPoint/value                 1 32 4 1 32 8 1 RW
+/Deep/Hierarchies/Need/Tests/As/well  1 36 4 1 32 0 1 RW
+/Deep/Hierarchies/Need/Another/test   1 40 4 1 32 0 1 RW