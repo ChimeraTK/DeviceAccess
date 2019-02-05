8474fb0635c898134fc1f3f14b21e34c90e78312
Martin Hierholzer <martin.hierholzer@desy.de>
2019-02-05 17:52:52 +0100

    reformat code for readability

--- tests/RebotDummyServer/src/RebotDummyServer.cc
+++ tests/RebotDummyServer/src/RebotDummyServer.cc
@@ -5,198 +5,208 @@
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 #include <stdexcept>
- 
+
 namespace ChimeraTK {
 
-bool volatile stop_rebot_server = false;
+  bool volatile stop_rebot_server = false;
 
-  RebotDummyServer::RebotDummyServer(unsigned int portNumber, std::string mapFile,
-                                     unsigned int protocolVersion)
-    :  _state(ACCEPT_NEW_COMMAND),
-       _heartbeatCount(0),
-       _helloCount(0),
-       _dont_answer(false),
-       _registerSpace(mapFile),
-      _serverPort(portNumber),
-      _protocolVersion(protocolVersion),
-      _io(),
-      _serverEndpoint(ip::tcp::v4(), _serverPort),
-      _connectionAcceptor(_io, _serverEndpoint),
-      _currentClientConnection() {
-
-    if (protocolVersion == 0){
+  RebotDummyServer::RebotDummyServer(unsigned int portNumber, std::string mapFile, unsigned int protocolVersion)
+  : _state(ACCEPT_NEW_COMMAND),
+    _heartbeatCount(0),
+    _helloCount(0),
+    _dont_answer(false),
+    _registerSpace(mapFile),
+    _serverPort(portNumber),
+    _protocolVersion(protocolVersion),
+    _io(),
+    _serverEndpoint(ip::tcp::v4(), _serverPort),
+    _connectionAcceptor(_io, _serverEndpoint),
+    _currentClientConnection()
+  {
+    if (protocolVersion == 0) {
       _protocolImplementor.reset(new DummyProtocol0(*this));
-    }else if (protocolVersion == 1){
+    }
+    else if (protocolVersion == 1) {
       _protocolImplementor.reset(new DummyProtocol1(*this));
-    }else{
+    }
+    else {
       throw std::invalid_argument("RebotDummyServer: unknown protocol version");
     }
-      
-  // set the acceptor backlog to 1
-  /*  _connectionAcceptor.open(_serverEndpoint.protocol());
+
+    // set the acceptor backlog to 1
+    /*  _connectionAcceptor.open(_serverEndpoint.protocol());
     _connectionAcceptor.bind(_serverEndpoint);*/
-  _connectionAcceptor.listen(1);
+    _connectionAcceptor.listen(1);
 
-  // The first address of the register space is set to a reference value. This
-  // would be used to test the rebot client.
-  uint32_t registerAddress = 0x04;
-  int32_t wordToWrite = 0xDEADBEEF; // Change this to someting standardized
-                                    // later (eg FW version ..)
-  uint8_t bar = 0;
-  _registerSpace.open();
-  _registerSpace.write(bar, registerAddress, &wordToWrite, sizeof(wordToWrite));
-}
+    // The first address of the register space is set to a reference value. This
+    // would be used to test the rebot client.
+    uint32_t registerAddress = 0x04;
+    int32_t wordToWrite = 0xDEADBEEF; // Change this to someting standardized later (eg FW version ..)
+    uint8_t bar = 0;
+    _registerSpace.open();
+    _registerSpace.write(bar, registerAddress, &wordToWrite, sizeof(wordToWrite));
+  }
 
-void RebotDummyServer::start() {
-  stop_rebot_server = false; // might have been stopped before
-  
-  while (stop_rebot_server ==
-         false) { // loop accepts client connections - one at a time
+  /********************************************************************************************************************/
 
-    boost::shared_ptr<ip::tcp::socket> incomingConnection(
-        new ip::tcp::socket(_io));
-    try {
-      _connectionAcceptor.accept(*incomingConnection);
+  void RebotDummyServer::start() {
+    stop_rebot_server = false; // might have been stopped before
+
+    // loop accepts client connections - one at a time
+    while(stop_rebot_server == false) {
+
+      boost::shared_ptr<ip::tcp::socket> incomingConnection(new ip::tcp::socket(_io));
+      try {
+        _connectionAcceptor.accept(*incomingConnection);
+      }
+      catch(boost::system::system_error&) {
+        if (stop_rebot_server) {
+          break; // exit gracefully
+        }
+        else {
+          throw; // something else went wrong, rethrow the exception
+        }
+      }
+
+      // http://www.boost.org/doc/libs/1_46_0/doc/html/boost_asio/example/echo/blocking_tcp_echo_server.cpp
+      RebotDummyServer::handleAcceptedConnection(incomingConnection);
+      // boost::asio::detail::thread
+      // t(boost::bind(&RebotDummyServer::handleAcceptedConnection, this,
+      // incomingConnection));
     }
-    catch (boost::system::system_error& e) {
-      if (stop_rebot_server) {
-        break; // exit gracefully
-      } else {
-        throw; // something else went wrong, rethrow the exception
+  }
+
+  /********************************************************************************************************************/
+
+  void RebotDummyServer::processReceivedPackage(std::vector<uint32_t>& buffer) {
+    if(_state == INSIDE_MULTI_WORD_WRITE) {
+      _state = _protocolImplementor->continueMultiWordWrite(buffer);
+    }
+    else {  // has to be ACCEPT_NEW_COMMAND
+
+      // cause an error condition: just don't answer
+      if(_dont_answer) {
+        return;
+      }
+
+      uint32_t requestedAction = buffer.at(0);
+      switch(requestedAction) {
+
+        case SINGLE_WORD_WRITE:
+          _protocolImplementor->singleWordWrite(buffer);
+          break;
+        case  MULTI_WORD_WRITE:
+          _state = _protocolImplementor->multiWordWrite(buffer);
+          break;
+        case  MULTI_WORD_READ:
+          _protocolImplementor->multiWordRead(buffer);
+          break;
+        case  HELLO:
+          ++_helloCount;
+          _protocolImplementor->hello(buffer);
+          break;
+        case  PING:
+          ++_heartbeatCount;
+          _protocolImplementor->ping(buffer);
+          break;
+        default:
+          std::cout << "Instruction unknown in all protocol versions " << requestedAction << std::endl;
+          sendSingleWord(UNKNOWN_INSTRUCTION);
       }
     }
-
-    // http://www.boost.org/doc/libs/1_46_0/doc/html/boost_asio/example/echo/blocking_tcp_echo_server.cpp
-    RebotDummyServer::handleAcceptedConnection(incomingConnection);
-    // boost::asio::detail::thread
-    // t(boost::bind(&RebotDummyServer::handleAcceptedConnection, this,
-    // incomingConnection));
   }
-}
 
-void RebotDummyServer::processReceivedPackage(std::vector<uint32_t>& buffer) {
-  if (_state == INSIDE_MULTI_WORD_WRITE){
-    _state = _protocolImplementor->continueMultiWordWrite(buffer);
-  }else{// has to be ACCEPT_NEW_COMMAND
+  /********************************************************************************************************************/
 
-    // cause an error condition: just don't answer
-    if (_dont_answer){
-      return;
-    }
-
-    uint32_t requestedAction = buffer.at(0);
-    switch (requestedAction) {
-      
-      case SINGLE_WORD_WRITE: 
-        _protocolImplementor->singleWordWrite(buffer);
-        break;
-      case  MULTI_WORD_WRITE:
-        _state = _protocolImplementor->multiWordWrite(buffer);
-        break;
-      case  MULTI_WORD_READ:
-        _protocolImplementor->multiWordRead(buffer);
-        break;
-      case  HELLO:
-        ++_helloCount;
-        _protocolImplementor->hello(buffer);
-        break;
-      case  PING:
-        ++_heartbeatCount;
-        _protocolImplementor->ping(buffer);
-        break;
-      default:
-        std::cout << "Instruction unknown in all protocol versions " << requestedAction << std::endl;
-        sendSingleWord(UNKNOWN_INSTRUCTION);
-    }
+  void RebotDummyServer::writeWordToRequestedAddress(std::vector<uint32_t>& buffer) {
+    uint32_t registerAddress = buffer.at(1); // This is the word offset; since
+    // dummy device deals with byte
+    // addresses convert to bytes FIXME
+    registerAddress = registerAddress * 4;
+    int32_t wordToWrite = buffer.at(2);
+    uint8_t bar = 0;
+    _registerSpace.write(bar, registerAddress, &wordToWrite, sizeof(wordToWrite));
   }
-}
 
-void RebotDummyServer::writeWordToRequestedAddress(
-    std::vector<uint32_t>& buffer) {
-  uint32_t registerAddress = buffer.at(1); // This is the word offset; since
-                                           // dummy device deals with byte
-                                           // addresses convert to bytes FIXME
-  registerAddress = registerAddress * 4;
-  int32_t wordToWrite = buffer.at(2);
-  uint8_t bar = 0;
-  _registerSpace.write(bar, registerAddress, &wordToWrite, sizeof(wordToWrite));
-}
+  /********************************************************************************************************************/
 
-void RebotDummyServer::readRegisterAndSendData(std::vector<uint32_t>& buffer) {
-  uint32_t registerAddress =
-      buffer.at(1); // This is a word offset. convert to bytes before use. FIXME
-  registerAddress = registerAddress * 4;
-  uint32_t numberOfWordsToRead = buffer.at(2);
+  void RebotDummyServer::readRegisterAndSendData(std::vector<uint32_t>& buffer) {
+    uint32_t registerAddress = buffer.at(1); // This is a word offset. convert to bytes before use. FIXME
+    registerAddress = registerAddress * 4;
+    uint32_t numberOfWordsToRead = buffer.at(2);
 
-  // send data in two packets instead of one; this is done for test coverage.
-  // Let READ_SUCCESS_INDICATION go in the first write and data in the second.
-  sendSingleWord(READ_SUCCESS_INDICATION);
+    // send data in two packets instead of one; this is done for test coverage.
+    // Let READ_SUCCESS_INDICATION go in the first write and data in the second.
+    sendSingleWord(READ_SUCCESS_INDICATION);
 
-  std::vector<int32_t> dataToSend(numberOfWordsToRead);
-  uint8_t bar = 0;
-  // start putting in the read values from location dataToSend[1]:
-  int32_t* startAddressForReadInData = dataToSend.data();
-  _registerSpace.read(bar, registerAddress, startAddressForReadInData,
-                      numberOfWordsToRead * sizeof(int32_t));
+    std::vector<int32_t> dataToSend(numberOfWordsToRead);
+    uint8_t bar = 0;
+    // start putting in the read values from location dataToSend[1]:
+    int32_t* startAddressForReadInData = dataToSend.data();
+    _registerSpace.read(bar, registerAddress, startAddressForReadInData, numberOfWordsToRead * sizeof(int32_t));
 
-  // FIXME: Nothing in protocol to indicate read failure.
-  boost::asio::write(*_currentClientConnection,
-                     boost::asio::buffer(dataToSend));
-}
-  
-void RebotDummyServer::sendSingleWord(int32_t response) {
-  std::vector<int32_t> data{ response };
-  boost::asio::write(*_currentClientConnection, boost::asio::buffer(data));
-}
-
-RebotDummyServer::~RebotDummyServer() {
-  _connectionAcceptor.close();
-
-  // if the terminate signal is caught while waiting for a connection
-  // there is no client connection, so we have to check the pointer here
-  if (_currentClientConnection) {
-    _currentClientConnection->close();
+    // FIXME: Nothing in protocol to indicate read failure.
+    boost::asio::write(*_currentClientConnection, boost::asio::buffer(dataToSend));
   }
-}
 
-void RebotDummyServer::handleAcceptedConnection(
-    boost::shared_ptr<ip::tcp::socket>& incomingSocket) {
-  boost::asio::ip::tcp::no_delay option(true);
-  incomingSocket->set_option(option);
-  _currentClientConnection = incomingSocket;
+  /********************************************************************************************************************/
 
-  while (stop_rebot_server == false) { // This loop handles the accepted connection
+  void RebotDummyServer::sendSingleWord(int32_t response) {
+    std::vector<int32_t> data{ response };
+    boost::asio::write(*_currentClientConnection, boost::asio::buffer(data));
+  }
 
-    std::vector<uint32_t> dataBuffer(BUFFER_SIZE_IN_WORDS);
-    boost::system::error_code errorCode;
-    _currentClientConnection->read_some(boost::asio::buffer(dataBuffer),
-                                        errorCode);
+  /********************************************************************************************************************/
 
-    if (errorCode == boost::asio::error::eof) { // The client has closed the
-                                                // connection; move to the
-                                                // outer  loop to accept new
-                                                // connections
+  RebotDummyServer::~RebotDummyServer() {
+    _connectionAcceptor.close();
+
+    // if the terminate signal is caught while waiting for a connection
+    // there is no client connection, so we have to check the pointer here
+    if(_currentClientConnection) {
       _currentClientConnection->close();
-      //std::cout << "connection closed" << std::endl;
-      break;
-    } else if (errorCode && stop_rebot_server) { // reading was interrupted by a
-                                              // terminate signal;
-      // move to the outer loop which will also quit
-      // and end the server gracefully
-      _currentClientConnection->close();
-      std::cout << "Terminate signal detected while reading. Connection "
-                   "closed, will exit now." << std::endl;
-      break;
-    } else if (errorCode) {
-      throw boost::system::system_error(errorCode);
     }
-
-    processReceivedPackage(dataBuffer);
   }
-}
 
-void RebotDummyServer::stop(){
-  stop_rebot_server=true;
-}
+  /********************************************************************************************************************/
+
+  void RebotDummyServer::handleAcceptedConnection(boost::shared_ptr<ip::tcp::socket>& incomingSocket) {
+    boost::asio::ip::tcp::no_delay option(true);
+    incomingSocket->set_option(option);
+    _currentClientConnection = incomingSocket;
+
+    // This loop handles the accepted connection
+    while(stop_rebot_server == false) {
+
+      std::vector<uint32_t> dataBuffer(BUFFER_SIZE_IN_WORDS);
+      boost::system::error_code errorCode;
+      _currentClientConnection->read_some(boost::asio::buffer(dataBuffer), errorCode);
+
+      if(errorCode == boost::asio::error::eof) {
+        // The client has closed the connection; move to the outer loop to accept new connections
+        _currentClientConnection->close();
+        //std::cout << "connection closed" << std::endl;
+        break;
+      }
+      else if (errorCode && stop_rebot_server) {
+        /* reading was interrupted by a terminate signal; move to the outer loop which will also quit and end the
+         * server gracefully */
+        _currentClientConnection->close();
+        std::cout << "Terminate signal detected while reading. Connection closed, will exit now." << std::endl;
+        break;
+      }
+      else if (errorCode) {
+        throw boost::system::system_error(errorCode);
+      }
+
+      processReceivedPackage(dataBuffer);
+    }
+  }
+
+  /********************************************************************************************************************/
+
+  void RebotDummyServer::stop() {
+    stop_rebot_server = true;
+  }
 
 } /* namespace ChimeraTK */
