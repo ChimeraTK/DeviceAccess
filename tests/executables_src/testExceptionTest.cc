072f9cee7d878db51251d1b0bc2aa4ab931bd5b7
Martin Hierholzer <martin.hierholzer@desy.de>
2019-02-18 15:11:57 +0100

    - don't require a dmap file (which was missing) - use latest backend factory interface and the CDD - changed code formatting

--- tests/executables_src/testExceptionTest.cc
+++ tests/executables_src/testExceptionTest.cc
@@ -21,110 +21,99 @@
 using namespace boost::unit_test_framework;
 namespace ctk = ChimeraTK;
 
-
 /* Module which limits a value to stay below a maximum value. */
 struct ModuleB : public ctk::ApplicationModule {
-    using ctk::ApplicationModule::ApplicationModule;
+  using ctk::ApplicationModule::ApplicationModule;
 
-    ctk::ScalarPushInput<double> var1{this, "var1", "centimeters", "Some length, confined to a configuratble range"};
-    ctk::ScalarOutput<double> var2{this, "var2", "centimeters", "The limited length"};
+  ctk::ScalarPushInput<double> var1{this, "var1", "centimeters", "Some length, confined to a configuratble range"};
+  ctk::ScalarOutput<double> var2{this, "var2", "centimeters", "The limited length"};
 
-   
-    void mainLoop() {
-      auto group = readAnyGroup();
-      while(true) {
-        auto var = group.readAny();
-        if(var == var2.getId()) {
-          var1 = std::floor(var2/2.54);
-          var1.write();
-        }
-        var2 = var1*2.54;
-        var2.write();
+  void mainLoop() {
+    auto group = readAnyGroup();
+    while(true) {
+      auto var = group.readAny();
+      if(var == var2.getId()) {
+        var1 = std::floor(var2 / 2.54);
+        var1.write();
       }
+      var2 = var1 * 2.54;
+      var2.write();
     }
+  }
 };
 
 /*********************************************************************************************************************/
 
 /* dummy application */
 
-
 struct TestApplication : public ctk::Application {
-    TestApplication() : Application("testSuite") {}
-    ~TestApplication() { shutdown(); }
+  TestApplication() : Application("testSuite") {}
+  ~TestApplication() { shutdown(); }
 
-    using Application::makeConnections;     // we call makeConnections() manually in the tests to catch exceptions etc.
-    
-    void defineConnections() {
-      //testableMode = true;
-      debugTestableMode();
-      
-      dumpConnections(); }             // the setup is done in the tests
-    
-    ctk::DeviceModule dev{this,"Exception"};
-    //ModuleB   b;
-    ctk::ControlSystemModule cs;
-    
+  using Application::makeConnections; // we call makeConnections() manually in the tests to catch exceptions etc.
+
+  void defineConnections() {
+    // testableMode = true;
+    debugTestableMode();
+
+    dumpConnections();
+  } // the setup is done in the tests
+
+  ctk::DeviceModule dev{this, "(ExceptionDummy?map=DemoDummy.map)"};
+  // ModuleB   b;
+  ctk::ControlSystemModule cs;
 };
 
 /*********************************************************************************************************************/
 
-
 BOOST_AUTO_TEST_CASE(testThinkOfAName) {
   std::cout << "testThinkOfAName" << std::endl;
 
-  ChimeraTK::BackendFactory::getInstance().setDMapFilePath("dummy2.dmap");
-  
-  
   TestApplication app;
-  
-  //app.b = {&app,"b", ""};
-  
+
+  // app.b = {&app,"b", ""};
+
   /*app.b.var2 >> app.dev("probeSignal");
-  
+
   app.b.connectTo(app.cs);
-  
+
   app.cs("probeSignal") >> app.dev("probeSignal");
-  
+
   app.dev.connectTo(app.cs);
   //app.cs >> app.b.var1;*/
-  //app.cs("probeSignal") >> app.dev("probeSignal");
+  // app.cs("probeSignal") >> app.dev("probeSignal");
   app.dev.connectTo(app.cs);
-  std::cout<<"CONNECTED"<<std::endl;
+  std::cout << "CONNECTED" << std::endl;
   ctk::TestFacility test;
   app.initialise();
   app.run();
-  
-  std::cout<<"RUNNING"<<std::endl;
-  
-  
+
+  std::cout << "RUNNING" << std::endl;
+
   /*auto var1 = test.getScalar<uint32_t>("probeSignal");
   var1 = 10;
   var1.write();
   test.stepApplication();*/
-  //std::cout<<"STEP"<<std::endl;
-  //app.dev.reportException("exception");
-  
-  
-  
-  boost::shared_ptr< ExceptionDummy > backend = boost::dynamic_pointer_cast<ExceptionDummy>(
-      ChimeraTK::BackendFactory::getInstance().createBackend("Exception") );
+  // std::cout<<"STEP"<<std::endl;
+  // app.dev.reportException("exception");
+
+  boost::shared_ptr<ExceptionDummy> backend = boost::dynamic_pointer_cast<ExceptionDummy>(
+      ChimeraTK::BackendFactory::getInstance().createBackend("(ExceptionDummy?map=DemoDummy.map)"));
   backend->close();
-  std::cout<<"backend closed"<<std::endl;
-  backend->throwException = true;    
-  try{
+  std::cout << "backend closed" << std::endl;
+  backend->throwException = true;
+  try {
     backend->open();
     BOOST_FAIL("Exception expected.");
   }
   catch(ChimeraTK::runtime_error&) {
   }
-  std::cout<<"report an exception"<<std::endl;
+  std::cout << "report an exception" << std::endl;
   app.dev.reportException("exception");
   sleep(5);
-  backend->throwException = false; 
-  std::cout<<"THE END"<<std::endl;
+  backend->throwException = false;
+  std::cout << "THE END" << std::endl;
 
-  
   /*auto var2 = test.getScalar<double>("var2");
   var2.read();
   test.stepApplication();
--- tests/include/ExceptionDevice.h
+++ tests/include/ExceptionDevice.h
@@ -3,35 +3,33 @@
 #include <ChimeraTK/DeviceAccessVersion.h>
 
 class ExceptionDummy : public ChimeraTK::DummyBackend {
-  public:
-    ExceptionDummy(std::string mapFileName) : DummyBackend(mapFileName) {
-      std::cout<<"ExceptionDummy::ExceptionDummy"<<std::endl;
-      throwException = false;
-      }
-    bool throwException;
-    static boost::shared_ptr<DeviceBackend> createInstance(std::string, std::string, std::list<std::string> parameters, std::string) {
-      return boost::shared_ptr<DeviceBackend>(new ExceptionDummy(parameters.front()));
-    }
-    void open()override{
-      std::cout<<"ExceptionDummy opened called"<<std::endl;
-      if (throwException)
-        throw(ChimeraTK::runtime_error("DummyException: This is a test"));
-      else  
-        ChimeraTK::DummyBackend::open();  
-      }  
-    
+ public:
+  ExceptionDummy(std::string mapFileName) : DummyBackend(mapFileName) {
+    std::cout << "ExceptionDummy::ExceptionDummy" << std::endl;
+    throwException = false;
+  }
+  bool throwException;
+  static boost::shared_ptr<DeviceBackend> createInstance(std::string, std::map<std::string, std::string> parameters) {
+    return boost::shared_ptr<DeviceBackend>(new ExceptionDummy(parameters["map"]));
+  }
+  void open() override {
+    std::cout << "ExceptionDummy opened called" << std::endl;
+    if(throwException)
+      throw(ChimeraTK::runtime_error("DummyException: This is a test"));
+    else
+      ChimeraTK::DummyBackend::open();
+  }
 
-    class BackendRegisterer {
-      public:
-        BackendRegisterer();
-    };
-    static BackendRegisterer backendRegisterer;
+  class BackendRegisterer {
+   public:
+    BackendRegisterer();
+  };
+  static BackendRegisterer backendRegisterer;
 };
-
 
 ExceptionDummy::BackendRegisterer ExceptionDummy::backendRegisterer;
 
 ExceptionDummy::BackendRegisterer::BackendRegisterer() {
-    std::cout << "ExceptionDummy::BackendRegisterer: registering backend type ExceptionDummy" << std::endl;
-    ChimeraTK::BackendFactory::getInstance().registerBackendType("ExceptionDummy","",&ExceptionDummy::createInstance, CHIMERATK_DEVICEACCESS_VERSION);
+  std::cout << "ExceptionDummy::BackendRegisterer: registering backend type ExceptionDummy" << std::endl;
+  ChimeraTK::BackendFactory::getInstance().registerBackendType("ExceptionDummy", &ExceptionDummy::createInstance);
 }
