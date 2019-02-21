a344cfef01d62c342af6074ff646ab0c80e5ecd5
Nadeem Shehzad <nadeem.shehzad@desy.de>
2019-02-21 12:54:33 +0100

    test improved.

--- tests/executables_src/testExceptionTest.cc
+++ tests/executables_src/testExceptionTest.cc
@@ -21,34 +21,13 @@
 using namespace boost::unit_test_framework;
 namespace ctk = ChimeraTK;
 
-/* Module which limits a value to stay below a maximum value. */
-struct ModuleB : public ctk::ApplicationModule {
-  using ctk::ApplicationModule::ApplicationModule;
-
-  ctk::ScalarPushInput<double> var1{this, "var1", "centimeters", "Some length, confined to a configuratble range"};
-  ctk::ScalarOutput<double> var2{this, "var2", "centimeters", "The limited length"};
-
-  void mainLoop() {
-    auto group = readAnyGroup();
-    while(true) {
-      auto var = group.readAny();
-      if(var == var2.getId()) {
-        var1 = std::floor(var2 / 2.54);
-        var1.write();
-      }
-      var2 = var1 * 2.54;
-      var2.write();
-    }
-  }
-};
-
-/*********************************************************************************************************************/
-
 /* dummy application */
 
+//bool resetDevice = false;
 struct TestApplication : public ctk::Application {
   TestApplication() : Application("testSuite") {}
   ~TestApplication() { shutdown(); }
+  void onExceptionResetDevice();
 
   using Application::makeConnections; // we call makeConnections() manually in the tests to catch exceptions etc.
 
@@ -58,50 +37,37 @@ struct TestApplication : public ctk::Application {
 
     dumpConnections();
   } // the setup is done in the tests
-
+  
   ctk::DeviceModule dev{this, "(ExceptionDummy?map=DemoDummy.map)"};
-  // ModuleB   b;
   ctk::ControlSystemModule cs;
 };
 
+
+  
 /*********************************************************************************************************************/
 
+
+
 BOOST_AUTO_TEST_CASE(testThinkOfAName) {
-  std::cout << "testThinkOfAName" << std::endl;
-
+  
   TestApplication app;
-
-  // app.b = {&app,"b", ""};
-
-  /*app.b.var2 >> app.dev("probeSignal");
-
-  app.b.connectTo(app.cs);
-
-  app.cs("probeSignal") >> app.dev("probeSignal");
-
+  
   app.dev.connectTo(app.cs);
-  //app.cs >> app.b.var1;*/
-  // app.cs("probeSignal") >> app.dev("probeSignal");
-  app.dev.connectTo(app.cs);
-  app.dev.deviceError.connectTo(app.cs["DeviceError"]);
-  std::cout << "CONNECTED" << std::endl;
+  //app.dev.deviceError.connectTo(app.cs["DeviceError"]);
   ctk::TestFacility test;
   app.initialise();
   app.run();
-
-  std::cout << "RUNNING" << std::endl;
-
-  /*auto var1 = test.getScalar<uint32_t>("probeSignal");
-  var1 = 10;
-  var1.write();
-  test.stepApplication();*/
-  // std::cout<<"STEP"<<std::endl;
-  // app.dev.reportException("exception");
-
+  auto message = test.getScalar<std::string>("/Devices.(ExceptionDummy?map=DemoDummy.map)/DeviceError/message");
+  auto status = test.getScalar<int>("/Devices.(ExceptionDummy?map=DemoDummy.map)/DeviceError/status");
+  
+  message.readLatest();
+  status.readLatest();
+  BOOST_CHECK(static_cast<std::string>(message) == "");
+  BOOST_CHECK(status.readLatest() == 0);
+  
   boost::shared_ptr<ExceptionDummy> backend = boost::dynamic_pointer_cast<ExceptionDummy>(
       ChimeraTK::BackendFactory::getInstance().createBackend("(ExceptionDummy?map=DemoDummy.map)"));
-  backend->close();
-  std::cout << "backend closed" << std::endl;
+      backend->close();
   backend->throwException = true;
   try {
     backend->open();
@@ -109,14 +75,30 @@ BOOST_AUTO_TEST_CASE(testThinkOfAName) {
   }
   catch(ChimeraTK::runtime_error&) {
   }
-  std::cout << "report an exception" << std::endl;
-  app.dev.reportException("exception");
-  sleep(5);
+  std::atomic<bool> reportExceptionFinished;
+  reportExceptionFinished = false;
+  std::thread reportThread([&]{
+    app.dev.reportException("exception");
+    reportExceptionFinished = true;
+  });
+  sleep(2);
+  
+  message.readLatest();
+  status.readLatest();
+  BOOST_CHECK(static_cast<std::string>(message) != "");
+  BOOST_CHECK(status == 1);
+  BOOST_CHECK(reportExceptionFinished == false);
   backend->throwException = false;
-  std::cout << "THE END" << std::endl;
-
-  /*auto var2 = test.getScalar<double>("var2");
-  var2.read();
-  test.stepApplication();
-  std::cout<<var2<<std::endl;*/
+  reportThread.join();
+  
+  BOOST_CHECK(reportExceptionFinished == true);
+  message.readLatest();
+  status.readLatest();
+  BOOST_CHECK(static_cast<std::string>(message) == "");
+  BOOST_CHECK(status.readLatest() == 0);
+  
+  
+  
+  
+  
 }
