51d356c0d260aae01e6286eea8ebc7785f7393c6
Martin Hierholzer <martin.hierholzer@desy.de>
2019-02-22 11:44:43 +0100

    added missing header files

--- tests/RebotDummyServer/include/argumentParser.h
+++ tests/RebotDummyServer/include/argumentParser.h
@@ -1,8 +1,10 @@
 #ifndef SOURCE_DIRECTORY__TESTS_REBOTDUMMYSERVER_ARGUMENTPARSER_H_
 #define SOURCE_DIRECTORY__TESTS_REBOTDUMMYSERVER_ARGUMENTPARSER_H_
 
-unsigned int getPortNumber(char **);
-std::string getMapFileLocation(char **);
-unsigned int getProtocolVersion(char **);
+#include <string>
+
+unsigned int getPortNumber(char**);
+std::string getMapFileLocation(char**);
+unsigned int getProtocolVersion(char**);
 
 #endif /* SOURCE_DIRECTORY__TESTS_REBOTDUMMYSERVER_ARGUMENTPARSER_H_ */
