e5f79f9935df900cf5eac302cc63849429bc6ea1
Martin Killenberg <martin.killenberg@desy.de>
2019-02-27 17:08:53 +0100

    added tests for IEEE754 in NumericAddressedBackendRegisterAccessor. Here the raw accessor is failing (known issue, just tests were missing)

--- tests/executables_src/testNumericAddressedBackendRegisterAccessor.cpp
+++ tests/executables_src/testNumericAddressedBackendRegisterAccessor.cpp
@@ -246,6 +246,7 @@ BOOST_AUTO_TEST_CASE(testConverterTypes){
   Device device;
   device.open(deviceDescriptor);
 
+  // FixedPointConverter, raw and coocked accessors
   // MODULE0.WORD_USER1 is fixed point, 16 bit, 3 fractional, signed
   auto user1Dummy = dummyBackend->getRawAccessor("MODULE0","WORD_USER1");
   user1Dummy = 0x4321;
@@ -282,6 +283,66 @@ BOOST_AUTO_TEST_CASE(testConverterTypes){
   user1CoockedInt.write();
 
   BOOST_CHECK_EQUAL( int32_t(user1Dummy), 0x80);
+
+  // IEEE754 converter, raw and coocked accessors
+  // FLOAT_TEST.ARRAY is IEEE754. We use the 1 D version in constrast to FixedPoint where we use scalar (just because we can)
+  auto floatTestDummy = dummyBackend->getRawAccessor("FLOAT_TEST","ARRAY");
+
+  float testValue = 1.1;
+  void* warningAvoider = &testValue; // directly reinterpret-casting float gives compiler warnings
+  floatTestDummy[0] = *(reinterpret_cast<int32_t*>(warningAvoider));
+  testValue = 2.2;
+  floatTestDummy[1] = *(reinterpret_cast<int32_t*>(warningAvoider));
+  testValue = 3.3;
+  floatTestDummy[2] = *(reinterpret_cast<int32_t*>(warningAvoider));
+  testValue = 4.4;
+  floatTestDummy[3] = *(reinterpret_cast<int32_t*>(warningAvoider));
+
+  auto floatTestCoocked = device.getOneDRegisterAccessor<float>("FLOAT_TEST/ARRAY");
+  floatTestCoocked.read();
+
+  BOOST_CHECK_CLOSE( floatTestCoocked[0], 1.1, 0.0001);
+  BOOST_CHECK_CLOSE( floatTestCoocked[1], 2.2, 0.0001);
+  BOOST_CHECK_CLOSE( floatTestCoocked[2], 3.3, 0.0001);
+  BOOST_CHECK_CLOSE( floatTestCoocked[3], 4.4, 0.0001);
+
+  floatTestCoocked[3] = 44.4;
+  floatTestCoocked.write();
+
+  *(reinterpret_cast<int32_t*>(warningAvoider)) = floatTestDummy[3];
+  BOOST_CHECK_CLOSE( testValue, 44.4, 0.0001);
+
+  auto floatTestRaw = device.getOneDRegisterAccessor<int32_t>("FLOAT_TEST/ARRAY", 0, 0, {AccessMode::raw});
+  floatTestRaw.read();
+
+  *(reinterpret_cast<int32_t*>(warningAvoider)) = floatTestRaw[2];
+ 
+  BOOST_CHECK_CLOSE(testValue, 3.3, 0.0001);
+  BOOST_CHECK_CLOSE( floatTestRaw.getAsCooked<float>(0), 1.1, 0.0001);
+  
+  floatTestRaw.setAsCooked(0,-2.5);
+
+  floatTestRaw.write();
+
+  *(reinterpret_cast<int32_t*>(warningAvoider)) = floatTestRaw[0];
+
+  BOOST_CHECK_CLOSE( testValue, -2.5, 0.0001);
+
+  // special case: int32 does not necessarily mean raw. There is also a cooked version:
+  auto floatTestCoockedInt = device.getOneDRegisterAccessor<int32_t>("FLOAT_TEST/ARRAY");
+  floatTestCoockedInt.read();
+
+  BOOST_CHECK_EQUAL( floatTestCoockedInt[0], -3); // was -2.5
+  BOOST_CHECK_EQUAL( floatTestCoockedInt[1], 2); // was 2.2
+  BOOST_CHECK_EQUAL( floatTestCoockedInt[2], 3); // was 3.3
+  BOOST_CHECK_EQUAL( floatTestCoockedInt[3], 44); // was 44.4
+
+  floatTestCoockedInt[1] = 16;
+  floatTestCoockedInt.write();
+
+  *(reinterpret_cast<int32_t*>(warningAvoider)) = floatTestRaw[1];
+  BOOST_CHECK_CLOSE( testValue, 16.0, 0.001);
+
 }
 
 // After you finished all test you have to end the test suite.
