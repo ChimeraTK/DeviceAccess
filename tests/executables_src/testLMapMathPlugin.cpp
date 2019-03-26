c65daebdbf40069406b705a6cab0b8a57dd5425c
Martin Hierholzer <martin.hierholzer@desy.de>
2019-03-26 17:48:03 +0100

    lmap math plugin: allow using and not using a return statement in the math formula

--- tests/executables_src/testLMapMathPlugin.cpp
+++ tests/executables_src/testLMapMathPlugin.cpp
@@ -12,20 +12,63 @@ BOOST_AUTO_TEST_SUITE(LMapMathPluginTestSuite)
 
 /********************************************************************************************************************/
 
-BOOST_AUTO_TEST_CASE(testReadScalar) {
+BOOST_AUTO_TEST_CASE(testSimpleReadWriteScalar) {
   ChimeraTK::Device device;
   device.open("(logicalNameMap?map=mathPlugin.xlmap)");
 
-  ChimeraTK::Device target;
-  target.open("(dummy?map=mtcadummy.map)");
+  auto accTarget = device.getScalarRegisterAccessor<int>("SimpleScalar");
+  auto accMath = device.getScalarRegisterAccessor<double>("SimpleScalarReadWrite");
 
-  auto accTarget = target.getScalarRegisterAccessor<int>("BOARD.WORD_USER");
-  auto accMath = device.getScalarRegisterAccessor<double>("SimpleScalarRead");
-
-  accTarget = 42;
+  accTarget = 45;
   accTarget.write();
   accMath.read();
-  BOOST_CHECK_CLOSE(double(accMath), 42. / 7. + 13., 0.00001);
+  BOOST_CHECK_CLOSE(double(accMath), 45. / 7. + 13., 0.00001);
+
+  accTarget = -666;
+  accTarget.write();
+  accMath.read();
+  BOOST_CHECK_CLOSE(double(accMath), -666. / 7. + 13., 0.00001);
+
+  accMath = 77.;
+  accMath.write();
+  accTarget.read();
+  BOOST_CHECK_EQUAL(int(accTarget), 24); // 77/7 + 13
+
+  accMath = -140.;
+  accMath.write();
+  accTarget.read();
+  BOOST_CHECK_EQUAL(int(accTarget), -7); // -140/7 + 13
+}
+
+/********************************************************************************************************************/
+
+BOOST_AUTO_TEST_CASE(testReturnedReadWriteScalar) {
+  ChimeraTK::Device device;
+  device.open("(logicalNameMap?map=mathPlugin.xlmap)");
+
+  auto accTarget = device.getScalarRegisterAccessor<int>("SimpleScalar");
+  // ReturnedScalarReadWrite uses a return statement, but is otherwise identical to the "simple" version
+  auto accMath = device.getScalarRegisterAccessor<double>("ReturnedScalarReadWrite");
+
+  accTarget = 45;
+  accTarget.write();
+  accMath.read();
+  BOOST_CHECK_CLOSE(double(accMath), 45. / 7. + 13., 0.00001);
+
+  accTarget = -666;
+  accTarget.write();
+  accMath.read();
+  BOOST_CHECK_CLOSE(double(accMath), -666. / 7. + 13., 0.00001);
+
+  accMath = 77.;
+  accMath.write();
+  accTarget.read();
+  BOOST_CHECK_EQUAL(int(accTarget), 24); // 77/7 + 13
+
+  accMath = -140.;
+  accMath.write();
+  accTarget.read();
+  BOOST_CHECK_EQUAL(int(accTarget), -7); // -140/7 + 13
 }
 
 /********************************************************************************************************************/
