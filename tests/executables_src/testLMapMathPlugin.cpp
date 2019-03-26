d6c96fb96ee5fa6040fc0ab91c3b4033d69f3fb9
Martin Hierholzer <martin.hierholzer@desy.de>
2019-03-26 18:09:42 +0100

    lmap math plugin: test also with arrays

--- tests/executables_src/testLMapMathPlugin.cpp
+++ tests/executables_src/testLMapMathPlugin.cpp
@@ -73,6 +73,37 @@ BOOST_AUTO_TEST_CASE(testReturnedReadWriteScalar) {
 
 /********************************************************************************************************************/
 
+BOOST_AUTO_TEST_CASE(testReadWriteArray) {
+  ChimeraTK::Device device;
+  device.open("(logicalNameMap?map=mathPlugin.xlmap)");
+
+  auto accTarget = device.getOneDRegisterAccessor<int>("SimpleArray");
+  auto accMath = device.getOneDRegisterAccessor<double>("ArrayReadWrite");
+  BOOST_CHECK_EQUAL(accMath.getNElements(), 6);
+
+  accTarget = {11, 22, 33, 44, 55, 66};
+  accTarget.write();
+  accMath.read();
+  for(size_t i = 0; i < 6; ++i) BOOST_CHECK_CLOSE(double(accMath[i]), accTarget[i] / 7. + 13., 0.00001);
+
+  accTarget = {-120, 123456, -18, 9999, -999999999, 0};
+  accTarget.write();
+  accMath.read();
+  for(size_t i = 0; i < 6; ++i) BOOST_CHECK_CLOSE(double(accMath[i]), accTarget[i] / 7. + 13., 0.00001);
+
+  accMath = {-120, 123456, -18, 9999, -999999999, 0};
+  accMath.write();
+  accTarget.read();
+  for(size_t i = 0; i < 6; ++i) BOOST_CHECK_EQUAL(double(accTarget[i]), std::round(accMath[i] / 7. + 13.));
+
+  accMath = {0, 1, 2, 3, 4, 5};
+  accMath.write();
+  accTarget.read();
+  for(size_t i = 0; i < 6; ++i) BOOST_CHECK_EQUAL(double(accTarget[i]), std::round(accMath[i] / 7. + 13.));
+}
+
+/********************************************************************************************************************/
+
 BOOST_AUTO_TEST_CASE(testExceptions) {}
 
 /********************************************************************************************************************/
