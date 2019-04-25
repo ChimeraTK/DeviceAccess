835e91540dd7b35c8499f31b70eb23409290c363
Martin Killenberg <martin.killenberg@desy.de>
2019-04-25 14:39:27 +0200

    - renamed some variables and prepared to have second ExceptionDummy in testExceptionHandling   no change to the test sequence yet

--- tests/executables_src/testExceptionHandling.cc
+++ tests/executables_src/testExceptionHandling.cc
@@ -21,6 +21,9 @@
 using namespace boost::unit_test_framework;
 namespace ctk = ChimeraTK;
 
+constexpr char ExceptionDummyCDD1[] = "(ExceptionDummy:1?map=test3.map)";
+constexpr char ExceptionDummyCDD2[] = "(ExceptionDummy:2?map=test3.map)";
+
 /* dummy application */
 
 struct TestApplication : public ctk::Application {
@@ -29,12 +32,13 @@ struct TestApplication : public ctk::Application {
 
   void defineConnections() {} // the setup is done in the tests
 
-  ctk::DeviceModule dev{this, "(ExceptionDummy?map=test3.map)"};
+  ctk::DeviceModule dev1{this, ExceptionDummyCDD1};
+  ctk::DeviceModule dev2{this, ExceptionDummyCDD2};
   ctk::ControlSystemModule cs;
 };
 
 /*********************************************************************************************************************/
-#if 0
+#if 1
 // ************************************************************************************************
 // Note: This test partially tests implementation details. It probably should be removed.
 // ************************************************************************************************
@@ -43,14 +47,14 @@ BOOST_AUTO_TEST_CASE(testDeviceModuleReportExceptionFunction) {
 
   TestApplication app;
   boost::shared_ptr<ExceptionDummy> backend = boost::dynamic_pointer_cast<ExceptionDummy>(
-      ChimeraTK::BackendFactory::getInstance().createBackend("(ExceptionDummy?map=test3.map)"));
+      ChimeraTK::BackendFactory::getInstance().createBackend(ExceptionDummyCDD1));
 
-  app.dev.connectTo(app.cs, app.cs["MyModule"]("actuator"));
+  app.dev1.connectTo(app.cs, app.cs["MyModule"]("actuator"));
   ctk::TestFacility test;
   test.runApplication();
 
-  auto message = test.getScalar<std::string>("/Devices/(ExceptionDummy?map=test3.map)/message");
-  auto status = test.getScalar<int>("/Devices/(ExceptionDummy?map=test3.map)/status");
+  auto message = test.getScalar<std::string>(std::string("/Devices/")+ExceptionDummyCDD1+"/message");
+  auto status = test.getScalar<int>(std::string("/Devices/")+ExceptionDummyCDD1+"/status");
   auto trigger = test.getScalar<int>("/MyModule/actuator");
 
   // initially there should be no error set
@@ -85,7 +89,7 @@ BOOST_AUTO_TEST_CASE(testDeviceModuleReportExceptionFunction) {
   reportExceptionFinished = false;
   std::thread reportThread([&] { // need to launch in background, reportException() blocks
     app.testableModeLock("");
-    app.dev.reportException("Some fancy exception text");
+    app.dev1.reportException("Some fancy exception text");
     reportExceptionFinished = true;
   });
 
@@ -122,43 +126,46 @@ BOOST_AUTO_TEST_CASE(testDeviceModuleReportExceptionFunction) {
 BOOST_AUTO_TEST_CASE(testExceptionHandling) {
   std::cout << "testExceptionHandling" << std::endl;
   TestApplication app;
-  boost::shared_ptr<ExceptionDummy> backend = boost::dynamic_pointer_cast<ExceptionDummy>(
-      ChimeraTK::BackendFactory::getInstance().createBackend("(ExceptionDummy?map=test3.map)"));
+  boost::shared_ptr<ExceptionDummy> backend1 = boost::dynamic_pointer_cast<ExceptionDummy>(
+      ChimeraTK::BackendFactory::getInstance().createBackend(ExceptionDummyCDD1));
 
-  app.dev.connectTo(app.cs, app.cs["MyModule"]("actuator"));
+  // Connect the whole device into the control system, and use the control system variable /MyModule/actuator as trigger.
+  // The variable becomes a control system to application variable and writing to it through the test facility is generating the triggers.
+  app.dev1.connectTo(app.cs, app.cs["MyModule"]("actuator"));
+
   ctk::TestFacility test;
   test.runApplication();
 
-  auto message = test.getScalar<std::string>("/Devices/(ExceptionDummy?map=test3.map)/message");
-  auto status = test.getScalar<int>("/Devices/(ExceptionDummy?map=test3.map)/status");
+  auto message1 = test.getScalar<std::string>(std::string("/Devices/")+ExceptionDummyCDD1+"/message");
+  auto status1 = test.getScalar<int>(std::string("/Devices/")+ExceptionDummyCDD1+"/status");
   auto trigger = test.getScalar<int>("/MyModule/actuator");
 
   // initially there should be no error set
-  message.readLatest();
-  status.readLatest();
-  BOOST_CHECK(static_cast<std::string>(message) == "");
-  BOOST_CHECK(status.readLatest() == 0);
+  message1.readLatest();
+  status1.readLatest();
+  BOOST_CHECK(static_cast<std::string>(message1) == "");
+  BOOST_CHECK(status1.readLatest() == 0);
 
   // repeat test a couple of times to make sure it works not only once
   for(size_t i = 0; i < 10; ++i) {
     // enable exception throwing in test device
-    backend->throwException = true;
+    backend1->throwException = true;
     trigger.write();
     test.stepApplication();
-    message.readLatest();
-    status.readLatest();
-    BOOST_CHECK(static_cast<std::string>(message) != "");
-    BOOST_CHECK(status == 1);
-    BOOST_CHECK(!backend->isOpen());
+    message1.readLatest();
+    status1.readLatest();
+    BOOST_CHECK(static_cast<std::string>(message1) != "");
+    BOOST_CHECK_EQUAL(status1, 1);
+    BOOST_CHECK(!backend1->isOpen());
 
     // Now "cure" the device problem
-    backend->throwException = false;
+    backend1->throwException = false;
     trigger.write();
     test.stepApplication();
-    message.readLatest();
-    status.readLatest();
-    BOOST_CHECK(static_cast<std::string>(message) == "");
-    BOOST_CHECK(status == 0);
-    BOOST_CHECK(backend->isOpen());
+    message1.readLatest();
+    status1.readLatest();
+    BOOST_CHECK_EQUAL(static_cast<std::string>(message1), "");
+    BOOST_CHECK_EQUAL(status1, 0);
+    BOOST_CHECK(backend1->isOpen());
   }
 }
