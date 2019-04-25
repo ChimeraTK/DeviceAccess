e1f5f10fdcd63f7d8b46df3b1c49859baad94b0a
Martin Hierholzer <martin.hierholzer@desy.de>
2019-04-25 12:48:08 +0200

    fixed bug in last commit

--- device_backends/LogicalNameMapping/src/LNMForcePollingReadPlugin.cc
+++ device_backends/LogicalNameMapping/src/LNMForcePollingReadPlugin.cc
@@ -2,6 +2,7 @@
 
 #include "LNMBackendRegisterInfo.h"
 #include "LNMAccessorPlugin.h"
+#include "NDRegisterAccessor.h"
 
 namespace ChimeraTK { namespace LNMBackend {
 
@@ -22,14 +23,31 @@ namespace ChimeraTK { namespace LNMBackend {
 
   /********************************************************************************************************************/
 
+  /** Helper class to implement MonostableTriggerPluginPlugin::decorateAccessor (can later be realised with if constexpr) */
   template<typename UserType, typename TargetType>
-  boost::shared_ptr<NDRegisterAccessor<UserType>> ForceReadOnlyPlugin::decorateAccessor(
+  struct ForcePollingReadPlugin_Helper {
+    static boost::shared_ptr<NDRegisterAccessor<UserType>> decorateAccessor(
+        boost::shared_ptr<NDRegisterAccessor<TargetType>>&) {
+      assert(false); // only specialisation is valid
+      return {};
+    }
+  };
+  template<typename UserType>
+  struct ForcePollingReadPlugin_Helper<UserType, UserType> {
+    static boost::shared_ptr<NDRegisterAccessor<UserType>> decorateAccessor(
+        boost::shared_ptr<NDRegisterAccessor<UserType>>& target) {
+      return target;
+    }
+  };
+
+  template<typename UserType, typename TargetType>
+  boost::shared_ptr<NDRegisterAccessor<UserType>> ForcePollingReadPlugin::decorateAccessor(
       boost::shared_ptr<LogicalNameMappingBackend>&, boost::shared_ptr<NDRegisterAccessor<TargetType>>& target) const {
     if(target->getAccessModeFlags().has(AccessMode::wait_for_new_data)) {
       throw ChimeraTK::logic_error(
           "AccessMode::wait_for_new_data is disallowed through ForcePollingReadPlugin for register '" +
           target->getName() + "'.");
     }
-    return target;
+    return ForcePollingReadPlugin_Helper<UserType, TargetType>::decorateAccessor(target);
   }
 }} // namespace ChimeraTK::LNMBackend
