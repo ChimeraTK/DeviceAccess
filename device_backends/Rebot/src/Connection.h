1c607ca257d6fbc544bb2009024e2e5ad737792b
vargheseg <vargheseg@noreply.github.com>
2019-08-23 17:51:57 +0200

    use boost asio async api for open/read/write.

--- device_backends/Rebot/src/Connection.h
+++ device_backends/Rebot/src/Connection.h
@@ -11,40 +11,37 @@
 #include <vector>
 
 namespace ChimeraTK {
-namespace  Rebot {
+namespace Rebot {
 
   /// Handles the communication over TCP protocol with RebotDevice-based devices
   class Connection {
-   public:
-    /// Gets an IP address and port of the device but does not open the connection
-    Connection(std::string ipaddr, std::string port);
-    ~Connection();
+  public:
+    /// Gets an IP address and port of the device but does not open the
+    /// connection
+    Connection(const std::string& address, const std::string& port,
+               uint32_t connectionTimeout_sec = 10);
+
     /// Opens a connection to the device.
     void open();
+
     /// Closes a connection with the device.
     void close();
+
     /// Receives uint32_t words from the socket.
-    std::vector<uint32_t> read(uint32_t const& numWordsToRead);
+    std::vector<uint32_t> read(uint32_t numWordsToRead);
+
     /// Sends a std::vector of bytes to the socket.
     void write(const std::vector<uint32_t>& data);
 
   private:
-    std::string _serverAddress;
-    std::string _port;
-    boost::shared_ptr<boost::asio::io_service> _io_service;
-    boost::shared_ptr<boost::asio::ip::tcp::socket> _socket;
-
-    /*!
-     * @brief connect to the first good endpoint from a list of endpoints.
-     * The endpoint list is accessed through the provided Iterator
-     *
-     * The code in this method is based on  boost::asio::connect. This convenience
-     * function is not available in the version of the boost library (1.46) used
-     * currently.
-     */
-    boost::system::error_code connectToResolvedEndPoints(boost::asio::ip::tcp::resolver::iterator endpointIterator);
+    std::string address_;
+    const std::string port_;
+    boost::asio::io_service ioService_;
+    boost::asio::ip::tcp::socket s_;
+    boost::asio::deadline_timer disconnectTimer_;
+    boost::asio::deadline_timer::duration_type connectionTimeout_;
   };
-} // Rebot
+} // namespace Rebot
 
 } // namespace ChimeraTK
 
