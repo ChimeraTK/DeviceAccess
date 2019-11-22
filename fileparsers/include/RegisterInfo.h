88e1e473c2abfb9b66cdc3ea5e8f467d29ca2b11
Martin Killenberg <martin.killenberg@desy.de>
2019-11-22 17:14:19 +0100

    Fixed nDigits and nFractionalDigits in constructor of RegisterInfo::DataDescriptor. No tests yet.

--- fileparsers/include/RegisterInfo.h
+++ fileparsers/include/RegisterInfo.h
@@ -242,8 +242,43 @@ namespace ChimeraTK {
     }
     _isIntegral = type.isIntegral();
     _isSigned = type.isSigned();
-    _nDigits = 0; /// @todo FIXME fill properly!
-    _nFractionalDigits = 0;
+
+    // the defaults
+    _nDigits = 0;           // invalid for non handled types
+    _nFractionalDigits = 0; // 0 for integers
+
+    if(type == DataType::int8) { // 8 bit signed
+      _nDigits = 4;              // -127 .. 128
+    }
+    if(type == DataType::uint8) { // 8 bit unsigned
+      _nDigits = 3;               // 0..256
+    }
+    else if(type == DataType::int16) { // 16 bit signed
+      _nDigits = 6;                    // -32000 .. 32000 (approx)
+    }
+    else if(type == DataType::uint16) { // 16 bit unsigned
+      _nDigits = 6;                     // 0 .. 65000 (approx)
+    }
+    else if(type == DataType::int32) { // 32 bit signed
+      _nDigits = 11;                   // -2e9 .. 2e9 (approx)
+    }
+    else if(type == DataType::uint32) { // 32 bit unsigned
+      _nDigits = 10;                    // 0 .. 4e9 (approx)
+    }
+    else if(type == DataType::int64) { // 64 bit signed
+      _nDigits = 20;                   // -9e18 .. 9e18 (approx)
+    }
+    else if(type == DataType::uint64) { // 64 bit unsigned
+      _nDigits = 20;                    // 0 .. 2e19 (approx)
+    }
+    else if(type == DataType::float32) { // 32 bit float IEEE 754
+      _nDigits = 3 + 45;                 // see RegisterInfoMap::RegisterInfo::RegisterInfo
+      _nFractionalDigits = 45;
+    }
+    else if(type == DataType::float64) { // 64 bit float IEEE 754
+      _nDigits = 3 + 325;                // see RegisterInfoMap::RegisterInfo::RegisterInfo
+      _nFractionalDigits = 325;
+    }
   }
 
   /*******************************************************************************************************************/
