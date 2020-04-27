3b6c144bb9fb09d8ed237303c642940f0c2fed12
Christoph Kampmeyer <christoph.kampmeyer@desy.de>
2020-04-27 20:38:23 +0200

    wip #107: Use getVirtualQualifiedName() in StatusAggregator

--- Modules/src/StatusAggregator.cc
+++ Modules/src/StatusAggregator.cc
@@ -1,6 +1,7 @@
 #include "StatusAggregator.h"
 
 #include <list>
+#include <regex>
 
 namespace ChimeraTK{
 
@@ -22,39 +23,44 @@ namespace ChimeraTK{
 //    }
 
 
-    // Works, as long as we do not use HIerarchyModifiers other than "none" in the test app
-     // Update: This still has problems, because getOwner gives us the "real" C++ Module
-    //          We need to do everything on the virtual plane
-//    auto allAccessors{getOwner()->findTag(".*").getAccessorListRecursive()};
-
-//    std::cout <<  "  Size of allAccessors: " << allAccessors.size() << std::endl;
-
-//    for(auto acc : allAccessors){
-
-//      if(acc.getDirection().dir == VariableDirection::feeding){
-//        std::cout << "      -- Accessor: " << acc.getName()
-//                  << " of module: " << acc.getOwningModule()->getName()
-//                  << std::endl;
-//      }
-
-//    }
-
-
     // Another try, virtualise the entire Application
     auto virtualApplicationHierarchy = Application::getInstance().findTag(".*");
+    auto virtualPathToThis = getVirtualQualifiedName();
     std::cout << "  ** Fully qualified name in vitual hierarchy: "
-              << virtualApplicationHierarchy.getQualifiedName() << std::endl;
+              //<< virtualApplicationHierarchy.getQualifiedName() << std::endl;
+              << getVirtualQualifiedName() << std::endl;
 
-    virtualApplicationHierarchy.virtualise();
-    auto virtualizedModuleList = virtualApplicationHierarchy.getSubmoduleListRecursive();
+    // Split the name into individual modules
+    std::regex pathsepRegex{"/"};
+    std::sregex_token_iterator name{virtualPathToThis.begin(), virtualPathToThis.end(),
+        pathsepRegex, -1}, end;
 
-    for(auto module : virtualizedModuleList){
-      std::cout << "  ** " << module->getQualifiedName();
-      if (module == this){
-        std::cout << "   THIS IS THIS STATUS_AGGREGATOR!";
-      }
-      std::cout << std::endl;
+
+    ++name; // Leave out first, empty match. FIXME: Should be avoided by regex
+    while(name != end){
+      std::cout << "Current path elem: " << *(name++) << std::endl;
     }
+
+    // I can just call submodule instead of the above procedure
+    //Module& thisModule = virtualApplicationHierarchy.submodule(virtualPathToThis);
+
+
+    // Works, as long as we do not use HIerarchyModifiers other than "none" in the test app
+    // Update: This still has problems, because getOwner gives us the "real" C++ Module
+    //          We need to do everything on the virtual plane
+    //    auto allAccessors{getOwner()->findTag(".*").getAccessorListRecursive()};
+
+    //    std::cout <<  "  Size of allAccessors: " << allAccessors.size() << std::endl;
+
+    //    for(auto acc : allAccessors){
+
+    //      if(acc.getDirection().dir == VariableDirection::feeding){
+    //        std::cout << "      -- Accessor: " << acc.getName()
+    //                  << " of module: " << acc.getOwningModule()->getName()
+    //                  << std::endl;
+    //      }
+
+    //    }
 
 
 
