915819cebadcecce4d7d812462039707f1721fb6
Martin Hierholzer <martin.hierholzer@desy.de>
2020-07-03 17:39:23 +0200

    fix formatting

--- device/include/TwoDRegisterAccessor.h
+++ device/include/TwoDRegisterAccessor.h
@@ -19,16 +19,13 @@ namespace ChimeraTK {
   template<class UserType>
   class TwoDRegisterAccessor : public NDRegisterAccessorAbstractor<UserType> {
    public:
-    /** Do not use this constructor directly. Instead call
-     * Device::getTwoDRegisterAccessor(). */
+    /** Do not use this constructor directly. Instead call Device::getTwoDRegisterAccessor(). */
     TwoDRegisterAccessor(boost::shared_ptr<NDRegisterAccessor<UserType>> _accessor)
     : NDRegisterAccessorAbstractor<UserType>(_accessor) {}
 
-    /** Placeholder constructer, to allow late initialisation of the accessor,
-     * e.g. in the open function.
-     *  @attention Accessors created with this constructors will be dysfunctional,
-     * calling any member function will throw an exception (by the
-     * boost::shared_ptr)! */
+    /** Placeholder constructer, to allow late initialisation of the accessor, e.g. in the open function.
+     *  @attention Accessors created with this constructors will be dysfunctional,  calling any member function will
+     *  throw an exception (by the boost::shared_ptr)! */
     TwoDRegisterAccessor() {}
 
     /** Operator to access individual sequences/channels. */
@@ -43,19 +40,15 @@ namespace ChimeraTK {
     /** Return number of elements/samples per channel */
     size_t getNElementsPerChannel() const { return get()->getNumberOfSamples(); }
 
-    /** Get the cooked values in case the accessor is a raw accessor (which does
-     * not do data conversion). This returns the converted data from the use
-     * buffer. It does not do any read or write transfer.
-     */
+    /** Get the cooked values in case the accessor is a raw accessor (which does not do data conversion). This returns
+     *  the converted data from the use buffer. It does not do any read or write transfer. */
     template<typename COOKED_TYPE>
     COOKED_TYPE getAsCooked(unsigned int channel, unsigned int sample) const {
       return get()->template getAsCooked<COOKED_TYPE>(channel, sample);
     }
 
-    /** Set the cooked values in case the accessor is a raw accessor (which does
-     * not do data conversion). This converts to raw and writes the data to the
-     * user buffer. It does not do any read or write transfer.
-     */
+    /** Set the cooked values in case the accessor is a raw accessor (which does not do data conversion). This converts
+     *  to raw and writes the data to the user buffer. It does not do any read or write transfer. */
     template<typename COOKED_TYPE>
     void setAsCooked(unsigned int channel, unsigned int sample, COOKED_TYPE value) {
       return get()->template setAsCooked<COOKED_TYPE>(channel, sample, value);
