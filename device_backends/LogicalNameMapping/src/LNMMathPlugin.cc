e1fe3fc38bf422ece018481a2658841549b4f17b
Martin Hierholzer <martin.hierholzer@desy.de>
2020-10-28 17:22:39 +0100

    LNM MathPlugin: move code into a new internal class to avoid code duplication

--- device_backends/LogicalNameMapping/src/LNMMathPlugin.cc
+++ device_backends/LogicalNameMapping/src/LNMMathPlugin.cc
@@ -39,6 +39,111 @@ namespace ChimeraTK { namespace LNMBackend {
 
   /********************************************************************************************************************/
 
+  struct MathPluginFormulaHelper {
+    std::string varName;
+    exprtk::expression<double> expression;
+    exprtk::symbol_table<double> symbols;
+    std::unique_ptr<exprtk::vector_view<double>> valueView;
+    std::map<boost::shared_ptr<NDRegisterAccessor<double>>, std::unique_ptr<exprtk::vector_view<double>>> params;
+
+    void compileFormula(const boost::shared_ptr<LogicalNameMappingBackend>& backend,
+        const std::map<std::string, std::string>& parameters,
+        const boost::shared_ptr<ChimeraTK::NDRegisterAccessor<double>>& target) {
+      // create exprtk parser
+      exprtk::parser<double> parser;
+
+      // add basic constants like pi
+      symbols.add_constants();
+
+      // Create vector view for the value and add it to the symbol table. We need to use a vector view instead of adding
+      // the buffer directly as a vector, since our buffers might be swapped and hence the address of the data can
+      // change.
+      valueView = std::make_unique<exprtk::vector_view<double>>(
+          exprtk::make_vector_view(target->accessChannel(0), target->accessChannel(0).size()));
+      symbols.add_vector("x", *valueView);
+
+      // iterate parameters, add all but 'formula' parameter as a variable.
+      for(auto& parpair : parameters) {
+        if(parpair.first == "formula") continue;
+        auto acc = backend->getRegisterAccessor<double>(parpair.second, 0, 0, {});
+        acc->setExceptionBackend(backend);
+        if(acc->getNumberOfChannels() != 1) {
+          throw ChimeraTK::logic_error(
+              "The LogicalNameMapper MathPlugin supports only scalar or 1D array registers. Register name: '" +
+              varName + "', parameter name: '" + parpair.first + "'");
+        }
+        auto view = std::make_unique<exprtk::vector_view<double>>(
+            exprtk::make_vector_view(acc->accessChannel(0), acc->getNumberOfSamples()));
+        symbols.add_vector(parpair.first, *view);
+        params[acc] = std::move(view);
+      }
+
+      // compile the expression
+      expression.register_symbol_table(symbols);
+      bool success = parser.compile(parameters.at("formula"), expression);
+      if(!success) {
+        throw ChimeraTK::logic_error("LogicalNameMapping MathPlugin for register '" + varName +
+            "': failed to compile expression '" + parameters.at("formula") + "': " + parser.error());
+      }
+    }
+
+    template<typename T>
+    void computeResult(std::vector<double>& x, std::vector<T>& resultBuffer) {
+      // inform the value view of the new data pointer - the buffer might have been swapped
+      valueView->rebase(x.data());
+
+      // update parameters
+      for(auto& p : params) {
+        p.first->read();
+        p.second->rebase(p.first->accessChannel(0).data());
+      }
+
+      // evaluate the expression, obtain the result in a way so it also works when using the return statement
+      double valueWhenNotUsingReturn = expression.value();
+      exprtk::results_context<double> results = expression.results();
+
+      // extract result depending on how it was returned
+      if(results.count() == 0) {
+        // if results.count() is 0, the return statement presumably has not been used
+        if(resultBuffer.size() != 1) {
+          throw ChimeraTK::logic_error("LogicalNameMapping MathPlugin for register '" + varName +
+              "': The expression returns a scalar but " + std::to_string(resultBuffer.size()) + " expected.");
+        }
+        resultBuffer[0] = numericToUserType<T>(valueWhenNotUsingReturn);
+      }
+      else if(results.count() == 1) {
+        // return statement has been used to return exactly one value (note: this value might be an array)
+        exprtk::type_store<double> result = results[0];
+
+        // make sure we got a numeric result
+        if(result.type != exprtk::type_store<double>::e_scalar && result.type != exprtk::type_store<double>::e_vector) {
+          throw ChimeraTK::logic_error("LogicalNameMapping MathPlugin for register '" + varName +
+              "': The expression did not return a numeric result.");
+        }
+
+        // create vector view and check that its size matches
+        exprtk::type_store<double>::type_view<double> view(result);
+        if(view.size() != resultBuffer.size()) {
+          throw ChimeraTK::logic_error("LogicalNameMapping MathPlugin for register '" + varName +
+              "': The expression returns " + std::to_string(view.size()) + " elements but " +
+              std::to_string(resultBuffer.size()) + " expected.");
+        }
+
+        // convert and copy data into target buffer
+        for(size_t k = 0; k < view.size(); ++k) {
+          resultBuffer[k] = numericToUserType<T>(view[k]);
+        }
+      }
+      else {
+        // multiple results in return statement are unexpected
+        throw ChimeraTK::logic_error("LogicalNameMapping MathPlugin for register '" + varName +
+            "': The expression returned " + std::to_string(results.count()) + " results, expect exactly one result.");
+      }
+    }
+  };
+
+  /********************************************************************************************************************/
+
   template<typename UserType>
   struct MathPluginDecorator : ChimeraTK::NDRegisterAccessorDecorator<UserType, double> {
     using ChimeraTK::NDRegisterAccessorDecorator<UserType, double>::buffer_2D;
@@ -62,10 +167,7 @@ namespace ChimeraTK { namespace LNMBackend {
       _target->postWrite(type, versionNumber);
     }
 
-    exprtk::expression<double> expression;
-    exprtk::symbol_table<double> symbols;
-    std::unique_ptr<exprtk::vector_view<double>> valueView;
-    std::map<boost::shared_ptr<NDRegisterAccessor<double>>, std::unique_ptr<exprtk::vector_view<double>>> params;
+    MathPluginFormulaHelper h;
     bool _isWrite;
 
     using ChimeraTK::NDRegisterAccessorDecorator<UserType, double>::_target;
@@ -87,46 +189,14 @@ namespace ChimeraTK { namespace LNMBackend {
           this->getName());
     }
 
-    // Although this is a decorator we do not use the exceptions backend from the target because this decorator is given out by a backend
+    // Although this is a decorator we do not use the exceptions backend from the target because this decorator is given
+    // out by a backend
     target->setExceptionBackend(backend);
     this->_exceptionBackend = backend;
 
-    // create exprtk parser
-    exprtk::parser<double> parser;
-
-    // add basic constants like pi
-    symbols.add_constants();
-
-    // Create vector view for the value and add it to the symbol table. We need to use a vector view instead of adding
-    // the buffer directly as a vector, since our buffers might be swapped and hence the address of the data can
-    // change.
-    valueView = std::make_unique<exprtk::vector_view<double>>(
-        exprtk::make_vector_view(_target->accessChannel(0), _target->accessChannel(0).size()));
-    symbols.add_vector("x", *valueView);
-
-    // iterate parameters, add all but 'formula' parameter as a variable
-    for(auto& parpair : parameters) {
-      if(parpair.first == "formula") continue;
-      auto acc = backend->getRegisterAccessor<double>(parpair.second, 0, 0, {});
-      acc->setExceptionBackend(backend);
-      if(acc->getNumberOfChannels() != 1) {
-        throw ChimeraTK::logic_error(
-            "The LogicalNameMapper MathPlugin supports only scalar or 1D array registers. Register name: '" +
-            this->getName() + "', parameter name: '" + parpair.first + "'");
-      }
-      auto view = std::make_unique<exprtk::vector_view<double>>(
-          exprtk::make_vector_view(acc->accessChannel(0), acc->getNumberOfSamples()));
-      symbols.add_vector(parpair.first, *view);
-      params[acc] = std::move(view);
-    }
-
-    // compile the expression
-    expression.register_symbol_table(symbols);
-    bool success = parser.compile(parameters.at("formula"), expression);
-    if(!success) {
-      throw ChimeraTK::logic_error("LogicalNameMapping MathPlugin for register '" + this->getName() +
-          "': failed to compile expression '" + parameters.at("formula") + "': " + parser.error());
-    }
+    // compile formula
+    h.varName = this->getName();
+    h.compileFormula(backend, parameters, target);
   }
 
   /********************************************************************************************************************/
@@ -136,60 +206,12 @@ namespace ChimeraTK { namespace LNMBackend {
     _target->postRead(type, hasNewData);
     if(!hasNewData) return;
 
-    // update data pointer
-    valueView->rebase(_target->accessChannel(0).data());
+    // evaluate the expression and store into application buffer
+    h.computeResult(_target->accessChannel(0), buffer_2D[0]);
 
-    // update parameters
-    for(auto& p : params) {
-      p.first->read();
-      p.second->rebase(p.first->accessChannel(0).data());
-    }
-
-    // evaluate the expression, obtain the result in a way so it also works when using the return statement
-    double valueWhenNotUsingReturn = expression.value();
-    exprtk::results_context<double> results = expression.results();
-
-    if(results.count() == 0) {
-      // if results.count() is 0, the return statement presumably has not been used
-      if(buffer_2D[0].size() != 1) {
-        throw ChimeraTK::logic_error("LogicalNameMapping MathPlugin for register '" + this->getName() +
-            "': The expression returns a scalar but " + std::to_string(buffer_2D[0].size()) +
-            " expected for read operations.");
-      }
-      buffer_2D[0][0] = numericToUserType<UserType>(valueWhenNotUsingReturn);
-      this->_versionNumber = _target->getVersionNumber();
-      this->_dataValidity = _target->dataValidity();
-    }
-    else if(results.count() == 1) {
-      // return statement has been used to return exactly one value (note: this value might be an array)
-      exprtk::type_store<double> result = results[0];
-
-      // make sure we got a numeric result
-      if(result.type != exprtk::type_store<double>::e_scalar && result.type != exprtk::type_store<double>::e_vector) {
-        throw ChimeraTK::logic_error("LogicalNameMapping MathPlugin for register '" + this->getName() +
-            "': The expression did not return a numeric result.");
-      }
-
-      // create vector view and check that its size matches
-      exprtk::type_store<double>::type_view<double> view(result);
-      if(view.size() != buffer_2D[0].size()) {
-        throw ChimeraTK::logic_error("LogicalNameMapping MathPlugin for register '" + this->getName() +
-            "': The expression returns " + std::to_string(view.size()) + " elements but " +
-            std::to_string(buffer_2D[0].size()) + " expected for read operations.");
-      }
-
-      // convert and copy data into user buffer
-      for(size_t k = 0; k < view.size(); ++k) {
-        buffer_2D[0][k] = numericToUserType<UserType>(view[k]);
-      }
-      this->_versionNumber = _target->getVersionNumber();
-      this->_dataValidity = _target->dataValidity();
-    }
-    else {
-      // multiple results in return statement are unexpected
-      throw ChimeraTK::logic_error("LogicalNameMapping MathPlugin for register '" + this->getName() +
-          "': The expression returned " + std::to_string(results.count()) + " results, expect exactly one result.");
-    }
+    // update version number and validity from target
+    this->_versionNumber = _target->getVersionNumber();
+    this->_dataValidity = _target->dataValidity();
   }
 
   /********************************************************************************************************************/
@@ -207,55 +229,10 @@ namespace ChimeraTK { namespace LNMBackend {
       _target->accessData(0, k) = userTypeToNumeric<double>(buffer_2D[0][k]);
     }
 
-    // inform the value view of the new data pointer - the buffer might have been swapped
-    valueView->rebase(_target->accessChannel(0).data());
+    // evaluate the expression and store into target accessor
+    h.computeResult(_target->accessChannel(0), _target->accessChannel(0));
 
-    // update parameters
-    for(auto& p : params) {
-      p.first->read();
-      p.second->rebase(p.first->accessChannel(0).data());
-    }
-
-    // evaluate the expression, obtain the result in a way so it also works when using the return statement
-    double valueWhenNotUsingReturn = expression.value();
-    exprtk::results_context<double> results = expression.results();
-    if(results.count() == 0) {
-      // if results.count() is 0, the return statement presumably has not been used
-      if(_target->accessChannel(0).size() != 1) {
-        throw ChimeraTK::logic_error("LogicalNameMapping MathPlugin for register '" + this->getName() +
-            "': The expression returns a scalar but " + std::to_string(buffer_2D[0].size()) +
-            " expected for write operations.");
-      }
-      _target->accessData(0, 0) = valueWhenNotUsingReturn;
-    }
-    else if(results.count() == 1) {
-      // return statement has been used to return exactly one value (note: this value might be an array)
-      exprtk::type_store<double> result = results[0];
-
-      // make sure we got a numeric result
-      if(result.type != exprtk::type_store<double>::e_scalar && result.type != exprtk::type_store<double>::e_vector) {
-        throw ChimeraTK::logic_error("LogicalNameMapping MathPlugin for register '" + this->getName() +
-            "': The expression did not return a numeric result.");
-      }
-
-      // create vector view and check that its size matches
-      exprtk::type_store<double>::type_view<double> view(result);
-      if(view.size() != _target->getNumberOfSamples()) {
-        throw ChimeraTK::logic_error("LogicalNameMapping MathPlugin for register '" + this->getName() +
-            "': The expression returns " + std::to_string(view.size()) + " elements but " +
-            std::to_string(_target->getNumberOfSamples()) + " expected for write operations.");
-      }
-
-      // convert and copy data into target buffer
-      for(size_t k = 0; k < view.size(); ++k) {
-        _target->accessData(0, k) = view[k];
-      }
-    }
-    else {
-      // multiple results in return statement are unexpected
-      throw ChimeraTK::logic_error("LogicalNameMapping MathPlugin for register '" + this->getName() +
-          "': The expression returned " + std::to_string(results.count()) + " results, expect exactly one result.");
-    }
+    // pass validity to target and delegate preWrite
     _target->setDataValidity(this->_dataValidity);
     _target->preWrite(type, versionNumber);
   }
@@ -267,7 +244,7 @@ namespace ChimeraTK { namespace LNMBackend {
     this->_exceptionBackend = exceptionBackend;
     _target->setExceptionBackend(exceptionBackend);
     // params is a map with NDRegisterAccessors as key
-    for(auto& p : params) {
+    for(auto& p : h.params) {
       p.first->setExceptionBackend(exceptionBackend);
     }
   }
