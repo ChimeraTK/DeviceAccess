ChimeraTK/DeviceAccess

commit 16a422d0b6984b64157306d1aa84177ae4f7c88d

XDMA backend does not throw in readDeviceInfo()

device_backends/xdma/src/XdmaBackend.cc
@@ -107,10 +107,14 @@ namespace ChimeraTK {
   }
 
   std::string XdmaBackend::readDeviceInfo() {
-    if(!isOpen()) throw ChimeraTK::logic_error("Device not opened.");
+    std::string result = "XDMA backend: Device path = " + _devicePath + ", number of DMA channels = ";
+    if(isOpen()) {
+      result += std::to_string(_dmaChannels.size());
+    }
+    else {
+      result += "uknown (device closed)";
+    }
 
-    std::string result = "XDMA device path = " + _devicePath + ", ";
-    result += std::to_string(_dmaChannels.size()) + " DMA channels";
     // TODO: retrieve other interesting stuff (driver version...) via ioctl
     return result;
   }

commit bebd72d08939e6b34bf51f068117fc1e99914f7c

fixed typo in XDMA backend (just introduced in 16a422d0b)

device_backends/xdma/src/XdmaBackend.cc
@@ -112,7 +112,7 @@ namespace ChimeraTK {
       result += std::to_string(_dmaChannels.size());
     }
     else {
-      result += "uknown (device closed)";
+      result += "unknown (device closed)";
     }
 
     // TODO: retrieve other interesting stuff (driver version...) via ioctl