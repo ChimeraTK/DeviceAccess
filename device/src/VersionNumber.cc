cad03b9267459233b58139a0b6ce8dd9489a922b
Martin Hierholzer <martin.hierholzer@desy.de>
2021-08-02 14:17:00 +0200

    VersionNumber: add ostream operator<<

--- device/src/VersionNumber.cc
+++ device/src/VersionNumber.cc
@@ -2,4 +2,10 @@
 
 namespace ChimeraTK {
   std::atomic<uint64_t> VersionNumber::_lastGeneratedVersionNumber{0};
-}
+
+  /** Stream operator passing the human readable representation to an ostream. */
+  std::ostream& operator<<(std::ostream& stream, const VersionNumber& version) {
+    stream << std::string(version);
+    return stream;
+  }
+} /* namespace ChimeraTK */
