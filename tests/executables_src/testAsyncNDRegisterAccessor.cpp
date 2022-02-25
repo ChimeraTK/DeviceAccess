fa1c69951aed16697915aa3a38d2e6ebd27b0e24
Martin Hierholzer <martin.hierholzer@desy.de>
2022-02-25 17:31:28 +0100

    testAsyncNDRegisterAccessor: fix and re-enable write test, remove read test (as convered by unified test)

--- tests/executables_src/testAsyncNDRegisterAccessor.cpp
+++ tests/executables_src/testAsyncNDRegisterAccessor.cpp
@@ -9,73 +9,21 @@ using namespace ChimeraTK;
 
 #include <future>
 
-// This is just a smoke test for basic functionality. Might quickly become obsolete with the UnifiedBackendTest
-BOOST_AUTO_TEST_CASE(SmokeTest) {
+// Note: Most functionality of the AsyncNDRegisterAccessor is tested in testNumericAddressedBackendUnified
+
+BOOST_AUTO_TEST_CASE(Test_setWriteAccessor) {
   Device d("(dummy?map=goodMapFile.map)");
   d.open();
   d.activateAsyncRead();
 
-  auto asyncAccessor =
-      d.getScalarRegisterAccessor<double>("MODULE0/INTERRUPT_TYPE", 0, {AccessMode::wait_for_new_data});
-  BOOST_CHECK(asyncAccessor.isReadOnly());
-  BOOST_CHECK(asyncAccessor.isReadable());
-  BOOST_CHECK(!asyncAccessor.isWriteable());
+  auto asyncAccessor = d.getScalarRegisterAccessor<int>("MODULE0/INTERRUPT_TYPE", 0, {AccessMode::wait_for_new_data});
 
-  auto asyncVoidAccessor = d.getVoidRegisterAccessor("MODULE0/INTERRUPT_VOID1", {AccessMode::wait_for_new_data});
+  auto syncAccessor = d.getScalarRegisterAccessor<int>("MODULE0/INTERRUPT_TYPE/DUMMY_WRITEABLE", 0);
+  auto syncAccessor_ptr = boost::dynamic_pointer_cast<NDRegisterAccessor<int>>(syncAccessor.get()->shared_from_this());
+  auto asyncAccessor_casted = dynamic_cast<AsyncNDRegisterAccessor<int>*>(asyncAccessor.get());
 
-  auto syncAccessor = d.getScalarRegisterAccessor<double>("MODULE0/INTERRUPT_TYPE", 0);
-  BOOST_CHECK(syncAccessor.isReadOnly());
-  BOOST_CHECK(syncAccessor.isReadable());
-  BOOST_CHECK(!syncAccessor.isWriteable());
+  asyncAccessor_casted->setWriteAccessor(syncAccessor_ptr);
 
-  auto writeableAccessor = d.getScalarRegisterAccessor<double>("MODULE0/INTERRUPT_TYPE/DUMMY_WRITEABLE", 0);
-  BOOST_CHECK(writeableAccessor.isWriteable());
-  writeableAccessor = 42;
-  writeableAccessor.write();
-
-  asyncAccessor.read();     // the initial value has arrived
-  asyncVoidAccessor.read(); // does this get stuck?
-  BOOST_CHECK_EQUAL(double(asyncAccessor), 0.0);
-
-  auto isReadFinished = std::async(std::launch::async, [&] { asyncAccessor.read(); });
-  auto isVoidReadFinished = std::async(std::launch::async, [&] { asyncVoidAccessor.read(); });
-
-  BOOST_CHECK(isReadFinished.wait_for(std::chrono::seconds(1)) == std::future_status::timeout);
-  BOOST_CHECK(isVoidReadFinished.wait_for(std::chrono::seconds(1)) == std::future_status::timeout);
-
-  auto dummy = boost::dynamic_pointer_cast<DummyBackend>(d.getBackend());
-
-  VersionNumber versionBeforeInterrupt;
-  auto interruptVersion = dummy->triggerInterrupt(5, 6); // the interrupt with data
-  BOOST_CHECK(isReadFinished.wait_for(std::chrono::seconds(3)) == std::future_status::ready);
-  BOOST_CHECK_EQUAL(double(asyncAccessor), 42.0);
-  BOOST_CHECK_EQUAL(interruptVersion, asyncAccessor.getVersionNumber());
-  BOOST_CHECK(interruptVersion > versionBeforeInterrupt);
-  BOOST_CHECK(isVoidReadFinished.wait_for(std::chrono::seconds(1)) == std::future_status::timeout);
-
-  syncAccessor.read();
-  BOOST_CHECK_EQUAL(double(syncAccessor), 42.0);
-
-  // starrt another read of the async accessor. It will never see data
-  isReadFinished = std::async(std::launch::async, [&] { asyncAccessor.read(); });
-
-  dummy->triggerInterrupt(1, 3); // the void interrupt
-
-  BOOST_CHECK(isVoidReadFinished.wait_for(std::chrono::seconds(3)) == std::future_status::ready);
-  BOOST_CHECK(isReadFinished.wait_for(std::chrono::seconds(1)) == std::future_status::timeout);
-
-  // interrupt the blocking read of the accessor with data to get out of the program
-  asyncAccessor.getHighLevelImplElement()->interrupt();
-  d.close();
-}
-/*
-BOOST_AUTO_TEST_CASE(SmokeTestWritable) {
-  Device d("(dummy?map=goodMapFile.map)");
-  d.open();
-  d.activateAsyncRead();
-
-  auto asyncAccessor =
-      d.getScalarRegisterAccessor<int>("MODULE0/INTERRUPT_TYPE/DUMMY_WRITEABLE", 0, {AccessMode::wait_for_new_data});
   BOOST_CHECK(!asyncAccessor.isReadOnly());
   BOOST_CHECK(asyncAccessor.isReadable());
   BOOST_CHECK(asyncAccessor.isWriteable());
@@ -95,4 +43,3 @@ BOOST_AUTO_TEST_CASE(SmokeTestWritable) {
 
   BOOST_CHECK_EQUAL(d.read<int>("MODULE0/INTERRUPT_TYPE"), 43);
 }
-*/
