1edbb2450295f0819bba7299e695a123fd6a2434
Martin Hierholzer <martin.hierholzer@desy.de>
2022-02-28 15:43:14 +0100

    explicitly delete copying BackendRegisterCatalogueBase

--- device_backends/include/BackendRegisterCatalogue.h
+++ device_backends/include/BackendRegisterCatalogue.h
@@ -24,6 +24,11 @@ namespace ChimeraTK {
 
     BackendRegisterCatalogueBase& operator=(BackendRegisterCatalogueBase&& other) = default;
 
+    // Copy constructor/assignment is deleted. It could be implemented (need to take care of pointers in
+    // insertionOrderedCatalogue), but currently there is no need for this and hence it is not done.
+    BackendRegisterCatalogueBase(const BackendRegisterCatalogueBase& other) = delete;
+    BackendRegisterCatalogueBase& operator=(const BackendRegisterCatalogueBase& other) = delete;
+
     /** 
      *  Get register information for a given full path name.
      *  
