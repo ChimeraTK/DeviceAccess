996f858631a24beceae0a0e320b621bf2f846e65
mc-randall <michael.randall@gmx.de>
2022-12-09 16:14:16 +0100

    Re-Implement the UIO backend (#10601) (#294)
    * first working version for read/write access through UIO backend
    
    * unify error and exception handling
    
    * refactor core code for UIO device
    
    * first version with interrupt handling
    
    * interrupt works; code improvements and refactoring needed
    
    * make thread safe, refactor and comment code
    
    * replace boost with std-lib where possible; remove debug prints
    
    * ensure correct interrupt thread behavior, rename UIO device class and deeper testing
    
    * Renamed and documented test code file.
    
    * updated documentation
    
    * fix code formatting
    
    * remove unused file
    
    * rename test files
    
    * test changed test code on hardware
    
    * overwrite virtual function barIndexValid(); remove bug with using mutex
    
    * catch and handle exceptions in interrupt handling thread
    
    Co-authored-by: Michael Randall <michael.randall@desy.de>
    Co-authored-by: Martin Killenberg <martin.killenberg@desy.de>

---
+++ device_backends/uio/include/UioAccess.h
@@ -0,0 +1,79 @@
+// SPDX-FileCopyrightText: Deutsches Elektronen-Synchrotron DESY, MSK, ChimeraTK Project <chimeratk-support@desy.de>
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#pragma once
+
+#include <boost/filesystem.hpp>
+
+#include <string>
+
+namespace ChimeraTK {
+  /// @brief Implements a generic userspace interface for UIO devices.
+  class UioAccess {
+   private:
+    boost::filesystem::path _deviceFilePath;
+    int _deviceFileDescriptor = 0;
+    void* _deviceUserBase = nullptr;
+    void* _deviceKernelBase = nullptr;
+    size_t _deviceMemSize = 0;
+    uint32_t _lastInterruptCount = 0;
+    std::atomic<bool> _opened{false};
+
+    /// @brief Maps user space memory range to address range of UIO device.
+    void UioMMap();
+
+    /// @brief Unmaps user space memory range for address range of UIO device.
+    void UioUnmap();
+
+    /// @brief Subtracts uint32_t values taking overflow into account.
+    /// @param minuend Minuend of subtraction
+    /// @param subtrahend Subtrahend of subtraction
+    /// @return Result value
+    uint32_t subtractUint32OverflowSafe(uint32_t minuend, uint32_t subtrahend);
+
+    /// @brief Reads a decimal formatted value from a file as unsigned 32-bit integer.
+    /// @param fileName File path to read from
+    /// @return Unsigned value
+    uint32_t readUint32FromFile(std::string fileName);
+
+    /// @brief Reads a hexadecimal formatted value from a file as unsigned 64-bit integer.
+    /// @param fileName File path to read from
+    /// @return Unsigned value
+    uint64_t readUint64HexFromFile(std::string fileName);
+
+   public:
+    UioAccess(std::string deviceFilePath);
+    ~UioAccess();
+
+    /// @brief Opens UIO device for read and write operations and interrupt handling.
+    void open();
+
+    /// @brief Closes UIO device.
+    void close();
+
+    /// @brief Read data from the specified memory offset address. The address range starts at '0'.
+    /// @param map Selected UIO memory region. Only region '0' is currently supported.
+    /// @param address Start address of memory to read from
+    /// @param data Address pointer to which data is to be copied
+    /// @param sizeInBytes Number of bytes to copy
+    void read(uint64_t map, uint64_t address, int32_t* data, size_t sizeInBytes);
+
+    /// @brief Write data to the specified memory offset address. The address range starts at '0'.
+    /// @param map Selected UIO memory region. Only region '0' is currently supported.
+    /// @param address Start address of memory to write to
+    /// @param data Address pointer from which data is to be copied
+    /// @param sizeInBytes Number of bytes to copy
+    void write(uint64_t map, uint64_t address, int32_t const* data, size_t sizeInBytes);
+
+    /// @brief Wait for hardware interrupt to occur within specified timeout period.
+    /// @param timeoutMs Timeout period in ms
+    /// @return Number of interrupts that occurred. '0' for none withing timeout period.
+    uint32_t waitForInterrupt(int timeoutMs);
+
+    /// @brief Clear all pending interrupts.
+    void clearInterrupts();
+
+    /// @brief Return UIO device file path.
+    /// @return File path
+    std::string getDeviceFilePath();
+  };
+} // namespace ChimeraTK
---
+++ tests/unitTestsNotUnderCtest/testUioBackendWithHardware.cpp
@@ -0,0 +1,154 @@
+// SPDX-FileCopyrightText: Deutsches Elektronen-Synchrotron DESY, MSK, ChimeraTK Project <chimeratk-support@desy.de>
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#include "Device.h"
+#include "Utilities.h"
+
+#include <iostream>
+
+#define EXPECTED_MOTOR_ID 268435504
+
+/*
+ * This test code needs to be executed on a Xilinx ZCU102 evaluation board, using the hardware project
+ *  files from git@gitlab.msktools.desy.de:fpgafw/projects/test/test_bsp_motctrl.git (Tag: 0.1.0, Commit ID: a6160e40)
+ */
+
+/*
+ * All information needed to access the device is
+ * the device alias and the register names
+ * (plus a .dmap file)
+ */
+
+int main() {
+  /*
+   * Before you use a device you have to tell DeviceAccess
+   * which dmap file to use.
+   */
+  ChimeraTK::setDMapFilePath("uioBackendTest.dmap");
+
+  /*
+   * Create a device. Make sure a device alias is present
+   * in the dmap file.
+   */
+  ChimeraTK::Device myDevice("MOTCTRL");
+
+  myDevice.open();
+
+  ChimeraTK::ScalarRegisterAccessor<uint32_t> motorPosition = myDevice.getScalarRegisterAccessor<uint32_t>(
+      "MOTOR_CONTROL/MOTOR_POSITION", 0, {ChimeraTK::AccessMode::wait_for_new_data});
+
+  myDevice.close();
+  myDevice.open();
+
+  myDevice.activateAsyncRead();
+
+  /*
+   * Registers are defined by a path, which consists of a hierarchy of
+   * names separated by '/'. In this is example it is Module/Register.
+   * In this basic example we use a register which contains a single value
+   * (a scalar).
+   */
+
+  ChimeraTK::ScalarRegisterAccessor<uint32_t> motorControlId =
+      myDevice.getScalarRegisterAccessor<uint32_t>("MOTOR_CONTROL/ID");
+
+  ChimeraTK::ScalarRegisterAccessor<uint32_t> maximumAcceleration =
+      myDevice.getScalarRegisterAccessor<uint32_t>("MOTOR_CONTROL/MOTOR_MAX_ACC");
+
+  ChimeraTK::ScalarRegisterAccessor<uint32_t> maximumAccelerationReadBack =
+      myDevice.getScalarRegisterAccessor<uint32_t>("MOTOR_CONTROL/MOTOR_MAX_ACC");
+
+  ChimeraTK::ScalarRegisterAccessor<uint32_t> maximumVelocity =
+      myDevice.getScalarRegisterAccessor<uint32_t>("MOTOR_CONTROL/MOTOR_MAX_VEL");
+
+  ChimeraTK::ScalarRegisterAccessor<uint32_t> baseVelocity =
+      myDevice.getScalarRegisterAccessor<uint32_t>("MOTOR_CONTROL/MOTOR_BASE_VEL");
+
+  ChimeraTK::ScalarRegisterAccessor<uint32_t> pulseWidth =
+      myDevice.getScalarRegisterAccessor<uint32_t>("MOTOR_CONTROL/MOTOR_PULSE_WIDTH");
+
+  ChimeraTK::ScalarRegisterAccessor<int32_t> motorDestination =
+      myDevice.getScalarRegisterAccessor<int32_t>("MOTOR_CONTROL/MOTOR_DESTINATION");
+
+  ChimeraTK::ScalarRegisterAccessor<uint32_t> motorStart =
+      myDevice.getScalarRegisterAccessor<uint32_t>("MOTOR_CONTROL/MOTOR_START");
+
+  ChimeraTK::ScalarRegisterAccessor<uint32_t> resetMotorPosition =
+      myDevice.getScalarRegisterAccessor<uint32_t>("MOTOR_CONTROL/MOTOR_POSITION_RESET");
+
+  /* Check read access*/
+  motorControlId.read();
+  if(motorControlId != EXPECTED_MOTOR_ID) {
+    std::cerr << "Read check failed: Module ID is expected to be 0x" << std::hex << EXPECTED_MOTOR_ID
+              << ", but it is 0x" << (uint32_t)motorControlId << "!" << std::endl;
+    exit(-1);
+  }
+
+  /* Configure motor control logic*/
+  maximumAcceleration = 2000;
+  maximumAcceleration.write();
+
+  maximumVelocity = 2000;
+  maximumVelocity.write();
+
+  baseVelocity = 0;
+  baseVelocity.write();
+
+  pulseWidth = 200;
+  pulseWidth.write();
+
+  /* Check write access*/
+  maximumAccelerationReadBack.read();
+  if(maximumAcceleration != maximumAccelerationReadBack) {
+    std::cerr << "Write check failed: Maximum acceleration is expected to be " << maximumAcceleration << ", but it is "
+              << maximumAccelerationReadBack << "!" << std::endl;
+    exit(-1);
+  }
+
+  /* Read back configuration*/
+  maximumAcceleration.read();
+  maximumVelocity.read();
+  baseVelocity.read();
+  pulseWidth.read();
+
+  std::cout << "maximumAcceleration = " << maximumAcceleration << std::endl;
+  std::cout << "maximumVelocity     = " << maximumVelocity << std::endl;
+  std::cout << "baseVelocity        = " << baseVelocity << std::endl;
+  std::cout << "pulseWidth          = " << pulseWidth << std::endl;
+
+  /* Move motor*/
+  motorStart = 0;
+  motorStart.write();
+
+  resetMotorPosition = 1;
+  resetMotorPosition.write();
+  resetMotorPosition = 0;
+  resetMotorPosition.write();
+
+  motorDestination = 0;
+  motorDestination.write();
+
+  motorPosition.read();
+  std::cout << "Motor at position " << motorPosition << std::endl;
+
+  for(size_t i = 0; i < 10; i++) {
+    // Set new target position
+    motorDestination += 5000;
+    motorDestination.write();
+    std::cout << std::endl << "Target position is " << motorDestination << std::endl;
+
+    // Start motor movement
+    motorStart = 1;
+    motorStart.write();
+    motorStart = 0;
+    motorStart.write();
+
+    // Wait until motor reached position
+    motorPosition.read();
+    std::cout << "Motor at position " << motorPosition << std::endl;
+  }
+
+  myDevice.close();
+
+  return 0;
+}
