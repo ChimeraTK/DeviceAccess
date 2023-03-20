c5f860d381cb874e262998382e799c86a1f0d9ba
Dietrich Rothe <dietrich.rothe@desy.de>
2023-03-20 15:37:53 +0100

    remove testVirtualFunctionEmplateCompilerFlags
    since compiler flags were removed and test became obsolote

--- tests/src/testVirtualFunctionTemplateCompilerFlags.cc
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: Deutsches Elektronen-Synchrotron DESY, MSK, ChimeraTK Project <chimeratk-support@desy.de>
-// SPDX-License-Identifier: LGPL-3.0-or-later
-
-// The only reason of this code is to test that compiling fails if the compiler flags are wrong. A preprocessor
-// directive in the VirtualFunctionTemplate raises a compiler error in this case, and we are testing if this works as
-// intended.
-
-#include "VirtualFunctionTemplate.h"
