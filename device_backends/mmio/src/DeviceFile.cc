0d22483d8cc96259b359e1b65bf4e85d5f540371
Jens Georg <jens.georg@desy.de>
2023-07-25 12:33:51 +0200

    fixup! wip: Extract mmio from XDMA backend, reuse in UIO

--- device_backends/mmio/src/DeviceFile.cc
+++ device_backends/mmio/src/DeviceFile.cc
@@ -26,16 +26,16 @@ namespace ChimeraTK {
   }
 
   DeviceFile::DeviceFile(int fd, bool takeFdOwnership) : _fd(fd), _fdOwner(takeFdOwnership) {
-    auto path = "/proc/self/" + std::to_string(fd);
+    auto path = "/proc/self/fd/" + std::to_string(fd);
     char buf[PATH_MAX + 1] = {0};
     auto result = readlink(path.c_str(), buf, sizeof(buf));
     _savedErrno = errno;
     if(result < 0) {
-      _path = std::string{buf};
+      throw runtime_error(_strerror("Cannot determine path of fd "));
     }
-    else {
-      throw runtime_error(_strerror("Cannot determine path of fd: "));
-    }
+
+    _path = std::string{buf};
+    std::cout << "Path for " << _fd << ": " << _path << std::endl;
   }
 
   DeviceFile::DeviceFile(DeviceFile&& d) noexcept : _path(std::move(d._path)), _fd(std::exchange(d._fd, 0)) {}
