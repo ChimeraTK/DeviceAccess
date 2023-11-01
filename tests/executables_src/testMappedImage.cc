0bc8ca761d7978e77b1389377dbbe74bb00ff163
Dietrich Rothe <dietrich.rothe@desy.de>
2023-11-01 17:38:24 +0100

    refactor: move MappedImage from ControlSystemAdapter here

---
+++ tests/executables_src/testMappedImage.cc
@@ -0,0 +1,125 @@
+// SPDX-FileCopyrightText: Deutsches Elektronen-Synchrotron DESY, MSK, ChimeraTK Project <chimeratk-support@desy.de>
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#define BOOST_TEST_MODULE MappedImageTest
+// Only after defining the name include the unit test header.
+#include <boost/test/included/unit_test.hpp>
+// boost unit_test needs to be included before serverBasedTestTools.h
+#include "MappedImage.h"
+
+#include <ChimeraTK/Device.h>
+#include <ChimeraTK/OneDRegisterAccessor.h>
+
+#include <boost/test/unit_test.hpp>
+
+using namespace boost::unit_test_framework;
+using namespace ChimeraTK;
+
+BOOST_AUTO_TEST_SUITE(MappedImageTestSuite)
+
+struct DummyFixture {
+  unsigned bufLen = 100; // includes header: 64 bytes
+  ChimeraTK::OneDRegisterAccessor<unsigned char> buf;
+  DummyFixture() {
+    setDMapFilePath("testMappedImage.dmap");
+    Device device;
+    device.open("DUMMY");
+
+    buf.replace(device.getOneDRegisterAccessor<unsigned char>("DAQ/IMAGE", bufLen));
+  }
+};
+
+BOOST_FIXTURE_TEST_CASE(testStructMapping, DummyFixture) {
+  // this test shows an example how to map user-defined opaque structs onto a byte array
+  struct AStruct : public OpaqueStructHeader {
+    int a = 0;
+    float x = 0, y = 1;
+    AStruct() : OpaqueStructHeader(typeid(AStruct)) {}
+  };
+
+  MappedStruct<AStruct> ms(buf, MappedStruct<AStruct>::InitData::Yes);
+  auto* h = ms.header();
+  h->x = 4.;
+  BOOST_CHECK(h->totalLength == sizeof(AStruct));
+
+  MappedStruct<AStruct> ms1(buf, MappedStruct<AStruct>::InitData::No);
+  BOOST_CHECK(ms1.header()->x == 4.);
+  BOOST_CHECK(ms1.header()->y == 1.); // a value set in constructor of AStruct
+
+  buf.write();
+  // test that values can be restored by reading from device
+  ms.header()->x = 0;
+  buf.read();
+  BOOST_CHECK(ms.header()->x == 4.);
+}
+
+BOOST_FIXTURE_TEST_CASE(testMappedImage, DummyFixture) {
+  // this test shows MappedImage usage
+
+  MappedImage A0(buf);
+  unsigned w = 4, h = 2;
+  A0.setShape(w, h, ImgFormat::Gray16);
+  auto Av = A0.interpretedView<uint16_t>();
+  Av(0, 0) = 8;
+  Av(1, 0) = 7;
+  Av(2, 0) = 6;
+  Av(3, 0) = 5;
+  Av(0, 1) = 4;
+  Av(1, 1) = 3;
+  Av(2, 1) = 2;
+  Av(3, 1) = 1;
+  float val = Av(2, 0);
+  BOOST_CHECK(val == 6);
+
+  // also test iterator-style access
+  for(unsigned y = 0; y < h; y++) {
+    unsigned x = 0;
+    for(auto* it = Av.beginRow(y); it != Av.endRow(y); ++it) {
+      BOOST_CHECK(*it == Av(x, y));
+      x++;
+    }
+  }
+  // iterate over whole image
+  unsigned counter = 0;
+  for(auto& pixVal : Av) {
+    pixVal = ++counter;
+  }
+  counter = 0;
+  for(auto& pixVal : Av) {
+    counter++;
+    BOOST_CHECK(pixVal == counter);
+  }
+
+  // test actual header contents of our buffer
+  auto* bufData0 = buf.data();
+  auto* head = reinterpret_cast<ImgHeader*>(bufData0);
+  BOOST_CHECK(head->width == w);
+  BOOST_CHECK(head->height == h);
+  BOOST_CHECK(head->image_format == ImgFormat::Gray16);
+  BOOST_CHECK(head->channels == 1);
+  BOOST_CHECK(head->bytesPerPixel == 2);
+
+  // test actual image body contents of buffer
+  auto* imgBody = reinterpret_cast<uint16_t*>(buf.data() + sizeof(ImgHeader));
+  unsigned i = 0;
+  for(auto& pixVal : Av) {
+    BOOST_CHECK(imgBody[i] == pixVal);
+    i++;
+  }
+
+  ImgHeader head0 = *head;
+  buf.write(); // this allows to analyse data in dummy device, e.g. with shm dummy
+  // set up memory location buf1 with slightly modified image content
+  std::vector<unsigned char> buf1(buf.getNElements());
+  *reinterpret_cast<ImgHeader*>(buf1.data()) = head0; // copy back header
+  auto* imgBody1 = reinterpret_cast<uint16_t*>(buf1.data() + sizeof(ImgHeader));
+  imgBody1[w * h - 1] = 42;
+  buf.swap(buf1);
+  BOOST_CHECK(buf.data() != bufData0);
+  // check that ImgView can still be used, even though user buffer of accessor was swapped
+  // compare bottow right value with previously written content.
+  auto lastVal = Av(w - 1, h - 1);
+  BOOST_CHECK(lastVal == 42);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ util/include/MappedImage.h
@@ -0,0 +1,277 @@
+// SPDX-FileCopyrightText: Deutsches Elektronen-Synchrotron DESY, MSK, ChimeraTK Project <chimeratk-support@desy.de>
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#pragma once
+
+#include <type_traits>
+
+#include <ChimeraTK/OneDRegisterAccessor.h>
+
+#include <cassert>
+#include <cstring>
+#include <iostream>
+#include <typeindex>
+#include <typeinfo>
+#include <vector>
+
+namespace ChimeraTK {
+
+  /**
+   *  generic header for opaque struct handling
+   *  It has fields needed for communication in the same process, e.g. between different ApplicationCore modules.
+   *  Fields needed for communication over the net belong not here but into the relevant ControlsystemAdapter, e.g. the
+   *  OPCUA adapter could implement OPC UA binary mapping and use type ids from a dictionary, in the future.
+   */
+  struct OpaqueStructHeader {
+    std::type_index dataTypeId;
+    uint32_t totalLength = 0; // 0: unknown/not set. length includes header.
+    explicit OpaqueStructHeader(std::type_index dataTypeId_) : dataTypeId(dataTypeId_) {}
+  };
+
+  /**
+   * Provides interface to a struct that is mapped onto a 1D array of ValType
+   * StructHeader must be derived from OpaqueStructHeader.
+   * Variable-length structs are supported, as long as they do not grow beyond the size of the given 1D array.
+   */
+  template<class StructHeader>
+  class MappedStruct {
+   public:
+    enum class InitData { Yes, No };
+    /// This keeps a reference to given OneDRegisterAccessor. If its underlying vector is swapped out,
+    /// the MappedStruct stays valid only if the swapped-in vector was also setup as MappedStruct.
+    explicit MappedStruct(ChimeraTK::OneDRegisterAccessor<unsigned char> &accToData,
+                          InitData doInitData = InitData::No);
+
+    /// returns pointer to data for header and struct content. The returned pointer stays valid until
+    /// write() or read() is called for the underlying accessor.
+    unsigned char* data();
+    /// capacity of used container
+    size_t capacity() const;
+    /// currently used size
+    size_t size() const { return static_cast<OpaqueStructHeader*>(header())->totalLength; }
+    /// returns header, e.g. for setting meta data
+    StructHeader* header() { return reinterpret_cast<StructHeader*>(data()); }
+    /// default initialize header and zero out data that follows
+    void initData();
+
+   protected:
+    // We keep the accessor instead of the naked pointer to simplify usage, like this the object stays valid even after
+    // memory used by accessor was swapped.
+    ChimeraTK::OneDRegisterAccessor<unsigned char>& _accToData;
+  };
+
+  /*******************************  application to Image encoding *********************************/
+
+  enum class ImgFormat { Unset = 0, Gray8, Gray16, RGB24, RGBA32 };
+  // values are defined for a bit field
+  enum class ImgOptions { RowMajor = 1, ColMajor = 0 };
+
+  /// Image header
+  struct ImgHeader : public OpaqueStructHeader {
+    ImgHeader() : OpaqueStructHeader(typeid(ImgHeader)) {}
+
+    uint32_t width = 0;
+    uint32_t height = 0;
+    /// start coordinates, in output
+    int32_t x_start = 0;
+    int32_t y_start = 0;
+    /// can be used in output to provide scaled coordinates
+    float scale_x = 1;
+    float scale_y = 1;
+    /// gray=1, rgb=3, rgba=4
+    uint32_t channels = 0;
+    uint32_t bytesPerPixel = 0;
+    /// effective bits per pixel
+    uint32_t effBitsPerPixel = 0;
+    ImgFormat image_format{ImgFormat::Unset};
+    ImgOptions options{ImgOptions::RowMajor};
+    /// frame number/counter
+    uint64_t frame = 0;
+  };
+
+  class MappedImage;
+  /**
+   * provides convenient matrix-like access for MappedImage
+   */
+  template<typename ValType, ImgOptions OPTIONS>
+  class ImgView {
+   public:
+    ImgView(MappedImage* owner) : _mi(owner) {}
+    /**
+     * This allows to read/write image pixel values, for given coordinates.
+     * dx, dy are relative to x_start, y_start, i.e. x = x_start+dx  on output side
+     * channel is 0..2 for RGB
+     * this method is for random access. for sequential access, iterators provide better performance
+     */
+    ValType& operator()(unsigned dx, unsigned dy, unsigned channel = 0);
+
+    // simply define iterator access via pointers
+    using iterator = ValType*;
+    using value_type = ValType;
+    // for iteration over whole image
+    ValType* begin() { return beginRow(0); }
+    ValType* end() { return beginRow(header()->height); }
+    // these assume ROW-MAJOR ordering
+    ValType* beginRow(unsigned row) { return vec() + row * header()->width * header()->channels; }
+    ValType* endRow(unsigned row) { return beginRow(row + 1); }
+    ImgHeader* header();
+
+   protected:
+    ValType* vec();
+    MappedImage* _mi;
+  };
+
+  /**
+   * interface to an image that is mapped onto a 1D array of ValType
+   */
+  class MappedImage : public MappedStruct<ImgHeader> {
+   public:
+    using MappedStruct<ImgHeader>::MappedStruct;
+
+    /// needs to be called after construction. corrupts all data.
+    /// this throws logic_error if our buffer size is too small. Try lenghtForShape() to check that in advance
+    void setShape(unsigned width, unsigned height, ImgFormat fmt);
+    size_t lengthForShape(unsigned width, unsigned height, ImgFormat fmt);
+    /// returns pointer to image payload data
+    unsigned char* imgBody() { return data() + sizeof(ImgHeader); }
+
+    /// returns an ImgView object which can be used like a matrix. The ImgView becomes invalid at next setShape call.
+    /// It also becomes invalid when memory location of underlying MappedStruct changes.
+    template<typename UserType, ImgOptions OPTIONS = ImgOptions::RowMajor>
+    ImgView<UserType, OPTIONS> interpretedView() {
+      [[maybe_unused]] auto* h = header();
+      assert(h->channels > 0 && "call setShape() before interpretedView()!");
+      assert(h->bytesPerPixel == h->channels * sizeof(UserType) &&
+          "choose correct bytesPerPixel and channels value before conversion!");
+      assert(((unsigned)h->options & (unsigned)ImgOptions::RowMajor) ==
+              ((unsigned)OPTIONS & (unsigned)ImgOptions::RowMajor) &&
+          "inconsistent data ordering col/row major");
+      ImgView<UserType, OPTIONS> ret(this);
+      return ret;
+    }
+
+   protected:
+    size_t formatsDefinition(
+        ImgFormat fmt, unsigned width, unsigned height, unsigned& channels, unsigned& bytesPerPixel);
+  };
+
+  /*************************** begin MappedStruct implementations  ************************************************/
+
+  template<class StructHeader>
+  MappedStruct<StructHeader>::MappedStruct(
+      ChimeraTK::OneDRegisterAccessor<unsigned char>& accToData, InitData doInitData)
+  : _accToData(accToData) {
+    static_assert(std::is_base_of<OpaqueStructHeader, StructHeader>::value,
+        "MappedStruct expects StructHeader to implement OpaqueStructHeader");
+    if(doInitData == InitData::Yes) {
+      initData();
+    }
+  }
+
+  template<class StructHeader>
+  unsigned char* MappedStruct<StructHeader>::data() {
+        return _accToData.data();
+  }
+
+  template<class StructHeader>
+  size_t MappedStruct<StructHeader>::capacity() const {
+        // reason for cast: getNElements not declared const
+        return const_cast<MappedStruct*>(this)->_accToData.getNElements();
+  }
+
+  template<class StructHeader>
+  void MappedStruct<StructHeader>::initData()
+  {
+      size_t sh = sizeof(StructHeader);
+      if (capacity() < sh) {
+          throw logic_error(
+              "buffer provided to MappedStruct is too small for correct initialization");
+      }
+      auto* p = data();
+      new(p) StructHeader;
+      header()->totalLength = sh; // minimal length, could be larger
+      memset(p + sh, 0, capacity() - sh);
+  }
+
+  /*************************** begin MappedImage implementations  ************************************************/
+
+  inline size_t MappedImage::formatsDefinition(
+      ImgFormat fmt, unsigned width, unsigned height, unsigned& channels, unsigned& bytesPerPixel) {
+    switch(fmt) {
+      case ImgFormat::Unset:
+        assert(false && "ImgFormat::Unset not allowed");
+        break;
+      case ImgFormat::Gray8:
+        channels = 1;
+        bytesPerPixel = 1;
+        break;
+      case ImgFormat::Gray16:
+        channels = 1;
+        bytesPerPixel = 2;
+        break;
+      case ImgFormat::RGB24:
+        channels = 3;
+        bytesPerPixel = 3;
+        break;
+      case ImgFormat::RGBA32:
+        channels = 4;
+        bytesPerPixel = 4;
+        break;
+    }
+    return sizeof(ImgHeader) + (size_t)width * height * bytesPerPixel;
+  }
+
+  inline size_t MappedImage::lengthForShape(unsigned width, unsigned height, ImgFormat fmt) {
+    unsigned channels;
+    unsigned bytesPerPixel;
+    return formatsDefinition(fmt, width, height, channels, bytesPerPixel);
+  }
+
+  inline void MappedImage::setShape(unsigned width, unsigned height, ImgFormat fmt) {
+    unsigned channels;
+    unsigned bytesPerPixel;
+    size_t totalLen = formatsDefinition(fmt, width, height, channels, bytesPerPixel);
+    if(totalLen > capacity()) {
+      throw logic_error("MappedImage: provided buffer to small for requested image shape");
+    }
+    auto* h = header();
+    // start with default values
+    new (h) ImgHeader;
+    h->image_format = fmt;
+    h->totalLength = totalLen;
+    h->width = width;
+    h->height = height;
+    h->channels = channels;
+    h->bytesPerPixel = bytesPerPixel;
+  }
+
+  template<typename ValType, ImgOptions OPTIONS>
+  ValType& ImgView<ValType, OPTIONS>::operator()(unsigned dx, unsigned dy, unsigned channel) {
+    auto* h = header();
+    assert(dy < h->height);
+    assert(dx < h->width);
+    assert(channel < h->channels);
+    // this is the only place where row-major / column-major storage is decided
+    // note, definition of row major/column major is confusing for images.
+    // - for a matrix M(i,j) we say it is stored row-major if rows are stored without interleaving: M11, M12,...
+    // - for the same Matrix, if we write M(x,y) for pixel value at coordite (x,y) of an image, this means
+    //   that pixel _columns_ are stored without interleaving
+    // So definition used here is opposite to matrix definition.
+    if constexpr((unsigned)OPTIONS & (unsigned)ImgOptions::RowMajor) {
+      return vec()[(dy * h->width + dx) * h->channels + channel];
+    }
+    else {
+      return vec()[(dy + dx * h->height) * h->channels + channel];
+    }
+  }
+
+  template<typename ValType, ImgOptions OPTIONS>
+  ImgHeader* ImgView<ValType, OPTIONS>::header() {
+    return _mi->header();
+  }
+
+  template<typename ValType, ImgOptions OPTIONS>
+  ValType* ImgView<ValType, OPTIONS>::vec() {
+    return reinterpret_cast<ValType*>(_mi->imgBody());
+  }
+
+} // namespace ChimeraTK
