2da09287c6c79cbce2fc837ff22c24a4b3e6a8a5
Nadeem <nadeem.shehzad@desy.de>
2024-03-22 10:59:50 +0100

    disabled IntCTests to try to build the branch. Still not building.

--- tests/executables_src/testIntcHandler.cpp
+++ tests/executables_src/testIntcHandler.cpp
@@ -4,13 +4,12 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE INTCHandlerTest
 #include <boost/test/unit_test.hpp>
-using namespace boost::unit_test_framework;
-
 #include "Device.h"
 #include "DummyRegisterAccessor.h"
 #include "ExceptionDummyBackend.h"
 #include "TransferGroup.h"
 using namespace ChimeraTK;
+using namespace boost::unit_test_framework;
 
 BOOST_AUTO_TEST_SUITE(INTCHandlerTestSuite)
 
@@ -22,9 +21,79 @@ BOOST_AUTO_TEST_SUITE(INTCHandlerTestSuite)
 
 /********************************************************************************************************************/
 
-BOOST_AUTO_TEST_CASE(testBlockingRead) {
-}
+//BOOST_AUTO_TEST_CASE(testBlockingRead) {
+//}
 /********************************************************************************************************************/
+
+
+/* probably this is the first one to be implemented
+ * if neither (SIE and CIE) nor MIR are present, or only IER is there, it writes 1<<N to IER and clears with 1<<N
+ * if neither ICR nor IAR are present: INTC writes the according bit mask to ISR
+*/
+BOOST_AUTO_TEST_CASE(testIERwithISR) {
+
+  ChimeraTK::Device device;
+  device.open("(dummy:xdma/slot5?map=irq_test.mapp)");
+  BOOST_CHECK(device.isOpened() == true);
+  auto accInterrput = device.getScalarRegisterAccessor<int>("!0:4",0, {ChimeraTK::AccessMode::wait_for_new_data});
+
+  device.activateAsyncRead();
+
+  auto testIER = device.getScalarRegisterAccessor<int>("APP/IER");
+
+ // BOOST_CHECK(0x8 == testIER.read());
+
+  auto accICR = device.getScalarRegisterAccessor<int>("APP/ISR");
+
+ // BOOST_CHECK((accICR >> 3) & 1) == true);
+
+  device.close();
+}
+/**********************************************************************************************************************/
+#ifdef _en
+/*if IAR is present: INTC writes 1<<n the according bit mask to IAR and not to ISR*/
+
+BOOST_AUTO_TEST_CASE(testIERwithIAR) {
+
+  ChimeraTK::Device device;
+  device.open("(dummy:xdma/slot5?map=irq_test.mapp)");
+  BOOST_CHECK(device.isOpened() == true);
+  auto accInterrput = device.getScalarRegisterAccessor<int>("!1:4",0, {ChimeraTK::AccessMode::wait_for_new_data});
+
+  device.activateAsyncRead();
+
+  //auto testIER = device.getScalarRegisterAccessor<int>("APP/IER");
+
+  //BOOST_CHECK(0x8  == testIER.read()):
+
+  auto accIAR = device.getScalarRegisterAccessor<int>("APP/IAR");
+
+  //BOOST_CHECK((accIAR >> 3) & 1) == true);
+
+  device.close();
+}
+/**********************************************************************************************************************/
+/* if ICR is present: INTC writes 1<<n the according bit maskto ICR and not to ISR */
+
+BOOST_AUTO_TEST_CASE(testIERwithICR) {
+
+  ChimeraTK::Device device;
+  device.open("(dummy:xdma/slot5?map=irq_test.mapp)");
+  BOOST_CHECK(device.isOpened() == true);
+  auto accInterrput = device.getScalarRegisterAccessor<int>("!2:4",0, {ChimeraTK::AccessMode::wait_for_new_data});
+
+  device.activateAsyncRead();
+
+  //auto testIER = device.getScalarRegisterAccessor<int>("APP/IER");
+
+  //BOOST_CHECK(0x8 == testIER.read());
+
+  auto accICR = device.getScalarRegisterAccessor<int>("APP/ICR");
+
+  //BOOST_CHECK((accICR >> 3) & 1) == true);
+
+  device.close();
+}
 
 BOOST_AUTO_TEST_CASE(testMER) {
 
@@ -34,25 +103,22 @@ BOOST_AUTO_TEST_CASE(testMER) {
   BOOST_CHECK(device.isOpened() == true);
   auto accInterrput = device.getScalarRegisterAccessor<int>("!0:1",0, {ChimeraTK::AccessMode::wait_for_new_data});
 
-  auto accMER = device.getScalarRegisterAccessor<int>("APP.MER");
+  auto accMER = device.getScalarRegisterAccessor<int>("APP/MER");
 
   // MER should not be set
-  BOOST_CHECK(0x0 = accMER.read())
+  BOOST_CHECK(0x0 == accMER.read());
 
   device.activateAsyncRead();
 
   // MER should be set now
 
-  BOOST_CHECK(0x1 = accMER.read())
+  BOOST_CHECK(0x1 == accMER.read());
 
   device.close();
 
 }
 
-
-
 /********************************************************************************************************************/
-
 BOOST_AUTO_TEST_CASE(testMIE) {
   ChimeraTK::Device device;
 
@@ -63,13 +129,13 @@ BOOST_AUTO_TEST_CASE(testMIE) {
   auto accMIE = device.getScalarRegisterAccessor<int>("APP.MIE");
 
   // MIE should not be set
-  BOOST_CHECK(0x0 = accMIE.read())
+  BOOST_CHECK(0x0 == accMIE.read());
 
   device.activateAsyncRead();
 
   // MIE should be set now
 
-  BOOST_CHECK(0x1 = accMIE.read())
+  BOOST_CHECK(0x1 == accMIE.read());
 
   device.close();
 }
@@ -86,13 +152,13 @@ BOOST_AUTO_TEST_CASE(testGIE) {
   auto accGIE = device.getScalarRegisterAccessor<int>("APP/GIE");
 
   // MIE should not be set
-  BOOST_CHECK(0 = accGIE.read())
+  BOOST_CHECK(0 == accGIE.read());
 
   device.activateAsyncRead();
 
   // MER should be set now
 
-  BOOST_CHECK(0x2 = accGIE.read())
+  BOOST_CHECK(0x2 == accGIE.read());
 
   device.close();
 }
@@ -110,7 +176,7 @@ BOOST_AUTO_TEST_CASE(testSIE) {
 
   auto accSIE = device.getScalarRegisterAccessor<int>("APP/SIE");
 
-  BOOST_CHECK(0x4 = accSIE.read())
+  BOOST_CHECK(0x4 == accSIE.read());
 
   /*auto accCIE = device.getScalarRegisterAccessor<int>("APP/CIE");
 
@@ -120,74 +186,8 @@ BOOST_AUTO_TEST_CASE(testSIE) {
 }
 
 /**********************************************************************************************************************/
-/* probably this is the first one to be implemented
- * if neither (SIE and CIE) nor MIR are present, or only IER is there, it writes 1<<N to IER and clears with 1<<N
- * if neither ICR nor IAR are present: INTC writes the according bit mask to ISR
-*/
-BOOST_AUTO_TEST_CASE(testIERwithISR) {
 
-  ChimeraTK::Device device;
-  device.open("(dummy:xdma/slot5?map=irq_test.mapp)");
-  BOOST_CHECK(device.isOpened() == true);
-  auto accInterrput = device.getScalarRegisterAccessor<int>("!0:4",0, {ChimeraTK::AccessMode::wait_for_new_data});
-
-  device.activateAsyncRead();
-
-  auto testIER = device.getScalarRegisterAccessor<int>("APP/IER");
-
-  BOOST_CHECK(0x8 = testIER.read())
-
-  auto accICR = device.getScalarRegisterAccessor<int>("APP/ISR");
-
-  BOOST_CHECK(true = (accICR >> 3) & 1))
-
-  device.close();
-}
-/**********************************************************************************************************************/
-/*if IAR is present: INTC writes 1<<n the according bit mask to IAR and not to ISR*/
-
-BOOST_AUTO_TEST_CASE(testIERwithIAR) {
-
-  ChimeraTK::Device device;
-  device.open("(dummy:xdma/slot5?map=irq_test.mapp)");
-  BOOST_CHECK(device.isOpened() == true);
-  auto accInterrput = device.getScalarRegisterAccessor<int>("!1:4",0, {ChimeraTK::AccessMode::wait_for_new_data});
-
-  device.activateAsyncRead();
-
-  auto testIER = device.getScalarRegisterAccessor<int>("APP/IER");
-
-  BOOST_CHECK(0x8 = testIER.read())
-
-  auto accIAR = device.getScalarRegisterAccessor<int>("APP/IAR");
-
-  BOOST_CHECK(true = (accIAR >> 3) & 1))
-
-  device.close();
-}
-/**********************************************************************************************************************/
-/* if ICR is present: INTC writes 1<<n the according bit maskto ICR and not to ISR */
-
-BOOST_AUTO_TEST_CASE(testIERwithICR) {
-
-  ChimeraTK::Device device;
-  device.open("(dummy:xdma/slot5?map=irq_test.mapp)");
-  BOOST_CHECK(device.isOpened() == true);
-  auto accInterrput = device.getScalarRegisterAccessor<int>("!2:4",0, {ChimeraTK::AccessMode::wait_for_new_data});
-
-  device.activateAsyncRead();
-
-  auto testIER = device.getScalarRegisterAccessor<int>("APP/IER");
-
-  BOOST_CHECK(0x8 = testIER.read())
-
-  auto accICR = device.getScalarRegisterAccessor<int>("APP/ICR");
-
-  BOOST_CHECK(true = (accICR >> 3) & 1))
-
-  device.close();
-}
-
+#endif
 /**********************************************************************************************************************/
 
 
