96e8ea3dee8f903a339baf7f6dc10ca8a93fc58c
Martin Killenberg <martin.killenberg@desy.de>
2024-04-10 12:47:37 +0200

    chore: rename DummyIntc and Axi4_Intc

--- device_backends/include/GenericInterruptControllerHandler.h
+++ device_backends/include/GenericInterruptControllerHandler.h
@@ -5,16 +5,16 @@
 
 namespace ChimeraTK {
 
-  class Axi4_Intc : public InterruptControllerHandler {
+  class GenericInterruptControllerHandler : public InterruptControllerHandler {
    public:
-    explicit Axi4_Intc(InterruptControllerHandlerFactory* controllerHandlerFactory,
+    explicit GenericInterruptControllerHandler(InterruptControllerHandlerFactory* controllerHandlerFactory,
         std::vector<uint32_t> const& controllerID, boost::shared_ptr<TriggerDistributor> parent)
     : InterruptControllerHandler(controllerHandlerFactory, controllerID, std::move(parent)) {}
-    ~Axi4_Intc() override = default;
+    ~GenericInterruptControllerHandler() override = default;
 
     void handle(VersionNumber version) override;
 
-    static std::unique_ptr<Axi4_Intc> create(InterruptControllerHandlerFactory*,
+    static std::unique_ptr<GenericInterruptControllerHandler> create(InterruptControllerHandlerFactory*,
         std::vector<uint32_t> const& controllerID, std::string const& desrciption,
         boost::shared_ptr<TriggerDistributor> parent);
   };
