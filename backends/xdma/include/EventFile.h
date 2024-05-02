2239e39abfd62f2d2b60804ff996ed4139a2bff4
Martin Killenberg <martin.killenberg@desy.de>
2024-05-02 17:16:48 +0200

    fix(UIO/XDMA): Backends have to store shared_ptr to Domain

--- backends/xdma/include/EventFile.h
+++ backends/xdma/include/EventFile.h
@@ -40,7 +40,7 @@ namespace ChimeraTK {
   class EventFile {
     friend class EventThread;
     DeviceFile _file;
-    boost::weak_ptr<async::DomainImpl<std::nullptr_t>> _asyncDomain;
+    boost::shared_ptr<async::DomainImpl<std::nullptr_t>> _asyncDomain;
 
     std::unique_ptr<EventThread> _evtThread;
 
