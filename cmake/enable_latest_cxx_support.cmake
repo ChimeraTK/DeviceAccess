d3b70cd1bbca532e97cd3ee894c2a4eb1fd27fc4
Martin Hierholzer <martin.hierholzer@desy.de>
2024-10-18 13:02:13 +0200

    feat(project-template): empty out enable_latest_cxx_support.cmake file
    This allows us to use target_compile_features to set the C++ standard
    in cppext and move to a newer standard.

--- cmake/enable_latest_cxx_support.cmake
+++ cmake/enable_latest_cxx_support.cmake
@@ -1,15 +1,12 @@
-#######################################################################################################################
+# ######################################################################################################################
 # enable_latest_cxx_support.cmake
 #
-# Enable the latest C++ standard supported by the compiler. by selecting the appropriate compiler flag. The flag will
-# be appended to all of the following cmake variables:
-# - CMAKE_CXX_FLAGS
-# - ${PROJECT_NAME}_CXX_FLAGS
-# - ${PROJECT_NAME}_CMAKE_CXX_FLAGS
+# This file exists for historic/compatibility reasons only. Do not use it in new projects. The correct C++ standard
+# will be propagated automatically from the base libraries and may be changed via target_compile_features if needed.
 #
-#######################################################################################################################
+# ######################################################################################################################
 
-#######################################################################################################################
+# ######################################################################################################################
 #
 # IMPORTANT NOTE:
 #
@@ -19,29 +16,4 @@
 # If you have modified this file inside a project despite this warning, make sure to cherry-pick all your changes
 # into the project-template repository immediately.
 #
-#######################################################################################################################
-
-include(CheckCXXCompilerFlag)
-CHECK_CXX_COMPILER_FLAG("-std=c++17" COMPILER_SUPPORTS_CXX17)
-CHECK_CXX_COMPILER_FLAG("-std=c++14" COMPILER_SUPPORTS_CXX14)
-CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
-CHECK_CXX_COMPILER_FLAG("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
-if(COMPILER_SUPPORTS_CXX17)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17")
-    set(${PROJECT_NAME}_CXX_FLAGS "${${PROJECT_NAME}_CXX_FLAGS} -std=c++17")
-    set(${PROJECT_NAME}_CMAKE_CXX_FLAGS "${${PROJECT_NAME}_CMAKE_CXX_FLAGS} -std=c++17")
-elseif(COMPILER_SUPPORTS_CXX14)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
-    set(${PROJECT_NAME}_CXX_FLAGS "${${PROJECT_NAME}_CXX_FLAGS} -std=c++14")
-    set(${PROJECT_NAME}_CMAKE_CXX_FLAGS "${${PROJECT_NAME}_CMAKE_CXX_FLAGS} -std=c++14")
-elseif(COMPILER_SUPPORTS_CXX11)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-    set(${PROJECT_NAME}_CXX_FLAGS "${${PROJECT_NAME}_CXX_FLAGS} -std=c++11")
-    set(${PROJECT_NAME}_CMAKE_CXX_FLAGS "${${PROJECT_NAME}_CMAKE_CXX_FLAGS} -std=c++11")
-elseif(COMPILER_SUPPORTS_CXX0X)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
-    set(${PROJECT_NAME}_CXX_FLAGS "${${PROJECT_NAME}_CXX_FLAGS} -std=c++0x")
-    set(${PROJECT_NAME}_CMAKE_CXX_FLAGS "${${PROJECT_NAME}_CMAKE_CXX_FLAGS} -std=c++0x")
-else()
-    message(STATUS "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
-endif()
+# ######################################################################################################################
