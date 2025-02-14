58e1077e6adfa6d0f2d54a63aa71ca62cd118944
Dietrich Rothe <dietrich.rothe@desy.de>
2025-02-14 19:10:51 +0100

    feat: extend DataConsistencyGroup by MatchingMode::historized which uses DataConsistencyDecorators
    * API of DataConsistencyGroup is backwards compatible but partly marked
      as deprecated, where MatchingMode::historized would not be supported.
      The recommended constructors take MatchingMode as an extra argument.
    
    * with MatchingMode::historized, DataConsistencyDecorators are put around
      inner target, i.e. via decorateDeepInside. Like this, ApplicationCore's
      MetaDataPropagationRegisterDecorator will be outside of
      DataConsistencyDecorator. If the added accessors are already part of a
      ReadAnyGroup, we take care to also decorate them in there.
    
    * With MatchingMode::historized, the ReadAnyGroup hands out only one update
      per consistent state that is reached.
      Internally, we keep vector of history buffers for target updates.
      We swap buffers into history just before target->postRead, and swap back
      data into user buffers when a consistent update is possible.

--- include/DataConsistencyGroup.h
+++ include/DataConsistencyGroup.h
@@ -3,108 +3,113 @@
 #pragma once
 
 #include "TransferElementAbstractor.h"
-#include "VersionNumber.h"
-
-#include <unordered_set>
 
 namespace ChimeraTK {
 
   /********************************************************************************************************************/
 
+  namespace DataConsistencyGroupDetail {
+    class MatcherBase;
+  }
+
   /**
    * Group several registers (= TransferElement) which ensures data consistency across multiple variables through an
    * algorithm which matches the VersionNumber. This group does not read on its own. It should work together with a
-   * ReadAnyGroup. You should wait for changed variable and transfer it to this group by calling
-   * ChimeraTK::DataConsistencyGroup::update. If a consistent state is reached, this function returns true.
+   * ReadAnyGroup.
+   * This is a proxy class, which delegates to two different implementations.
+   * MatchingMode=exact is handled by SimpleMatcher (the legacy DataConsistencyGroup implementation).
+   * For this, you should wait for changed variable and transfer it to this group by calling
+   * DataConsistencyGroup::update. If a consistent state is reached, this function returns true.
+   * MatchingMode=historized is handled by HistorizedMatcher. In this case, the provided accessors are decorated
+   * with DataConsistencyDecorators, with the effect that the readAny() returns only on consistent inputs.
+   * In this mode, it is unnecessary but still allowed to call DataConsistencyGroup::update and it simply returns true.
    */
   class DataConsistencyGroup {
    public:
+    /** Enum describing the matching mode of a DataConsistencyGroup. */
+    enum class MatchingMode {
+      none,  ///< No matching, effectively disable the DataConsitencyGroup. update() will always return true.
+      exact, ///< Require an exact match of the VersionNumber of all current values of the group's members.
+             ///< Require an exact match of the VersionNumber of all current or historized values of the group's members
+      historized
+    };
+    constexpr static unsigned defaultHistLen = 2;
+
     /** Construct empty group. Elements can later be added using the add() function. */
-    DataConsistencyGroup();
+    explicit DataConsistencyGroup(MatchingMode mode = MatchingMode::exact);
+    ~DataConsistencyGroup();
+    DataConsistencyGroup(const DataConsistencyGroup& other) = delete;
+    DataConsistencyGroup(DataConsistencyGroup&& other) noexcept;
+    DataConsistencyGroup& operator=(DataConsistencyGroup&& other) noexcept;
 
     /** Construct this group with elements from the list using the add() function. */
+    [[deprecated("use list constructor with MatchingMode instead")]]
     DataConsistencyGroup(std::initializer_list<TransferElementAbstractor> list);
+    [[deprecated("use list constructor with MatchingMode instead")]]
     DataConsistencyGroup(std::initializer_list<boost::shared_ptr<TransferElement>> list);
+    DataConsistencyGroup(std::initializer_list<std::reference_wrapper<TransferElementAbstractor>> list,
+        MatchingMode mode, unsigned histLen = defaultHistLen);
 
     template<typename ITERATOR>
-    DataConsistencyGroup(ITERATOR first, ITERATOR last);
+    DataConsistencyGroup(
+        ITERATOR first, ITERATOR last, MatchingMode mode = MatchingMode::exact, unsigned int histLen = defaultHistLen);
 
     /** Add register to group. The same TransferElement can be part of multiple DataConsistencyGroups. The register
      *  must be must be readable, and it must have AccessMode::wait_for_new_data. */
+    [[deprecated("use add function with histLen instead")]]
     void add(const TransferElementAbstractor& element);
+    [[deprecated("use add function with histLen instead")]]
     void add(boost::shared_ptr<TransferElement> element);
+    /** Add register to group. The same TransferElement can be part of multiple DataConsistencyGroups. The register
+     *  must be must be readable, and it must have AccessMode::wait_for_new_data.
+     *  This function may modify the register accessor, placing a DataConsistencyDecorator around it.
+     **/
+    void add(TransferElementAbstractor& acc, unsigned histLen = defaultHistLen);
 
-    /** This function updates consistentElements, a set of TransferElementID. It returns true, if a consistent state is
-     *  reached. It returns false if an TransferElementID was updated, that was not added to this Group. */
-    bool update(const TransferElementID& transferelementid);
+    /** This function must be called after an update was received from the ReadAnyGroup.
+     *  It returns true, if a consistent state is reached.
+     *  It returns false if an TransferElementID was updated, that was not added to this group.
+     *  For MatchingMode::historized, readAny will only let through consistent updates, so then update always returns
+     *  true.
+     */
+    bool update(const TransferElementID& transferElementID);
 
-    /** Enum describing the matching mode of a DataConsistencyGroup. */
-    enum class MatchingMode {
-      none, ///< No matching, effectively disable the DataConsitencyGroup. update() will always return true.
-      exact ///< Require an exact match of the VersionNumber of all current values of the group's members.
-    };
-
-    /** Change the matching mode. The default mode is MatchingMode::exact. */
-    void setMatchingMode(MatchingMode newMode) { mode = newMode; }
+    /** Change the matching mode. The default mode is MatchingMode::exact, if not set differently in constructor.
+     *  This method is deprecated since it's not possible to set MatchingMode::historized like this.
+     */
+    [[deprecated("set MatchingMode in constructor instead")]]
+    void setMatchingMode(MatchingMode newMode);
 
     /** Return the current matching mode. */
-    MatchingMode getMatchingMode() const { return mode; };
+    [[nodiscard]] MatchingMode getMatchingMode() const { return _mode; };
 
     /** For inspection of contents */
-    const std::map<TransferElementID, TransferElementAbstractor>& getElements() const { return push_elements; }
+    [[nodiscard]] const std::map<TransferElementID, TransferElementAbstractor>& getElements() const;
+    /** For diagnostics */
+    [[nodiscard]] const DataConsistencyGroupDetail::MatcherBase& getMatcher();
 
     /** returns true if consistent state is reached */
-    bool isConsistent() const { return consistentElements.size() == push_elements.size(); }
+    [[nodiscard]] bool isConsistent() const;
 
    private:
-    /// A set of TransferElementID, that were updated with update();
-    std::unordered_set<TransferElementID> consistentElements;
-
-    std::unordered_set<TransferElementID> lasteSateOfConsistentElements;
-
-    /// Holds the version number this group elements should be consistent to
-    VersionNumber versionNumberToBeConsistentTo{nullptr};
-
-    /// Vector of push-type elements in this group, there are only push type elemenst, like in ReadAnyGroup
-    std::map<TransferElementID, TransferElementAbstractor> push_elements;
+    void initMatcher();
+    static void checkAccess(const TransferElementAbstractor& element);
 
     /// the matching mode used in update()
-    MatchingMode mode{MatchingMode::exact};
+    MatchingMode _mode{MatchingMode::exact};
+
+    std::unique_ptr<DataConsistencyGroupDetail::MatcherBase> _impl;
   };
 
   /********************************************************************************************************************/
 
   template<typename ITERATOR>
-  DataConsistencyGroup::DataConsistencyGroup(ITERATOR first, ITERATOR last) {
-    for(auto it = first; it != last; ++it) add(*it);
-  }
-
-  /********************************************************************************************************************/
-
-  inline bool DataConsistencyGroup::update(const TransferElementID& transferElementID) {
-    // ignore transferElementID does not belong to DataConsistencyGroup
-    if(push_elements.find(transferElementID) == push_elements.end()) {
-      return false;
+  DataConsistencyGroup::DataConsistencyGroup(ITERATOR first, ITERATOR last, MatchingMode mode, unsigned int histLen)
+  : _mode(mode) {
+    initMatcher();
+    for(auto it = first; it != last; ++it) {
+      add(*it, histLen);
     }
-    // if matching mode is none, always return true
-    if(mode == MatchingMode::none) return true;
-    assert(mode == MatchingMode::exact);
-
-    auto getVNFromElement = push_elements[transferElementID].getVersionNumber();
-    assert(getVNFromElement != VersionNumber{nullptr});
-    if(getVNFromElement < versionNumberToBeConsistentTo) {
-      return false;
-    }
-    if(versionNumberToBeConsistentTo != getVNFromElement) {
-      versionNumberToBeConsistentTo = getVNFromElement;
-      consistentElements.clear();
-    }
-    consistentElements.insert(transferElementID);
-    if(consistentElements.size() == push_elements.size()) {
-      lasteSateOfConsistentElements = consistentElements;
-      return true;
-    }
-    return false;
   }
 
   /********************************************************************************************************************/
