130ac6760b128b59101d355b9bda8915e41e4af7
Dietrich Rothe <dietrich.rothe@desy.de>
2025-02-28 11:57:02 +0100

    fix: move TransferElementID functions to right place
    relevant if TransferElement.h not included

--- include/TransferElement.h
+++ include/TransferElement.h
@@ -869,24 +869,3 @@ namespace ChimeraTK {
   }; // namespace ChimeraTK
 
 } /* namespace ChimeraTK */
-namespace std {
-
-  /********************************************************************************************************************/
-
-  /** Hash function for putting TransferElementID e.g. into an std::unordered_map
-   */
-  template<>
-  struct hash<ChimeraTK::TransferElementID> {
-    std::size_t operator()(const ChimeraTK::TransferElementID& f) const { return std::hash<size_t>{}(f._id); }
-  };
-
-  /********************************************************************************************************************/
-
-  /** Comparison for putting TransferElementID e.g. into an std::map */
-  template<>
-  struct less<ChimeraTK::TransferElementID> {
-    bool operator()(const ChimeraTK::TransferElementID& a, const ChimeraTK::TransferElementID& b) const {
-      return a._id < b._id;
-    }
-  };
-} // namespace std
