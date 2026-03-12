c3aa4bb0daf8ee789e5dbc05444bb64097144da2
Martin Killenberg <martin.killenberg@desy.de>
2026-03-12 11:16:39 +0100

    feat(SubDevice): use RawConverter instead of FixedPointConverter

---
+++ backends/Subdevice/include/SubdeviceConvertingDecorator.h
@@ -0,0 +1,58 @@
+// SPDX-FileCopyrightText: Deutsches Elektronen-Synchrotron DESY, MSK, ChimeraTK Project <chimeratk-support@desy.de>
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#pragma once
+
+#include "NDRegisterAccessorDecorator.h"
+#include "RawConverter.h"
+
+namespace ChimeraTK {
+
+  /********************************************************************************************************************/
+
+  template<typename UserType, typename TargetUserType>
+  class ConvertingDecorator : public NDRegisterAccessorDecorator<UserType, TargetUserType> {
+   public:
+    explicit ConvertingDecorator(const boost::shared_ptr<ChimeraTK::NDRegisterAccessor<TargetUserType>>& target,
+        NumericAddressedRegisterInfo const& registerInfo);
+
+    void doPreRead(TransferType type) override;
+
+    void doPostRead(TransferType type, bool hasNewData) override;
+
+    // Callback for ConverterLoopHelper, see documentation there.
+    template<class CookedType, typename RawType, RawConverter::SignificantBitsCase sc, RawConverter::FractionalCase fc,
+        bool isSigned>
+    void doPostReadImpl(RawConverter::Converter<CookedType, RawType, sc, fc, isSigned> converter,
+        [[maybe_unused]] size_t implParameter);
+
+    void doPreWrite(TransferType type, VersionNumber versionNumber) override;
+
+    // Callback for ConverterLoopHelper, see documentation there.
+    template<class CookedType, typename RawType, RawConverter::SignificantBitsCase sc, RawConverter::FractionalCase fc,
+        bool isSigned>
+    void doPreWriteImpl(RawConverter::Converter<CookedType, RawType, sc, fc, isSigned> converter,
+        [[maybe_unused]] size_t implParameter);
+
+    void doPostWrite(TransferType type, VersionNumber versionNumber) override;
+
+    [[nodiscard]] bool mayReplaceOther(const boost::shared_ptr<ChimeraTK::TransferElement const>& other) const override;
+
+   protected:
+    std::unique_ptr<RawConverter::ConverterLoopHelper> _converterLoopHelper;
+    NumericAddressedRegisterInfo _registerInfo;
+
+    using NDRegisterAccessorDecorator<UserType, TargetUserType>::_target;
+    using NDRegisterAccessor<UserType>::buffer_2D;
+  };
+
+  /********************************************************************************************************************/
+
+  DECLARE_MULTI_TEMPLATE_FOR_CHIMERATK_USER_TYPES(ConvertingDecorator, uint8_t);
+  DECLARE_MULTI_TEMPLATE_FOR_CHIMERATK_USER_TYPES(ConvertingDecorator, uint16_t);
+  DECLARE_MULTI_TEMPLATE_FOR_CHIMERATK_USER_TYPES(ConvertingDecorator, uint32_t);
+  DECLARE_MULTI_TEMPLATE_FOR_CHIMERATK_USER_TYPES(ConvertingDecorator, uint64_t);
+
+  // FIXME: get rid of the signed ints!
+  DECLARE_MULTI_TEMPLATE_FOR_CHIMERATK_USER_TYPES(ConvertingDecorator, int32_t);
+
+} // namespace ChimeraTK
---
+++ backends/Subdevice/include/SubdeviceConvertingRawDecorator.h
@@ -0,0 +1,49 @@
+// SPDX-FileCopyrightText: Deutsches Elektronen-Synchrotron DESY, MSK, ChimeraTK Project <chimeratk-support@desy.de>
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#pragma once
+
+#include "NDRegisterAccessorDecorator.h"
+#include "RawConverter.h"
+
+namespace ChimeraTK {
+
+  /********************************************************************************************************************/
+
+  template<typename TargetUserType>
+  class ConvertingRawDecorator : public NDRegisterAccessorDecorator<TargetUserType> {
+   public:
+    ConvertingRawDecorator(const boost::shared_ptr<ChimeraTK::NDRegisterAccessor<TargetUserType>>& target,
+        NumericAddressedRegisterInfo const& registerInfo);
+
+    template<typename COOKED_TYPE>
+    // NOLINTNEXTLINE(readability-identifier-naming)
+    COOKED_TYPE getAsCooked_impl(unsigned int channel, unsigned int sample);
+
+    template<typename COOKED_TYPE>
+    // NOLINTNEXTLINE(readability-identifier-naming)
+    void setAsCooked_impl(unsigned int channel, unsigned int sample, COOKED_TYPE value);
+
+    [[nodiscard]] bool mayReplaceOther(const boost::shared_ptr<ChimeraTK::TransferElement const>& other) const override;
+
+   protected:
+    NumericAddressedRegisterInfo _registerInfo;
+    DEFINE_VIRTUAL_FUNCTION_TEMPLATE_VTABLE_FILLER(
+        FixedPointConvertingRawDecorator<TargetUserType>, getAsCooked_impl, 2);
+    DEFINE_VIRTUAL_FUNCTION_TEMPLATE_VTABLE_FILLER(
+        FixedPointConvertingRawDecorator<TargetUserType>, setAsCooked_impl, 3);
+
+    using NDRegisterAccessorDecorator<TargetUserType>::_target;
+    using NDRegisterAccessor<TargetUserType>::buffer_2D;
+  };
+
+  /********************************************************************************************************************/
+
+  DECLARE_TEMPLATE_FOR_CHIMERATK_RAW_TYPES(ConvertingRawDecorator);
+
+  // FIXME: get rid of the deprecated signed raw
+  extern template class ConvertingRawDecorator<int8_t>;
+  extern template class ConvertingRawDecorator<int16_t>;
+  extern template class ConvertingRawDecorator<int32_t>;
+  extern template class ConvertingRawDecorator<int64_t>;
+
+} // namespace ChimeraTK
