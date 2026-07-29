553359b9b80c7d2358dd5866a66762d17aba85d5
Martin Killenberg <martin.killenberg@desy.de>
2026-07-29 14:35:40 +0200

    feat: Different data types for numeric addressed registers with # (BAR) syntax
    Until now, the data type for direct numerical addressing was always implicitly signed int32. It now can be any signed or unsigned integer with 8, 16, 32 or 64 bits.

--- src/NumericAddressedBackend.cc
+++ src/NumericAddressedBackend.cc
@@ -48,30 +48,7 @@ namespace ChimeraTK {
   /********************************************************************************************************************/
 
   NumericAddressedRegisterInfo NumericAddressedBackend::getRegisterInfo(const RegisterPath& registerPathName) {
-    if(!registerPathName.startsWith(numeric_address::BAR())) {
-      return _registerMap.getBackendRegister(registerPathName);
-    }
-    /// FIXME move into catalogue implementation, then make return type a const reference!
-
-    auto components = registerPathName.getComponents();
-    if(components.size() != 3) {
-      throw ChimeraTK::logic_error("Illegal numeric address: '" + (registerPathName) + "'");
-    }
-    auto bar = std::stoi(components[1]);
-    size_t pos = components[2].find_first_of('*');
-    auto address = std::stoi(components[2].substr(0, pos));
-    size_t nBytes;
-    if(pos != std::string::npos) {
-      nBytes = std::stoi(components[2].substr(pos + 1));
-    }
-    else {
-      nBytes = sizeof(int32_t);
-    }
-    auto nElements = nBytes / sizeof(int32_t);
-    if(nBytes == 0 || nBytes % sizeof(int32_t) != 0) {
-      throw ChimeraTK::logic_error("Illegal numeric address: '" + (registerPathName) + "'");
-    }
-    return NumericAddressedRegisterInfo(registerPathName, nElements, address, nBytes, bar);
+    return _registerMap.getBackendRegister(registerPathName);
   }
 
   /********************************************************************************************************************/
